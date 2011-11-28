628aca8594e3b5aa365d39cb6ffbc494e72f99fb
zhangj83 <jitao_david.zhang@roche.com>
2011-11-28 15:39:23 +0100

    a not-yet finished version of fmecor; see the man page for the current development status

--- src/fme_cor.c
+++ src/fme_cor.c
@@ -1,6 +1,7 @@
 /* Fast and memory-efficient correlation matrix */
 //#include <Rmath.h>
 #include <Rdefines.h>
+#include <math.h>
 #include <array.h>
 #include <log.h>
 #include "gsl/gsl_statistics.h"
@@ -8,38 +9,45 @@
 #include "ribios_utils.h"
 #include "ribios_utils.h"
 
-
+// note that fme_cor differs from the native cor function: it calculates row-wise correlation coefficients
 SEXP fme_cor(SEXP mat) {
   int dima, nrow, ncol;
   SEXP ans, ptr;
-  int i,j,count,ind;
+  int i,j,k,ind;
   double tmp;
-  double* pmat;
-
+  double* pmat=REAL(mat);
 
   nrow=nrows(mat);
   ncol=ncols(mat);
-  double data1[ncol], data2[ncol];
-  dima= nrow*(nrow-1)/2;
-  Array res = arrayCreate(dima, short);  
-  pmat = NUMERIC_POINTER(mat);
 
+  dima= nrow/2*(nrow+1);
+  short res[dima];
+  double means[nrow], sds[nrow];
+
+  for(i=0; i<nrow;++i) {
+    means[i]=pmat[i];
+    sds[i]=0;
+    for(j=1;j<ncol;++j) {
+      tmp=pmat[i+j*nrow]-means[i];
+      means[i] += tmp/(j+1);
+      sds[i]+=tmp*(pmat[i+j*nrow]-means[i]);
+    }
+    sds[i]=sqrt(sds[i]/(ncol-1));
+  }
+  
   ind=0;
-  for(i=0; i<nrow-1; ++i) {
-    for(j=i+1;j<nrow;++j) {
-      for(count=0;count<ncol;++count) {
-	data1[count]=pmat[i+count*nrow];
-	data2[count]=pmat[j+count*nrow];
+  for(i=0; i<nrow; ++i) {
+    for(j=i;j<nrow;++j) {
+      tmp=0; 
+      for(k=0;k<ncol;++k) {
+	tmp+=(pmat[k*nrow+i]-means[i])*(pmat[k*nrow+j]-means[j]);
       }
-      tmp=gsl_stats_correlation(data1,
-				1,
-				data2,
-				1,
-				ncol);
+      tmp/=((ncol-1)*sds[i]*sds[j]);
 #ifdef DEBUG
+      Rprintf("[%1.2f, %1.2f]", means[i], sds[i]);
       Rprintf("%1.3f\t", tmp);
 #endif
-      array(res, ind,short)= (short) tmp*10000;
+      res[ind]=tmp;
       ind++;
     }
 #ifdef DEBUG
@@ -47,10 +55,12 @@ SEXP fme_cor(SEXP mat) {
 #endif
   }
 
-  PROTECT(ans = allocVector(INTSXP, 1));
+  PROTECT(ans = allocVector(INTSXP, 3));
   PROTECT(ptr=R_MakeExternalPtr(&res, install("RIBIOSUTILS_cor"), R_NilValue));
 
-  INTEGER(ans)[0]=arrayMax(res);
+  INTEGER(ans)[0]=ncol;
+  INTEGER(ans)[1]=ncol;
+  INTEGER(ans)[2]=dima;
 
   setAttrib(ans, install("handle_ptr"),ptr);
   UNPROTECT(2);

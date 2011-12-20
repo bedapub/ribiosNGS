5eab676740e3a22ada67baa3bfafb674901bc2f3
zhangj83 <jitao_david.zhang@roche.com>
2011-12-20 16:54:12 +0100

    version 1.0.7: .onLoad() judges whether ORACLE_BASE and ORACLE_HOME environmental variables should be set

--- R/utils.R
+++ R/utils.R
@@ -3,6 +3,14 @@ ORACLE.PWD <- "biread"
 
 .onLoad <- function(libname, pkgname) {
   assign("ORA", Oracle(), pos=sys.frame())
+  obase <- Sys.getenv("ORACLE_BASE")
+  ohome <- Sys.getenv("ORACLE_HOME")
+  if(identical(obase, "") || identical(obase, ".") || !"client" %in% dir(obase)) {
+    Sys.setenv("ORACLE_BASE"="/opt/oracle")
+  }
+  if(identical(ohome, "") || identical(ohome, ".") || !"bin" %in% dir(ohome)) {
+    Sys.setenv("ORACLE_HOME"="/opt/oracle/client/10/run_1")
+  }
 }
 
 newcon <- function() {

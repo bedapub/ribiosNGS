84697d3e01f7a91fbf42f2d82f5809a850981207
zhangj83 <jitao_david.zhang@roche.com>
2011-12-20 08:52:43 +0100

    Version 1.0-6: functions all have gti as prefix

--- tests/gtiChipAnnotation.R
+++ tests/gtiChipAnnotation.R
@@ -1,11 +1,11 @@
 library(ribiosAnnotation)
 
-supported.chips <- raceChiptypes() 
+supported.chips <- gtiChiptypes() 
 stopifnot(is.character(supported.chips) && length(supported.chips)>1)
 ## "house-keeping" chips  probably are there
 stopifnot(all(c("HG-U133_PLUS_2", "HG_U95A", "HUMANHT-12_V3_0_R1_11283641_A") %in% supported.chips))
 
 mychip <- "HG-U133_PLUS_2"
-mychip.anno <- raceChipAnnotation(mychip)
+mychip.anno <- gtiChipAnnotation(mychip)
 mychip.reanno <- annotateProbesets(sample(mychip.anno$ProbeID, 20),
                                    mychip)

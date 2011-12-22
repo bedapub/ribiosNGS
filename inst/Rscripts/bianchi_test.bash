b64701a12aa3436bfe2ee9ad6ed8ceec7eec8144
zhangj83 <jitao_david.zhang@roche.com>
2011-12-22 16:13:34 +0100

    the test run was finished without error

--- inst/Rscripts/bianchi_test.bash
+++ inst/Rscripts/bianchi_test.bash
@@ -4,6 +4,7 @@ export RS_DIR=/SOFT/bi/apps/ribios/ribiosBic/inst/Rscripts/
 export DATA_DIR=/SOFT/bi/apps/ribios/ribiosBic/inst/extdata/
 export OUT_DIR=/DATA/tmp/
 
+## testing file -> RData
 ${RS_DIR}/bianchi_preprocess.Rscript -infile ${DATA_DIR}/sample_exprs.txt -outfile ${OUT_DIR}/bianchi_test_tab.RData
 ${RS_DIR}/bianchi_preprocess.Rscript -infile ${DATA_DIR}/sample_exprs.txt -ffile ${DATA_DIR}/sample_fdata.txt -outfile ${OUT_DIR}/bianchi_test_tab_fdata.RData
 ${RS_DIR}/bianchi_preprocess.Rscript -infile ${DATA_DIR}/sample_exprs.txt -pfile ${DATA_DIR}/sample_pdata.txt -outfile ${OUT_DIR}/bianchi_test_tab_pdata.RData
@@ -19,3 +20,13 @@ ${RS_DIR}/bianchi_preprocess.Rscript -infile ${DATA_DIR}/sample.gct -clsfile ${D
 ${RS_DIR}/bianchi_preprocess.Rscript -infile ${DATA_DIR}/sample.gct -clsfile ${DATA_DIR}/sample.cls -chiptype HG_U95AV2 -summfeat false -outfile ${OUT_DIR}/bianchi_test_ann_gct_cls_nosumm.RData
 ${RS_DIR}/bianchi_preprocess.Rscript -infile ${DATA_DIR}/sample.gct -clsfile ${DATA_DIR}/sample.cls -chiptype HG_U95AV2 -summfeat false -topvar 100 -outfile ${OUT_DIR}/bianchi_test_ann_gct_cls_nosumm_top100.RData
 ${RS_DIR}/bianchi_preprocess.Rscript -infile ${DATA_DIR}/sample.gct -clsfile ${DATA_DIR}/sample.cls -chiptype HG_U95AV2 -summfeat true -outfile ${OUT_DIR}/bianchi_test_ann_gct_cls_summ.RData
+
+## testing RData->ISA
+./bianchi_ISA.Rscript -infile ${OUT_DIR}/bianchi_test_tab.RData -binary -outfile ${OUT_DIR}/bianchi_test_tab.isa.bic
+
+./bianchi_QUBIC.Rscript -infile ${OUT_DIR}/bianchi_test_tab.RData -binary -outfile ${OUT_DIR}/bianchi_test_tab.qubic.bic
+./bianchi_QUBIC.Rscript -infile ${OUT_DIR}/bianchi_test_tab.RData -binary -rank 1 -prop 0.1 -outfile ${OUT_DIR}/bianchi_test_tab.qubic.nocore.bic
+./bianchi_QUBIC.Rscript -infile ${OUT_DIR}/bianchi_test_tab.RData -binary -rank 1,2,3 -prop 0.1,0.2,0.4 -coreNo 3 -outfile ${OUT_DIR}/bianchi_test_tab.qubic_core.bic
+./bianchi_QUBIC.Rscript -infile ${DATA_DIR}/sample_exprs.txt -rank 1,3 -prop 0.1,0.2 -coreNo 3 -outfile ${OUT_DIR}/bianchi_test_tab.qubic_txt.bic
+./bianchi_QUBIC.Rscript -infile ${DATA_DIR}/sample.gct -rank 1,3 -prop 0.1,0.2 -coreNo 3 -outfile ${OUT_DIR}/bianchi_test_tab.qubic_gct.bic -parafile ${OUT_DIR}/bianchi_test_tab.qubic_gct.para
+./bianchi_QUBIC.Rscript -infile ${OUT_DIR}/bianchi_test_tab.RData -binary -rank 1,2 -prop 0.1,0.4 -coreNo 3 -outfile ${OUT_DIR}/bianchi_test_tab_all.qubic.bic -tolerance 0.85 -filter 0.1 -report 10 -parafile ${OUT_DIR}/bianchi_test_tab_all.qubic.para

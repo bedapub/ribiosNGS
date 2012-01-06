4feba6a4039dcce21ecb2ac188886fc555dffa12
zhangj83 <jitao_david.zhang@roche.com>
2012-01-06 14:36:38 +0100

    Replace sprintGmt by formatGmt, a S4 method allowing flexible printing of Gmt files

---
+++ tests/test_gsea.R
@@ -0,0 +1,16 @@
+library(ribiosExpression)
+
+up.file <- system.file("extdata/tags_up.grp", package="ribiosExpression")
+down.file <- system.file("extdata/tags_down.grp", package="ribiosExpression")
+
+grp2gmt(readLines(up.file, n=-1))
+grp2gmt(readLines(up.file, n=-1), chiptype="HG_U95AV2")
+grp2gmt(readLines(up.file, n=3), chiptype="HG_U95AV2", name="my_up")
+grp2gmt(readLines(down.file, n=-1))
+grp2gmt(readLines(down.file, n=-1), chiptype="HG_U95AV2")
+grp2gmt(readLines(down.file, n=-1), chiptype="HG_U95AV2")
+
+grpFiles2gmt(up.file)
+grpFiles2gmt(c(up.file, down.file))
+grpFiles2gmt(c(up.file, down.file), n=3)
+grpFiles2gmt(c(up.file, down.file), n=3, chiptype="HG_U95AV2")

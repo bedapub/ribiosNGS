dd00c911106a0ae9544dc74488183c668ef9dad9
zhangj83 <jitao_david.zhang@roche.com>
2012-01-06 11:20:40 +0100

    bioc2gti() and gti2bioc() supports missing parameter

--- R/gti2bioc.R
+++ R/gti2bioc.R
@@ -1,8 +1,23 @@
 ## translate GTI chiptype to bioc chiptype
-gti2bioc <- function(chipname) {
-  matchColumn(chipname, gtibioc, "GTI")$Bioconductor
+bioc2gti <- function (chipname) 
+{
+  if(missing(chipname)) {
+    supp <- !is.na(gtibioc$Bioconductor)
+    vec <- gtibioc$GTI[supp]
+    names(vec) <- gtibioc$Bioconductor[supp]
+    return(vec)
+  } else {
+    matchColumn(chipname, gtibioc, "Bioconductor")$GTI
+  }
 }
 
-bioc2gti <- function(chipname) {
-  matchColumn(chipname, gtibioc, "Bioconductor")$GTI
+gti2bioc <- function(chipname) {
+  if(missing(chipname)) {
+    supp <- !is.na(gtibioc$Bioconductor)
+    vec <- gtibioc$Bioconductor[supp]
+    names(vec) <- gtibioc$GTI[supp]
+    return(vec)
+  } else {
+    matchColumn(chipname, gtibioc, "GTI")$Bioconductor
+  }
 }
--- tests/gtibioc.R
+++ tests/gtibioc.R
@@ -9,3 +9,11 @@ stopifnot(identical(gti2bioc("HG_U95AV2"),
                     "hgu95av2"))
 stopifnot(identical(gti2bioc(c("HG_U95AV2", "CANINE", "HG_U95A")),
                     c("hgu95av2", "canine", "hgu95a")))
+
+## testing missing options
+gtis <- bioc2gti()
+biocs <- gti2bioc()
+stopifnot(all(c("HG-U133_PLUS_2", "HG_U95AV2") %in% gtis))
+stopifnot(all(c("hgu133plus2", "hgu95av2") %in% biocs))
+stopifnot(all(c("HG-U133_PLUS_2", "HG_U95AV2") %in% names(biocs)))
+stopifnot(all(c("hgu133plus2", "hgu95av2") %in% names(gtis)))

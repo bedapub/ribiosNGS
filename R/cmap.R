2e3f9544433b43948ab982beb72fcd1f4a687fe2
zhangj83 <jitao_david.zhang@roche.com>
2012-01-12 17:23:06 +0100

    version 1.0-1: see NEWS

--- R/cmap.R
+++ R/cmap.R
@@ -1,25 +1,28 @@
 cmap <- function(rnks, up, down,
-                 scaling=TRUE,
                  permB=0,
                  sortBy=c("default", "none", "p")) {
   sortBy <- match.arg(sortBy)
-  cscoresAll <- lapply(rnks, function(x) connScore(x, up=up, down=down))
-  cscores <- sapply(cscoresAll, "[[", 1)
+  stopifnot(is.matrix(rnks))
+  
+  cscoreAll <- connScore(rnks, up, down)
+  
+  cscores <- cscoreAll[, 1L]
+  ksups <- cscoreAll[, 2L]
+  ksdowns <- cscoreAll[, 3L]
   
   ## permutation: only applied to cases where connScore!=0
   if(permB>0) {
     n.up <- length(up)
     n.down <- length(down)
     non0.cscore <- which(cscores != 0)
-    cscoreP <- numeric(length(rnks))
+    cscoreP <- numeric(length(cscores))
     cscoreP[cscores==0] <- 1
     if(length(non0.cscore)>0) {
-      cscorePerm <- lapply(rnks[non0.cscore],
-                           function(x) indexConnScorePerm(n=length(x), n.up=n.up, n.down=n.down, B=permB)[, 1L])
+      cscorePerm <- connScorePerm(x=length(cscores), up=n.up, down=n.down, B=permB)[,1L]
       cscoreP[non0.cscore] <- sapply(seq(along=non0.cscore),
                                      function(i) {
                                        cs <- cscores[ non0.cscore[i] ]
-                                       mean(abs(cscorePerm[[i]])>=abs(cs))
+                                       mean(abs(cscorePerm)>=abs(cs))
                                      })
     }
     cscoreFDR <- p.adjust(cscoreP, "BH")
@@ -27,19 +30,14 @@ cmap <- function(rnks, up, down,
     cscoreP <- cscoreFDR <- rep(as.numeric(NA), length(rnks))
   }
 
-  ksups <- sapply(cscoresAll, "[[", 2)
-  ksdowns <- sapply(cscoresAll, "[[", 3)
-  rind <- seq(along=rnks)
-  S <- vector("numeric", length(rnks))
+  rind <- seq(along=cscores)
+  S <- vector("numeric", length(cscores))
   
-  if(scaling) {
-    p <- max(cscores)
-    q <- min(cscores)
-    S[cscores>0] <- cscores[cscores>0]/p
-    S[cscores<0] <- -cscores[cscores<0]/q
-  } else {
-    S <- cscores
-  }
+  ## scaling
+  p <- max(cscores)
+  q <- min(cscores)
+  S[cscores>0] <- cscores[cscores>0]/p
+  S[cscores<0] <- -cscores[cscores<0]/q
   
   ind <- rind[order(S, ksups, decreasing=TRUE)]
   res <- data.frame(index=ind,
@@ -49,6 +47,8 @@ cmap <- function(rnks, up, down,
                     ksdown=ksdowns[ind],
                     p=cscoreP[ind],
                     FDR=cscoreFDR[ind])
+
+  ## reordering
   if(sortBy=="none") {
     res <- res[order(res$index),,drop=FALSE]
   } else if (sortBy=="p") {
--- R/connScore.R
+++ R/connScore.R
@@ -1,34 +1,108 @@
-indexConnScore <- function(n, ind.up, ind.down) {
-  if(all(is.na(ind.up)) || all(is.na(ind.down)))
-    return(c(cscore=0, ksup=NA, ksdown=NA))
-  ksup <- ksScore(n=n, vec=ind.up)
-  ksdown <- ksScore(n=n, vec=ind.down)
-  if(sign(ksup)==sign(ksdown)) {
-    cscore <- 0
-  } else {
-    cscore <- ksup - ksdown
-  }
-  return(c(cscore=cscore, ksup=ksup, ksdown=ksdown))
-}
+##----------------------------------------##
+## generic method connScore
+## ? overhead : S4 method does not have much more overhead than S3 function
+##----------------------------------------##
+setGeneric("connScore", function(x, up, down) standardGeneric("connScore"))
 
-indexConnScorePerm <- function(n, n.up, n.down, B=1000) {
-  t(sapply(1:B, function(b) {
-    vup <- sample(1:n, n.up, replace=TRUE)
-    vdown <- sample(1:n, n.down, replace=TRUE)
-    indexConnScore(n=n, ind.up=vup, ind.down=vdown)
-  }))
-}
 
-connScore <- function(rnk, up, down) {
-  n <- length(rnk)
-  vup <- match(up, rnk)
-  vdown <- match(down,rnk)
-  indexConnScore(n=n, ind.up=vup, ind.down=vdown)
-}
+setMethod("connScore",
+          c("character", "character", "character"),
+          function(x,up,down) {
+            n <- length(x)
+            vup <- match(up, x)
+            vdown <- match(down,x)
+            connScore(x=n, up=vup, down=vdown)
+          })
+setMethod("connScore",
+          c("numeric", "numeric", "numeric"),
+          function(x,up,down) {
+            if(length(x)!=1) {
+              warning("'x' length larger than 1: it was treated as a character vector\n")
+              return(connScore(as.character(x), as.character(up), as.character(down)))
+            }
+            if(all(is.na(up)) || all(is.na(down)))
+              return(c(cscore=0, ksup=NA, ksdown=NA))
+            ksup <- ksScore(n=x, vec=up)
+            ksdown <- ksScore(n=x, vec=down)
+            if(sign(ksup)==sign(ksdown)) {
+              return(c(cscore=0, ksup=ksup, ksdown=ksdown))
+            } else {
+              return(c(cscore=ksup - ksdown, ksup=ksup, ksdown=ksdown))
+            }
+          })
+setMethod("connScore",
+          c("list", "character", "character"),
+          function(x, up, down) {
+            t(sapply(x, function(v) connScore(v, up, down)))
+          })
+setMethod("connScore",
+          c("matrix", "numeric", "numeric"),
+          function(x, up, down) {
+            nr <- nrow(x)
+            t(apply(x, 2L, function(v) connScore(nr, v[up], v[down])))
+          })
+setMethod("connScore",
+          c("matrix", "character", "character"),
+          function(x, up, down) {
+            up.ind <- match(up, rownames(x))
+            down.ind <- match(down, rownames(x))
+            connScore(x, up=up.ind, down=down.ind)
+          })
 
-connScorePerm <- function(rnk, up, down, B=1000) {
-  n <- length(rnk)
-  n.up <- length(up)
-  n.down <- length(down)
-  indexConnScorePerm(n=n, n.up=n.up, n.down=n.down, B=B)
-}
+##----------------------------------------##
+## connScorePerm
+##----------------------------------------##
+setGeneric("connScorePerm", function(x, up, down, B) standardGeneric("connScorePerm"))
+
+setMethod("connScorePerm",
+          c("numeric", "numeric", "numeric", "numeric"),
+          function(x, up, down, B) {
+            if(length(x)>1) {
+              warning("'x' is a numeric vector: its length will be used for permutation.\n  To avoid this message, use length(x) as input.")
+              x <- length(x)
+            }
+            if(length(up)>1) {
+              warning("'up' is a numeric vector: its length will be used for permutation.\n  To avoid this message, use length(x) as input.")
+              up <- length(up)
+            }
+            if(length(down)>1) {
+              warning("'down' is a numeric vector: its length will be used for permutation.\n  To avoid this message, use length(x) as input.")
+              down <- length(down)
+            }
+            stopifnot(length(x)==1 && length(up)==1 && length(down)==1)
+            vec <- 1:x
+            t(sapply(1:B, function(b) {
+              vup <- sample(vec, up, replace=TRUE)
+              vdown <- sample(vec, down, replace=TRUE)
+              connScore(x=x, up=vup, down=vdown)
+            }))
+          })
+setMethod("connScorePerm",
+          c("matrix", "numeric", "numeric","numeric"),
+          function(x, up, down, B) {
+            connScorePerm(nrow(x), up=up, down=down, B=B)
+          })
+          
+setMethod("connScorePerm",
+          c("character", "numeric", "numeric", "numeric"),
+          function(x, up, down, B) {
+            connScorePerm(length(x), up=up, down=down, B=B)
+          })
+setMethod("connScorePerm",
+          c("character", "character", "character", "numeric"),
+          function(x, up, down, B) {
+            connScorePerm(length(x), up=length(up), down=length(down), B=B)
+          })
+setMethod("connScorePerm",
+          c("list", "numeric", "numeric", "numeric"),
+          function(x,up, down, B) {
+            lapply(x, function(t) connScorePerm(length(t), up=up, down=down, B=B))
+          })
+setMethod("connScorePerm",
+          c("list", "character", "character", "numeric"),
+          function(x,up, down, B) {
+            nup <- length(up)
+            ndown <- length(down)
+            lapply(x, function(t) connScorePerm(length(t), up=nup, down=ndown, B=B))
+          })
+          

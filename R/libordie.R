ce1bb317c3f6162d318fe74de30530e2cd4da8c8
zhangj83 <jitao_david.zhang@roche.com>
2012-01-18 14:57:33 +0100

    the libordie function now runs on both R-2.11.1 (stable) and R-2.15.0 (svn)

--- R/libordie.R
+++ R/libordie.R
@@ -1,3 +1,4 @@
+## The libordie function has been tested to run on both R-2.11.1 and R-2.15.0
 libordie <- function(package,
                      minVer,
                      missing.quit.status=1,
@@ -6,11 +7,17 @@ libordie <- function(package,
   package <- as.character(substitute(package))
   if(!package %in% installed.packages()[,1])
     qqmsg("Error: This script requires ", package, " but not found",
-          status=missing.quit.status) 
-  if(!missing(minVer) && packageVersion(package) < package_version (minVer))
+          status=missing.quit.status)
+  pkgVer <- package_version(packageDescription(package, fields="Version"))
+  if(!missing(minVer) && pkgVer < package_version (minVer))
     qqmsg("Error: This script requires ", package, " >=",
           package_version(minVer), " but ",
-          packageVersion(package), " was found",
+          pkgVer, " was found",
           status=ver.quit.status)
-  suppressMessages(library(package,character.only=TRUE,quietly=TRUE, ...))
+  ## quietly was a feature (at latest) available from R-2.13.0
+  canQuiet <- "quietly" %in% names(as.list(args(library)))
+  if(canQuiet)
+    suppressMessages(library(package,character.only=TRUE,quietly=TRUE, ...))
+  else
+    suppressMessages(library(package,character.only=TRUE,...))
 }

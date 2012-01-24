5b9c78a8208d0db42c525ec99007aa6f6a08ed64
zhangj83 <jitao_david.zhang@roche.com>
2012-01-24 14:42:10 +0100

    parameterized ORACLE_BASE and ORACLE_HOME; added ORACLE_LIB, however changing it in a R session does not help to load ribiosAnnotation. LD_LIBRARY_PATH must be set before R starts

--- R/utils.R
+++ R/utils.R
@@ -1,16 +1,21 @@
 ORACLE.USER <- "biread"
 ORACLE.PWD <- "biread"
+ORACLE.BASE <- "/opt/oracle"
+ORACLE.HOME <- "/opt/oracle/client/10/run_1"
+## ORACLE.LIB <- ":/opt/oracle/client/10/run_1/lib"
 
 .onLoad <- function(libname, pkgname) {
   assign("ORA", Oracle(), pos=sys.frame())
   obase <- Sys.getenv("ORACLE_BASE")
   ohome <- Sys.getenv("ORACLE_HOME")
   if(identical(obase, "") || identical(obase, ".") || !"client" %in% dir(obase)) {
-    Sys.setenv("ORACLE_BASE"="/opt/oracle")
+    Sys.setenv("ORACLE_BASE"=ORACLE.BASE)
   }
   if(identical(ohome, "") || identical(ohome, ".") || !"bin" %in% dir(ohome)) {
-    Sys.setenv("ORACLE_HOME"="/opt/oracle/client/10/run_1")
+    Sys.setenv("ORACLE_HOME"=ORACLE.HOME)
   }
+##  new.path <- paste(Sys.getenv("LD_LIBRARY_PATH"), ORACLE.LIB, sep=":");
+##  Sys.setenv("LD_LIBRARY_PATH"=new.path)
 }
 
 newcon <- function() {

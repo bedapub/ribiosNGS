447b561175ab44cfaacf040cf2dd98dc463174e8
zhangj83 <jitao_david.zhang@roche.com>
2012-01-24 10:50:16 +0100

    add annotateGeneIDs and annotateGeneSymbols

--- R/annotateProbesets.R
+++ R/annotateProbesets.R
@@ -1,12 +1,32 @@
-annotateProbesets <- function(ids, chip="HG-U133_PLUS_2") {
+annotateIDs <- function(ids, chip="HG-U133_PLUS_2", column="ProbeID") {
   ids <- as.character(ids)
-
   chipAnn <- gtiChipAnnotation(chip)
-  chipAnnOrd <- matchColumn(ids, chipAnn, "ProbeID")
-  if(identical(anyDuplicated(ids), 0L)) {
+  chipAnnOrd <- matchColumn(ids, chipAnn, column)
+  
+  if(identical(anyDuplicated(ids), 0L) & !any(is.na(ids))) {
     rownames(chipAnnOrd) <- ids
   } else {
     rownames(chipAnnOrd) <- NULL
   }
+  return(chipAnnOrd)
+}
+
+annotateProbesets <- function(ids, chip="HG-U133_PLUS_2") {
+  annotateIDs(ids=ids, chip=chip, column="ProbeID")
+}
+
+annotateProbeIDs <- annotateProbesets
+
+annotateGeneIDs <- function(ids, chip="HG-U133_PLUS_2", keepProbeID=TRUE) {
+  chipAnnOrd <- annotateIDs(ids=ids, chip=chip, column="GeneID")
+  if(!keepProbeID)
+    chipAnnOrd[,"ProbeID"] <- ids
+  return(chipAnnOrd)
+}
+
+annotateGeneSymbols <- function(ids, chip="HG-U133_PLUS_2", keepProbeID=TRUE) {
+  chipAnnOrd <- annotateIDs(ids=ids, chip=chip, column="GeneSymbol")
+  if(!keepProbeID)
+    chipAnnOrd[,"ProbeID"] <- ids
   return(chipAnnOrd)
 }

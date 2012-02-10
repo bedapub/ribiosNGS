77fa444ebb9b94817458193976ce58664475c5ac
zhangj83 <jitao_david.zhang@roche.com>
2012-02-10 18:00:26 +0100

    The chip annotation is read from genome.chip_probeset_gene

--- R/gtiChipAnnotation.R
+++ R/gtiChipAnnotation.R
@@ -1,21 +1,49 @@
+## format IN syntax
+formatIn <- function(x)
+  paste("(",paste("'", x, "'", sep="", collapse=","),")", sep="")
+
 ## query current GeneID/GeneSymbol from BIOS Oracle
-gtiChipAnnotation <- function(chip) {
-  if(missing(chip)) {
+gtiChipAnnotation <- function(chip,
+                              inCol,
+                              inValues) {
+  if(missing(chip))
     stop("'chip' cannot be be missing. Use 'gtiChipnames()' to see supported chip names")
+  
+  state <- paste("SELECT a.PROBESET_ID, e.RO_GENE_ID, e.GENE_SYMBOL, e.DESCRIPTION, 'TRUE' AS isSingleGeneID, a.ARRAY_TYPE ",
+                 "FROM genome.chip_probeset_gene a, genome.GTI_GENES e ",
+                 "where a.RO_GENE_ID(+) = e.RO_GENE_ID ",
+                 "AND ARRAY_TYPE='",chip, "'", sep="")
+  
+  hasInCol <- !missing(inCol)
+  hasInVal <- !missing(inValues)
+  filterInSQL <- TRUE
+  
+  if(hasInCol && hasInVal) {
+    inValues <- unique(inValues)
+    filterInSQL <- length(inValues) <= ORACLE.IN.NMAX
+    if(filterInSQL) {
+      inCol <- match.arg(inCol,
+                         c("ProbeID", "GeneID", "GeneSymbol"),
+                         several.ok=FALSE)
+      inColTrans <- c("ProbeID"="a.PROBESET_ID", "GeneID"="a.RO_GENE_ID", "GeneSymbol"="e.GENE_SYMBOL")[inCol]
+      state <- paste(state, " AND ", inColTrans, " IN ",
+                     formatIn(inValues));
+    }
+  } else if (!(!hasInCol && !hasInVal)) {
+    stop("'inCol' and 'inVal' must be both specified or left as missing simultaneously\n")
   }
-  con <- newconBIA()
-  state <- paste("SELECT a.AFFY_ID, a.LL, e.OFFICIAL_SYMBOL, e.OFFICIAL_NAME, a.SINGLE_LL,a.PCHIP_NAME ",
-                 "FROM bi.AFFYCHIP_XREF_LL a, bi.EG_GENE_INFO e ",
-                 "where a.LL = e.GENEID ",
-                 "AND PCHIP_NAME='",chip, "'", sep="")
-  rs <- dbSendQuery(con, state)
-  while(!dbHasCompleted(rs)) {
-    ann <- fetch(rs, n=-1)
-  }
-  dbClearResult(rs)
-  dbDisconnect(con)
+  
+  ann <- querydb(state, db="bin")
   colnames(ann) <- c("ProbeID", "GeneID", "GeneSymbol", "GeneName", "isSingleGeneID", "Chip")
-  ann[,"isSingleGeneID"] <- as.logical(ann[,"isSingleGeneID"])
+  asg <- as.logical(ann[,"isSingleGeneID"])
+  ann[,"isSingleGeneID"] <- asg
+  ann[is.na(asg),"isSingleGeneID"] <- FALSE
+  
+  if(hasInCol && !filterInSQL) {
+    filter <- ann[, inCol] %in% inValues
+    ann <- ann[filter,]
+  }
+  
   rownames(ann) <- NULL
   return(ann)
 }

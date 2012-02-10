39f5270fabc44810f59820d3e37dab122b274e13
zhangj83 <jitao_david.zhang@roche.com>
2012-02-10 17:59:29 +0100

    HapMap snps

---
+++ tests/testHapmapSnp.R
@@ -0,0 +1,23 @@
+library(ribiosAnnotation)
+
+snpIds <- c("rs884080", "rs557477", "rs531099", "rs763318")
+hapmapSnp(snpIds)
+hapmapSnp(snpIds, flanking=TRUE, genes=TRUE)
+hapmapSnp(snpIds, flanking=TRUE, genes=FALSE)
+hapmapSnp(snpIds, flanking=FALSE, genes=TRUE)
+hapmapSnp(snpIds, flanking=FALSE, genes=TRUE)
+
+illuminaSnp(snpIds)
+illuminaSnp(snpIds, genes=TRUE)
+illuminaSnp(snpIds, genes=FALSE)
+
+torture <- FALSE
+if(torture) {
+  td <- "/DATA/bi/developers/zhangj83/2012-01-bertram/data/secondary_data/pp_genomePthr_FeatureMarkerPairs.txt"
+  tbl <- read.table(td, sep="\t",header=TRUE)
+  tbl.snps <- as.character(tbl[,2L])
+  system.time(test <- hapmapSnp(tbl.snps, genes=TRUE))
+  system.time(test2 <- hapmapSnp(tbl.snps, genes=FALSE))
+  system.time(test3 <- illuminaSnp(tbl.snps, genes=FALSE))
+  system.time(test4 <- illuminaSnp(tbl.snps, genes=TRUE))
+}

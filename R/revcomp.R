c8fc0d270f6b8f40568ca862b9e3c8563fddfe49
zhangj83 <jitao_david.zhang@roche.com>
2012-02-15 09:06:42 +0100

    revcompNative supports N

--- R/revcomp.R
+++ R/revcomp.R
@@ -9,6 +9,6 @@ revcomp <- function(x) {
 revcompNative <- function(x) {
   chars <- strsplit(x,"")
   chars.rev <- lapply(chars, rev)
-  dict <- c("A"="T", "G"="C", "C"="G", "T"="A", "U"="A")
+  dict <- c("A"="T", "G"="C", "C"="G", "T"="A", "U"="A", "N"="N")
   sapply(chars.rev, function(x) paste(dict[x], collapse=""))
 }

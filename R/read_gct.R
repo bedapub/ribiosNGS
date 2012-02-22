f13d91fe5cdf65367e32bc575d1f8a19686cfcbe
zhangj83 <jitao_david.zhang@roche.com>
2012-02-22 15:53:04 +0100

    Add read_gctstr_matrix function to parse GCT-formatted strings into Matrix

--- R/read_gct.R
+++ R/read_gct.R
@@ -1,5 +1,10 @@
 read_gct_matrix <- function(gct.file, keep.desc=TRUE) {
   gct.file <- checkfile(gct.file)
-  mat <- .Call("read_gct", gct.file, keep.desc)
+  mat <- .Call("read_gct", gct.file, NULL, keep.desc)
+  return(mat)
+}
+
+read_gctstr_matrix <- function(string, keep.desc=TRUE) {
+  mat <- .Call("read_gct", NULL, string, keep.desc)
   return(mat)
 }

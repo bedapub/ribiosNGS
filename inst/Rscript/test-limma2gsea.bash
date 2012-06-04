9736cfd9f9d0d22ee2357ab032210e10c7773e8f
zhangj83 <jitao_david.zhang@roche.com>
2012-06-04 17:24:29 +0200

    version 1.0-16: adding limma2gsea.Rscript

---
+++ inst/Rscript/test-limma2gsea.bash
@@ -0,0 +1,9 @@
+## basic
+./limma2gsea.Rscript -infile ../extdata/test-limma2gsea.txt -chiptype RAT230_2
+
+./limma2gsea.Rscript -infile ../extdata/test-limma2gsea.txt -chiptype RAT230_2 -stat t
+
+./limma2gsea.Rscript -infile ../extdata/test-limma2gsea.txt -chiptype RAT230_2 -stat logFC
+
+./limma2gsea.Rscript -infile ../extdata/test-limma2gsea.txt -chiptype RAT230_2 -stat logFC -orthologue
+

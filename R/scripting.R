b1da5859766baca05b914085115c3f243d2dd826
zhangj83 <jitao_david.zhang@roche.com>
2012-06-07 18:05:15 +0200

    changing the wordy name isInteractiveDebugging to isIntDebugging

--- R/scripting.R
+++ R/scripting.R
@@ -1,4 +1,4 @@
-isInteractiveDebugging <- function() isDebugging() || interactive()
+isIntDebugging <- function() isDebugging() || interactive()
 isDebugging <- function() Sys.getenv("RIBIOS_SCRIPT_DEBUG") != ""
 setDebug <- function() Sys.setenv(RIBIOS_SCRIPT_DEBUG="TRUE")
 unsetDebug <- function() Sys.unsetenv("RIBIOS_SCRIPT_DEBUG")

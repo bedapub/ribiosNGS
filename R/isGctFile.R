abb6e9e4c469c700f306f3aa636eda56714910d6
zhangj83 <jitao_david.zhang@roche.com>
2012-06-22 14:09:17 +0200

    making GCT file testing less strict (against empty spaces)

--- R/isGctFile.R
+++ R/isGctFile.R
@@ -1,7 +1,7 @@
 isGctFile <- function(file) {
   headers <- readLines(file, n=3)
-  isFL <- headers[[1]]=="#1.2"
-  isSL <- grepl("^[0-9]*\\s[0-9]*$", headers[[2]])
+  isFL <- grepl("^#1.2", headers[[1]])
+  isSL <- grepl("^[0-9]*\\s[0-9]*\\s*$", headers[[2]])
   isTL <- grepl("^NAME\\sDescription\\s.*", headers[[3]], ignore.case=TRUE)
   return(isFL && isSL && isTL)
 }

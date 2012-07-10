c836c299be4fa4989bdd2ba26494a38b56b0441c
zhangj83 <jitao_david.zhang@roche.com>
2012-07-10 14:38:30 +0200

    command-line R script to transform matrix to GCT format

---
+++ R/write_gct.R
@@ -0,0 +1,22 @@
+write_gct <- function(matrix, file=stdout(), feat.name, feat.desc) {
+  if(missing(feat.name)) {
+    feat.name <- rownames(matrix)
+    if(is.null(feat.name)) {
+      warning("'matrix' has NULL as row names, therefore integer indices are used.\n  Consider specifying 'feat.name' instead\n")
+      feat.name <- 1:nrow(matrix)
+    }
+  } else {
+    stopifnot(length(feat.name)==nrow(matrix))
+  }
+  if(missing(feat.desc) || is.null(feat.desc)) {
+    feat.desc <- ""
+  } 
+  prefix <- paste("#1.2", "\n", nrow(matrix), "\t", ncol(matrix), 
+                  sep = "")
+  writeLines(prefix, file)
+  df <- data.frame(NAME=feat.name, Description=feat.desc)
+  df <- cbind(df, matrix)
+  suppressWarnings(write.table(df, file=file, append=TRUE,
+                               quote=FALSE, sep="\t",
+                               row.names=FALSE, col.names=TRUE, na=""))
+}

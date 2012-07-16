59e9b05eaefb5f365132fb4288ac1bd72403a96d
zhangj83 <jitao_david.zhang@roche.com>
2012-07-16 15:16:23 +0200

    add builgGSEAcomm command to build command-line executable bash scripts for GSEA projects

---
+++ R/buildGSEAcomm.R
@@ -0,0 +1,79 @@
+## complication: any parameter cannot contain minus
+## gseaLnks: makes links when necessary
+gseaLns <- function(filename, pattern="file", fileext=".rnk") {
+  if(grepl("-", filename)) {
+    temp <- tempfile(tmpdir="/tmp", pattern=pattern, fileext=fileext)
+    makeLn <- paste("ln -s", filename, temp)
+    rmLn <- paste("unlink", temp)
+  } else {
+    temp <- filename
+    makeLn <- ""
+    rmLn <- ""
+  }
+  return(c(file=temp, makeLn=makeLn, rmLn=rmLn))
+}
+
+buildGSEAcomm <- function(rnkFiles,
+                          gmtFile=DEFAULT_GMT,
+                          chipFile=DEFAULT_CHIP,
+                          nperm=1000L,
+                          collapse=FALSE,
+                          plotTopX=25,
+                          outdir="./",
+                          addShebang=TRUE) {
+  stopifnot(all(file.exists(rnkFiles)))
+  rnkFiles <- normalizePath(rnkFiles)
+  if(!file.exists(outdir))
+    dir.create(outdir)
+  outdir <- normalizePath(outdir)
+  mkOutDir <- paste("mkdir -p", outdir,"\n", sep=" ")
+
+  stopifnot(file.exists(gmtFile))
+  if(!file.exists(chipFile))
+    stop("Specified chip not found. Please call 'gseaChiptypes()' to find out supported chiptypes\n")
+  
+  gsea.command.template <- paste(c(ifelse(addShebang, "#!/bin/bash\n", ""),
+                                   mkOutDir,
+                                   "%s\n%s\n",
+                                   "(time ", JAVA_BIN, "-Xmx2500m",
+                                   "-classpath ", GSEA_JAR, " xtools.gsea.GseaPreranked",
+                                   "-gmx %s",
+                                   "-chip %s",
+                                   "-rnk %s -rpt_label %s -nperm %d -collapse %s",
+                                   "-mode Max_probe -norm meandiv -include_only_symbols true",
+                                   "-scoring_scheme weighted -make_sets true -plot_top_x %d",
+                                   "-rnd_seed timestamp -set_min 5 -set_max 500", 
+                                   "-zip_report false -gui false -out %s > %s) >& %s\n",
+                                   "%s\n%s\n"),
+                                 collapse=" ")
+  gsea.commands <- vector("character", length(rnkFiles))
+  for(i in seq(along=rnkFiles)) {
+    outfile <- file.path(outdir, paste(basename(rnkFiles[i]), ".out", sep=""))
+    errfile <- file.path(outdir, paste(basename(rnkFiles[i]), ".err",sep=""))
+    rf <- rnkFiles[i]
+    label.base <- paste("runGSEA",
+                     gsub("(.*)\\..*", "\\1", basename(rf)), sep="_")
+    label <- gsub("-", ".", label.base)
+
+    rfLns <- gseaLns(rf, pattern="file", fileext=".rnk")
+    rfTemp <- rfLns[1]; linkRf <- rfLns[2]; delRf <- rfLns[3]
+    ofLns <- gseaLns(outdir, pattern="dir", fileext="")
+    ofTemp <- ofLns[1]; linkOf <- ofLns[2]; delOf <- ofLns[3]
+    
+    gsea.commands[i] <- sprintf(gsea.command.template,
+                                linkRf, linkOf,
+                                gmtFile,
+                                chipFile,
+                                rfTemp,
+                                label,
+                                as.integer(nperm),
+                                ifelse(collapse, "true", "false"),
+                                as.integer(plotTopX),
+                                ofTemp,
+                                outfile,
+                                errfile,
+                                delRf, delOf)
+  }
+  return(gsea.commands)
+}
+
---
+++ R/utils.R
@@ -0,0 +1,10 @@
+GSEA_DATA_DIR <- "/DATA/bi/httpd_8080/htdoc/apps/gsea"
+GSEA_ANNOTATION_DIR <- file.path(GSEA_DATA_DIR, "annotations")
+GSEA_GENESET_DIR <- file.path(GSEA_DATA_DIR, "genesets")
+JAVA_BIN <- "/SOFT/bi/apps/java/c/bin/java"
+GSEA_JAR <- "/SOFT/bi/apps/gsea/GSEA2-2.02/my_gsea2.jar"
+DEFAULT_GMT <- file.path(GSEA_GENESET_DIR,
+                         "path.ronet.roche.symbols.gmt")
+DEFAULT_CHIP <- file.path(GSEA_ANNOTATION_DIR,
+                          "GENE_SYMBOL.chip")
+                          

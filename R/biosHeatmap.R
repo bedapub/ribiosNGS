2ba25c4b471b1ffafbe99517d94b3fcbf7c5097a
zhangj83 <jitao_david.zhang@roche.com>
2012-09-10 16:55:04 +0200

    aux functions moved to utils.R

--- R/biosHeatmap.R
+++ R/biosHeatmap.R
@@ -26,24 +26,6 @@ strbreak <- function (x, width = getOption("width"), exdent = 2, collapse = "\n"
 }
 
 
-bound <- function(x,low,high) {
-  pmin(pmax(x, low),high)
-}
-boundNorm <- function(x, low = min(x), high = max(x)) {
-  x <- (x - low)/(high - low)
-  x
-}
-isInvalid <- function (x) 
-{
-    if (missing(x) || is.null(x) || length(x) == 0) 
-        return(TRUE)
-    if (is.list(x)) 
-        return(all(sapply(x, isInvalid)))
-    else if (is.vector(x)) 
-        return(all(is.na(x)))
-    else return(FALSE)
-}
-
 guessWH <- function(nrow, ncol,
                     rownames, colnames,
                     cexRow, cexCol,

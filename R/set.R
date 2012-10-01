06a16d8b03b0ddf6f9eba10f33f198f0a23e2b41
zhangj83 <jitao_david.zhang@roche.com>
2012-10-01 10:59:10 +0200

    add multiple-set operations

---
+++ R/set.R
@@ -0,0 +1,15 @@
+mset <- function(func, ...) {
+  li <- list(...)
+  if(length(li)==1L) li <- li[[1L]]
+  
+  stopifnot(length(li)>=2L)
+  res <- do.call(func, list(li[[1L]], li[[2L]]))
+  if(length(li)>2L)
+    for(i in 3L:length(li))
+      res <- do.call(func, list(res, li[[i]]))
+  res
+}
+munion <- function(...) mset(func=union, ...)
+mintersect <- function(...) mset(func=intersect, ...)
+msetdiff <- function(...) mset(func=setdiff, ...)
+

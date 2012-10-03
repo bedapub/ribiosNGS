80a7d326972a1f4c119c74a3a47e2899bf95e1e2
zhangj83 <jitao_david.zhang@roche.com>
2012-10-03 17:29:07 +0200

    midentical works now with older R versions

--- R/midentical.R
+++ R/midentical.R
@@ -4,19 +4,21 @@ midentical <- function(..., num.eq=TRUE, single.NA=TRUE, attrib.as.set=TRUE,
   if(length(li)==1L) li <- li[[1L]]
   
   stopifnot(length(li)>=2L)
-  res <- do.call(identical, list(li[[1L]], li[[2L]],
-                                 num.eq=num.eq, single.NA=single.NA,
-                                 attrib.as.set=attrib.as.set,
-                                 ignore.bytecode=ignore.bytecode,
-                                 ignore.environment=ignore.environment))
+  identical.formals <- names(formals(identical))
+  call.list <- list(x=li[[1L]], y=li[[2L]],
+                    num.eq=num.eq, single.NA=single.NA,
+                    attrib.as.set=attrib.as.set,
+                    ignore.bytecode=ignore.bytecode,
+                    ignore.environment=ignore.environment)[identical.formals]
+  res <- do.call(identical, call.list)
   if(length(li)>2L)
     for(i in 2L:(length(li)-1L)) {
-      res <- res && do.call(identical,
-                            list(li[[i]],li[[i+1]],
-                                 num.eq=num.eq, single.NA=single.NA,
-                                 attrib.as.set=attrib.as.set,
-                                 ignore.bytecode=ignore.bytecode,
-                                 ignore.environment=ignore.environment ))
+      call.list <- list(x=li[[i]], y=li[[i+1L]],
+                        num.eq=num.eq, single.NA=single.NA,
+                        attrib.as.set=attrib.as.set,
+                        ignore.bytecode=ignore.bytecode,
+                        ignore.environment=ignore.environment)[identical.formals]
+      res <- res && do.call(identical, call.list)
       if(!res) return(FALSE)
     }
   return(res)

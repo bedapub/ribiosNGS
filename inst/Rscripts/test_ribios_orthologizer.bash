91fb85618ed4ea0d89fd17309b71e688d7e9f0bc
zhangj83 <jitao_david.zhang@roche.com>
2012-10-03 10:52:53 +0200

    make Rscript as a variable in the test_ribios_orthologizer.bash

--- inst/Rscripts/test_ribios_orthologizer.bash
+++ inst/Rscripts/test_ribios_orthologizer.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+RSCRIPT=/SOFT/bi/apps/R/bdeRscript
 PDIR=/SOFT/bi/apps/ribios/ribiosAnnotation/inst/Rscripts
 PROG=${PDIR}/ribios_orthologizer.Rscript
 INDIR=${PDIR}/../extdata/ribios_orthologizer_testdata/
@@ -7,27 +8,27 @@ OUTDIR=${INDIR}
 
 echo "====Examples of ribios_annotate.Rscript===="
 echo "==[see help message and usage]=="
-${PROG}
+${RSCRIPT} ${PROG}
 
 echo "==[list all supported chiptypes]=="
-${PROG} -chiptype
+${RSCRIPT} ${PROG} -chiptype
 
 echo
 echo "==[Orthologize GeneID]=="
-./ribios_orthologizer.Rscript -infile ${INDIR}/rat-geneid.txt -chiptype GeneID
+${RSCRIPT} ./ribios_orthologizer.Rscript -infile ${INDIR}/rat-geneid.txt -chiptype GeneID
 
 echo
 echo "==[Orthologize GeneSymbol, do NOT remove unmapped ones]=="
-./ribios_orthologizer.Rscript -infile ${INDIR}/rat-genesymbol.txt -chiptype genesymbol
+${RSCRIPT} ./ribios_orthologizer.Rscript -infile ${INDIR}/rat-genesymbol.txt -chiptype genesymbol
 
 echo
 echo "==[Orthologize GeneSymbol, remove unmapped ones]=="
-./ribios_orthologizer.Rscript -infile ${INDIR}/rat-genesymbol.txt -chiptype genesymbol -delUnmapped
+${RSCRIPT} ./ribios_orthologizer.Rscript -infile ${INDIR}/rat-genesymbol.txt -chiptype genesymbol -delUnmapped
 
 echo
 echo "==[Orthologize probesets, given the chiptype]=="
-./ribios_orthologizer.Rscript -infile ${INDIR}/rat-probesets.txt -chiptype RAT230_2
+${RSCRIPT} ./ribios_orthologizer.Rscript -infile ${INDIR}/rat-probesets.txt -chiptype RAT230_2
 
 echo
 echo "==[Orthologize probesets, without chiptype (SLOW)]=="
-./ribios_orthologizer.Rscript -infile ${INDIR}/rat-probesets.txt
+${RSCRIPT} ./ribios_orthologizer.Rscript -infile ${INDIR}/rat-probesets.txt

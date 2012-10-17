a9cf25b35412590d0512c5f29656ea2883b340b2
zhangj83 <jitao_david.zhang@roche.com>
2012-10-17 18:03:57 +0200

    add new function haltifnot

---
+++ R/haltifnot.R
@@ -0,0 +1,14 @@
+haltifnot <- function(..., msg="Error undefined. Please contact developer") {
+  n <- length(ll <- list(...))
+  if (n == 0L)
+    return(invisible())
+  mc <- match.call()
+  for (i in 1L:n) if (!(is.logical(r <- ll[[i]]) && !any(is.na(r)) &&
+                        all(r))) {
+    ch <- deparse(mc[[i + 1]], width.cutoff = 60L)
+    if (length(ch) > 1L)
+      ch <- paste(ch[1L], "....")
+    stop(msg, call. = FALSE)
+  }
+  invisible()
+}

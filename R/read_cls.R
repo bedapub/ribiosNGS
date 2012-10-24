87680a64d627d8f5ddca73de21bc0d243d080d5f
zhangj83 <jitao_david.zhang@roche.com>
2012-10-24 15:52:40 +0200

    move readCls to read_cls in ribiosIO

---
+++ R/read_cls.R
@@ -0,0 +1,21 @@
+read_cls <- function (cls.file) {
+    lns <- readLines(cls.file)
+    stopifnot(length(lns) == 3)
+    indims <- as.integer(strsplit(lns[1L], "\\s")[[1]])
+    if (!(length(indims) == 3L & indims[3L] == 1L)) {
+        stop("The first line of cls file is mal-formated.\n")
+    }
+    slevels <- strsplit(lns[2L], "\\s")[[1L]]
+    if (!(identical(slevels[1], "#") & length(slevels) == indims[2L] + 
+        1L)) {
+        stop("The label line of cls file has fewer classes than indicated.\n")
+    }
+    slevels <- slevels[-1L]
+    fn <- as.integer(strsplit(lns[3], "\\s")[[1]])
+    if (length(fn) != indims[1L]) {
+        stop("In cls file there are number-indexed samples than indicated in the header line.\n ")
+    }
+    sf <- as.factor(fn)
+    levels(sf) <- slevels
+    return(sf)
+}

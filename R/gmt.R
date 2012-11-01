3feb59e8b4939c698a07473238c34200297eeaba
zhangj83 <jitao_david.zhang@roche.com>
2012-11-01 10:34:44 +0100

    add functions for limma and gmt files

---
+++ R/gmt.R
@@ -0,0 +1,13 @@
+filterGmtByGeneCount <- function(gmtlist,min=5L, max=1000L) {
+  gmt.count <- sapply(gmtlist, function(x) uniqueLength(x$genes))
+  gmt.keep <- gmt.count >= min & gmt.count <= max
+  gmtlist[gmt.keep]
+}
+
+gmtIndices <- function(gmtlist, symbols) {
+  gs.names <- sapply(gmtlist, function(x) x$name)
+  gs.genes <- lapply(gmtlist, function(x) x$genes)
+  gs.indices <- lapply(gs.genes, function(x) which(symbols %in% x))
+  names(gs.indices) <- gs.names
+  gs.indices
+}

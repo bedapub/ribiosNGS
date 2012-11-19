3f6ae50e7f60fcee7026387831f6bb70d8e7af17
zhangj83 <jitao_david.zhang@roche.com>
2012-11-19 18:42:27 +0100

    the scripts are not dependent on the S4 class anymore, therefore much faster

--- inst/Rscript/test-keepMaxMeanProbes.bash
+++ inst/Rscript/test-keepMaxMeanProbes.bash
@@ -1,1 +1,3 @@
 ./keepMaxMeanProbes.Rscript -infile ../extdata/testKeepMaxMeanProbes.txt
+
+./keepMaxMeanProbes.Rscript -infile ../extdata/testKeepMaxMeanProbes-chip.gct -chipfile ../extdata/test.chip

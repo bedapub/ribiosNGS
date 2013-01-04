ed402e7551d69216b09efd8e90b3229ff941135a
zhangj83 <jitao_david.zhang@roche.com>
2013-01-04 10:52:08 +0100

    argparse.h

---
+++ src/argparse.h
@@ -0,0 +1,14 @@
+#ifndef _argparse_h_
+#define _argparse_h_
+
+#include "Rinternals.h"
+
+extern char* rst2c (SEXP);
+extern char* rstVec2c (SEXP,int);
+extern void usagef (int);
+extern SEXP rarg_parse (SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP rarg_getPos(SEXP, SEXP);
+extern SEXP rarg_get(SEXP);
+extern SEXP rarg_present(SEXP);
+
+#endif

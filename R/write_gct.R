9d08ae39e6a03677e0e9944c7c7ed3de9033f6b5
zhangj83 <jitao_david.zhang@roche.com>
2013-02-21 07:44:17 +0100

    version 1.0-22: write_gct handles zero-row matrix

--- R/write_gct.R
+++ R/write_gct.R
@@ -16,10 +16,21 @@ write_gct <- function(matrix, file=stdout(), feat.name, feat.desc) {
   if(identical(file, "")) file <- stdout()
   prefix <- paste("#1.2", "\n", nrow(matrix), "\t", ncol(matrix), 
                   sep = "")
-  writeLines(prefix, file)
-  df <- data.frame(NAME=feat.name, Description=feat.desc)
-  df <- cbind(df, matrix)
-  suppressWarnings(write.table(df, file=file, append=TRUE,
-                               quote=FALSE, sep="\t",
-                               row.names=FALSE, col.names=TRUE, na=""))
+  if(nrow(matrix)>0) {
+    writeLines(prefix, file)
+    df <- data.frame(NAME=feat.name, Description=feat.desc)
+    df <- cbind(df, matrix)
+    suppressWarnings(write.table(df, file=file, append=TRUE,
+                                 quote=FALSE, sep="\t",
+                                 row.names=FALSE, col.names=TRUE, na=""))
+  } else {
+    if(is.null(colnames(matrix))) {
+      warning("'matrix' is empty and has NULL as col names. Integer indices are used.\n")
+      colnames(matrix) <- 1:ncol(matrix)
+    }
+    df <- paste(c("NAME", "Description", colnames(matrix)),
+                collapse="\t")
+    prefix <- paste(prefix, df, sep="\n")
+    writeLines(prefix, file)
+  }
 }

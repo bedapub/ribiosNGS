e4f13809ff6de9bdada6b14787c62b35a804a0c1
zhangj83 <jitao_david.zhang@roche.com>
2013-02-28 10:54:30 +0100

    Bug fix in read_pheno and read_pheno_factor: hash is now supported as comment

--- R/read_pheno.R
+++ R/read_pheno.R
@@ -6,7 +6,7 @@ read_pheno <- function(file) {
     tbl <- data.frame(Array=seq(along=sclass),
                       Class=sclass)
   } else {
-    tbl <- read.csv(txt, sep="\t", header=TRUE)
+    tbl <- read.csv(txt, sep="\t", header=TRUE, comment.char="#")
   }
   close(txt)
   return(tbl)
@@ -18,7 +18,7 @@ read_pheno_factor <- function(file) {
   if(length(lns)==3 && grepl("^#", lns[2])) {
     sclass <- read_cls(txt)
   } else {
-    tbl <- read.csv(txt, sep="\t", header=TRUE)
+    tbl <- read.csv(txt, sep="\t", header=TRUE, comment.char="#")
     if(ncol(tbl)==1) {
       sclass <- factor(tbl[,1L], levels=unique(tbl[,1L]))
     } else {

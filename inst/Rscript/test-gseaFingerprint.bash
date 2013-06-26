a1daedfd9f3794956863ec243b34fc26a83d0ba9
zhangj83 <jitao_david.zhang@roche.com>
2013-06-26 08:46:12 +0200

    gseaFingerprint.Rscript supports now sort by maxAbsStat and meanStat

--- inst/Rscript/test-gseaFingerprint.bash
+++ inst/Rscript/test-gseaFingerprint.bash
@@ -1,1 +1,3 @@
 ./gseaFingerprint.Rscript -infile ../extdata/test-gseaFingerprint.txt -stat q
+./gseaFingerprint.Rscript -infile ../extdata/test-gseaFingerprint.txt -stat nes -sortBy maxAbsStat
+./gseaFingerprint.Rscript -infile ../extdata/test-gseaFingerprint.txt -stat nes -sortBy meanStat

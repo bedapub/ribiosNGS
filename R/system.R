58d963db7c27d07993d4ccb6803d2ba45d3782d0
zhangj83 <jitao_david.zhang@roche.com>
2013-07-24 15:21:50 +0200

    at qsystem to run sysmtem commands quietly, and add rmat to remove files at a given time

--- R/system.R
+++ R/system.R
@@ -3,16 +3,16 @@ qsystem <- function(command) {
 }
 
 rmat <- function(..., days=NULL, hours=NULL, minutes=NULL, dry=TRUE) {
-  seconds=0.0;
+  count=0.0;
   if(!is.null(days) && !is.na(nday <- as.numeric(days)))
-    seconds=seconds + 86400 * nday
+    count=count + 1440 * nday
   if(!is.null(hours) && !is.na(nhour <- as.numeric(hours)))
-    seconds=seconds + 3600 * nhour
-  if(!is.null(minutes) && !is.na(nday <- as.numeric(minutes)))
-    seconds=seconds + 60 * nday
+    count=count + 60 * nhour
+  if(!is.null(minutes) && !is.na(nmin <- as.numeric(minutes)))
+    count=count + nmin
   files <- paste(as.character(unlist(list(...))), collapse=" ")
-  comm <- sprintf("echo rm -rf %s | at now + %.0f seconds",
-                  files, seconds)
+  comm <- sprintf("echo rm -rf %s | at now + %.0f minutes",
+                  files, count)
   if(dry) {
     return(comm)
   } else {

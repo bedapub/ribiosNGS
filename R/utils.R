d7a5f3e967d7a3beee4794ea6e29cba502332ad7
zhangj83 <jitao_david.zhang@roche.com>
2013-07-24 14:43:13 +0200

    add cgiPairParam (not tested yet); fix the problem of fileext parameter in temp

--- R/utils.R
+++ R/utils.R
@@ -2,7 +2,13 @@ RIBIOS_CGI_TMP_DIR <- "/DATA/bi/httpd_8080/htdoc/sawitmp/"
 RIBIOS_CGI_TMP_URL <- "http://bioinfo.bas.roche.com:8080/sawitmp/"
 
 tmpWebFile <- function(pattern="file", fileext="", tmpdir=RIBIOS_CGI_TMP_DIR) {
-  tempfile(pattern=pattern, tmpdir=tmpdir, fileext=fileext)
+  ## 'fileext' was a feature (at latest) available from R-2.16.0
+  if("fileext" %in% names(as.list(args(tempfile)))) {
+    return(tempfile(pattern=pattern, tmpdir=tmpdir, fileext=fileext))
+  } else {
+    return(paste(tempfile(pattern=pattern, tmpdir=tmpdir),
+                 fileext, sep=""))
+  }
 }
 tmpWebURL <- function(tmpFile, tmpurl=RIBIOS_CGI_TMP_URL) {
   paste(tmpurl,basename(tmpFile), sep="")

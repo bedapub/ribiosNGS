1eb0134d62ca607d7400bb7dfb6f6bd7910bacd1
zhangj83 <jitao_david.zhang@roche.com>
2013-07-24 15:21:00 +0200

    add closeFileDevice; add dpi option to openFileDevice

--- R/openFileDevice.R
+++ R/openFileDevice.R
@@ -1,18 +1,21 @@
-openFileDevice <- function(filename, width=7, height=7) {
-  RES_INCH <- 300L
+openFileDevice <- function(filename, width=7, height=7, dpi=300L) {
   filetype <- extname(filename)
   if(identical(filetype,"pdf")) {
-    pdf(filename, width=width, height=height)
+    pdf(filename, width=width, height=height, useDingbats=FALSE)
   } else if (identical(filetype, "png")) {
-    png(filename, width=width, height=height, units="in", type="cairo", res=RES_INCH)
+    png(filename, width=width, height=height, units="in", type="cairo", res=dpi)
   } else if(identical(filetype,"tiff") || identical(filetype, "tif")) {
-    tiff(filename, width=width, height=height, units="in", res=RES_INCH)
+    tiff(filename, width=width, height=height, units="in", res=dpi)
   } else if(identical(filetype, "bmp")) {
-    bmp(filename, width=width, height=height, units="in", res=RES_INCH)
+    bmp(filename, width=width, height=height, units="in", res=dpi)
   } else if(identical(filetype, "jpeg") || identical(filetype, "jpg")) {
-    jpeg(filename, width=width, height=height, units="in", res=RES_INCH, quality=90)
+    jpeg(filename, width=width, height=height, units="in", res=dpi, quality=90)
   } else {
     warning("Unrecognized output format. PDF will be used")
-    pdf(filename, width=width, height=height)
+    pdf(filename, width=width, height=height, useDingbats=FALSE)
   }
+}
+
+closeFileDevice <- function() {
+  invisible(dev.off())
 }

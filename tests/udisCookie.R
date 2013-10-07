90e601873730e182f962f144f1e930c62f07e417
zhangj83 <jitao_david.zhang@roche.com>
2013-10-07 11:35:01 +0200

    removing plain-text password of Yuan's account

--- tests/udisCookie.R
+++ tests/udisCookie.R
@@ -1,9 +1,5 @@
 library(ribiosUDIS)
 
-##cookie <- bakeCookie(appname="udis_expression", user="wangy78", apppassword="klee")
-##exampleURL <- "http://udisdev.roche.com/udiscgiqa/expressionData_cgi?query=signals&studyidexternal=NCS_tissue_rat&studydomain=undefined&format=gct&outdest=browser"
-##myQuery <- queryUrl(exampleURL, cookie)
-
 system.time(test <- getUDISexpression(id="GSE20986"))
 stopifnot(is(test, "ExpressionSet"))
 

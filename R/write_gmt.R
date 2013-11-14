b48241ca0f3c1a2bd9bfeb2a29beb2515eb2eb72
zhangj83 <jitao_david.zhang@roche.com>
2013-11-14 09:33:15 +0100

    version 1.0-27: write_gmt supports simple list as input

--- R/write_gmt.R
+++ R/write_gmt.R
@@ -1,3 +1,17 @@
-write_gmt <- function(gmt, file) {
-  invisible(.Call("write_gmt", gmt, as.character(file)))
+write_gmt <- function(gmt, file, description=NULL) {
+  isGeneList <- all(sapply(gmt,function(x) class(x)!="list"))
+  isGmtList <- all(sapply(gmt, function(x) class(x)=="list" & length(x)==3))
+  if(isGeneList) {
+    gsets <- names(gmt)
+    if(length(description)!=length(gmt))
+      description <- rep(description, length(gmt)/length(description))
+    gout <- lapply(seq(along=gmt),function(i)
+                   list(name=gsets[i], description=description[i], genes=as.character(gmt[[i]])))
+  } else if (isGmtList) {
+    gout <- gmt
+  } else {
+    stop("Unrecognized GMT list: it is either a list of gene symbols, or a list of a three-element list with name, description, and genes")
+  }
+    
+  invisible(.Call("write_gmt", gout, as.character(file)))
 }

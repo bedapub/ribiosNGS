d8c4a076513be77c81dce6ff53f53e3af7f55f83
zhangj83 <jitao_david.zhang@roche.com>
2014-01-18 11:40:03 +0100

    add read_rocheNGS_exprs function to read a single Roche NGS expression file into data.frame

---
+++ src/read_rocheNGS_exprs.c
@@ -0,0 +1,124 @@
+#include <math.h>
+#include <log.h>
+#include <format.h>
+#include <linestream.h>
+
+#include "ribios_io.h"
+
+SEXP read_rocheNGS_exprs (SEXP filename) {
+  LineStream ls;
+  char* line;
+  const int MAND_NCOL=6; // the first column is the row name, and column 2-6 are mandatory
+  int add_ncol=0;
+  Texta it;
+  Texta rnames=textCreate(128);
+  Array mrpkms=arrayCreate(128, double);
+  Array mreads=arrayCreate(128, int);
+  Array srpkms=arrayCreate(128, double);
+  Array sreads=arrayCreate(128, int);
+  Array mprop=arrayCreate(128, double);
+  Array annos=arrayCreate(128, Texta);
+  Texta anno=NULL; // must have a NULL assigned; otherwise textCreateClear leads to memory error
+  Stringa str=stringCreate(8);
+
+  SEXP R_rnames, R_mrpkms, R_mreads, R_srpkms, R_sreads, R_mprop,R_res;
+  SEXP R_colnames, R_class;
+  
+  int nprot=0;
+  int i=0;
+  int j=0;
+  int nrow=0;
+  const char* fn=CHAR(STRING_ELT(filename, 0));
+  ls = ls_createFromFile(strdup(fn));
+
+  while(line = ls_nextLine(ls)) {
+    it = textFieldtokP(line, "\t");
+    if(arrayMax(it)<MAND_NCOL)
+      error("Input file must contain no less than %d columns", MAND_NCOL);
+
+    textAdd(rnames, textItem(it, 0));
+    array(mrpkms, arrayMax(mrpkms), double)=atof(textItem(it, 1));
+    array(mreads, arrayMax(mreads), int)=atoi(textItem(it, 2));
+    array(srpkms, arrayMax(srpkms), double)=atof(textItem(it, 3));
+    array(sreads, arrayMax(sreads), int)=atoi(textItem(it, 4));
+    array(mprop, arrayMax(mprop), double)=atof(textItem(it, 5));
+
+    add_ncol = max(arrayMax(it)-MAND_NCOL, add_ncol);
+    textCreateClear(anno, arrayMax(it)-MAND_NCOL);
+    for(i=MAND_NCOL; i<arrayMax(it);  ++i) {
+      textAdd(anno, textItem(it, i));
+    }
+    array(annos, arrayMax(annos), Texta)=textClone(anno);
+    nrow++;
+  }
+
+  R_rnames=PROTECT(allocVector(STRSXP, nrow)); nprot++;
+  R_mrpkms=PROTECT(allocVector(REALSXP, nrow)); nprot++;
+  R_mreads=PROTECT(allocVector(INTSXP, nrow)); nprot++;
+  R_srpkms=PROTECT(allocVector(REALSXP, nrow)); nprot++;
+  R_sreads=PROTECT(allocVector(INTSXP, nrow)); nprot++;
+  R_mprop=PROTECT(allocVector(REALSXP, nrow)); nprot++;
+
+  for(i=0; i<nrow; ++i) {
+    SET_STRING_ELT(R_rnames, i, mkChar(textItem(rnames, i)));
+    REAL(R_mrpkms)[i]=arru(mrpkms, i, double);
+    INTEGER(R_mreads)[i]=arru(mreads, i, int);
+    REAL(R_srpkms)[i]=arru(srpkms, i, double);
+    INTEGER(R_sreads)[i]=arru(sreads, i, int);
+    REAL(R_mprop)[i]=arru(mprop, i, double);
+  }
+
+  R_res=PROTECT(allocVector(VECSXP, MAND_NCOL+add_ncol-1)); nprot++;
+  SET_VECTOR_ELT(R_res, 0, R_mrpkms);
+  SET_VECTOR_ELT(R_res, 1, R_mreads);
+  SET_VECTOR_ELT(R_res, 2, R_srpkms);
+  SET_VECTOR_ELT(R_res, 3, R_sreads);
+  SET_VECTOR_ELT(R_res, 4, R_mprop);
+  for(i=0; i<add_ncol; ++i) {
+    SEXP R_anno=NULL;
+    R_anno=PROTECT(allocVector(STRSXP, nrow));
+    for(j=0; j<nrow; ++j) {
+      anno=array(annos, j, Texta);
+      if(arrayMax(anno)>i) {
+         SET_STRING_ELT(R_anno, j, mkChar(textItem(anno, i)));
+      } else {
+         SET_STRING_ELT(R_anno, j, R_NaString);
+      }
+    }
+    SET_VECTOR_ELT(R_res, i+MAND_NCOL-1, R_anno); // -1 because the first column is row name
+    UNPROTECT(1);
+  }
+
+  PROTECT(R_colnames=allocVector(STRSXP, MAND_NCOL+add_ncol-1)); nprot++;
+  PROTECT(R_class=allocVector(STRSXP, 1)); nprot++;
+  SET_STRING_ELT(R_colnames, 0, mkChar("RPKM_MultiMap"));
+  SET_STRING_ELT(R_colnames, 1, mkChar("ReadCount_MultiMap"));
+  SET_STRING_ELT(R_colnames, 2, mkChar("RPKM_UniqMap"));
+  SET_STRING_ELT(R_colnames, 3, mkChar("ReadCount_UniqMap"));
+  SET_STRING_ELT(R_colnames, 4, mkChar("MultiProp"));
+  for(i=0; i<add_ncol; ++i) {
+    stringPrintf(str, "Annotation%d", i+1);
+    SET_STRING_ELT(R_colnames, i+MAND_NCOL-1,
+                   mkChar(string(str)));
+  }
+  SET_STRING_ELT(R_class, 0, mkChar("data.frame"));
+  setAttrib(R_res, install("names"), R_colnames);
+  setAttrib(R_res, install("row.names"), R_rnames);
+  setAttrib(R_res, install("class"), R_class);
+
+  for(i=0; i<nrow; ++i) {
+    textDestroy(array(annos, i, Texta));
+  }
+  arrayDestroy(annos);
+  arrayDestroy(rnames);
+  arrayDestroy(mrpkms);
+  arrayDestroy(mreads);
+  arrayDestroy(srpkms);
+  arrayDestroy(sreads);
+  arrayDestroy(mprop);
+  stringDestroy(str);
+
+  ls_destroy(ls);
+  UNPROTECT(nprot);
+  return(R_res);
+}
---
+++ tests/test_read_rocheNGS_exprs.R
@@ -0,0 +1,12 @@
+library(ribiosIO)
+
+ngsFile <- system.file("extdata/NGS_exprs/file1.expression", package="ribiosIO")
+ngs <- read_rocheNGS_exprs(ngsFile)
+
+## mandatory columns
+stopifnot(identical(colnames(ngs)[1:5],
+                    c("RPKM_MultiMap", "ReadCount_MultiMap", "RPKM_UniqMap", "ReadCount_UniqMap", "MultiProp")))
+
+## rownames
+stopifnot(identical(rownames(ngs)[1:3],
+                    c("ENSG00000214105_17", "ENSG00000234128_7", "ENSG00000172653_17")))

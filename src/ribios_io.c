7b4108dd0b8d35d48ff52ae0303d0e1211c94a16
zhangj83 <jitao_david.zhang@roche.com>
2014-01-18 11:48:27 +0100

    register read_rocheNGS_exprs

--- src/ribios_io.c
+++ src/ribios_io.c
@@ -2,8 +2,10 @@
 #include <R_ext/Rdynload.h>
 
 static const R_CallMethodDef callMethods[] = {
-  /* read_gct.c */
   CALLMETHOD_DEF(read_gct, 3),
+  CALLMETHOD_DEF(write_gmt, 2),
+  CALLMETHOD_DEF(read_chip, 1),
+  CALLMETHOD_DEF(read_rocheNGS_exprs, 1),
 
   {NULL, NULL, 0}
 };
--- src/ribios_io.h
+++ src/ribios_io.h
@@ -3,11 +3,15 @@
 
 #include <Rinternals.h>
 
-#define CALLMETHOD_DEF(fun, numArgs) {#fun, (DL_FUNC) &fun, numArgs}
+#define CALLMETHOD_DEF(fun, numArgs) {(fun), (DL_FUNC) &(fun), (numArgs)}
 
 // public functions
 SEXP read_gct (SEXP filename, SEXP pchr, SEXP keepdesc);
-SEXP write_gct (SEXP list, SEXP filename);
+SEXP write_gmt (SEXP list, SEXP filename);
 SEXP read_chip (SEXP filename);
+SEXP read_rocheNGS_exprs (SEXP filename);
+
+// common macros
+#define max(a,b) (((a)>(b))?(a):(b))
 
 #endif

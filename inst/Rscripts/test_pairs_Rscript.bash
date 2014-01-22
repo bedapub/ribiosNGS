a78557dbf91f3ddb92e318005fda2fdae2d65aeb
zhangj83 <jitao_david.zhang@roche.com>
2014-01-22 16:45:06 +0100

    the pairs.Rscript is up-to-date and accepts gct/txt fils

--- inst/Rscripts/test_pairs_Rscript.bash
+++ inst/Rscripts/test_pairs_Rscript.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-
-./pairs.Rscript -infile a_maps.txt -outfile a_maps.png -lm -cor && display a_maps.png
-./pairs.Rscript -infile a_maps.txt -outfile a_maps.png  && display a_maps.png
+./pairs.Rscript -infile ../extdata/test_pairs.txt -outfile test_pairs.png -lm -cor && display test_pairs.png
+./pairs.Rscript -infile ../extdata/test_pairs.txt -outfile test_pairs.tiff -lm -cor -log10 && display test_pairs.tiff
+./pairs.Rscript -infile ../extdata/test_pairs_binary.txt -outfile test_pairs.pdf -lm -corr -log10 && display test_pairs.pdf
+rm test_pairs.pdf test_pairs.png test_pairs.tiff

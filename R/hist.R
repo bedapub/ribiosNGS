c17690dfcdef6b24000326779b23bf8d9c5a2a72
zhangj83 <jitao_david.zhang@roche.com>
2014-04-22 15:49:38 +0200

    add histMat

--- R/hist.R
+++ R/hist.R
@@ -34,3 +34,42 @@ xclipHist <- function(x, xclip=c(0.01, 0.99), breaks=100,
   qHist(x, breaks=xBreaks, xlim=xlim, quantiles=quantiles,
         qlty=qlty, qlwd=qlwd, qcol=qcol, ...)
 }
+
+## histogram of matrix
+histMat <- function(mat,
+                    linesOpt=list(lwd=NULL, col=NULL,lty=NULL, type=NULL, pch=NULL),
+                    main=NULL, xlab=NULL, xlim=NULL,
+                    ...) {
+
+  if(!is.matrix(mat)) {
+    stop("input must be a numeric matrix")
+  }
+  xrange <- symrange(mat, mid=0)
+  mat.dens <- apply(mat, 2, density, na.rm=TRUE)
+  
+  if(missing(main)) 
+    main <- as.character(substitute(mat))
+  if(missing(xlab))
+    xlab <- ""
+  if(missing(xlim))
+    xlim <- xrange
+  
+  oh <- hist(mat, freq=FALSE, xlab=xlab, main=main, xlim=xlim, ...)
+
+  lines.col <- nonNull(linesOpt$col, palette(), ncol(mat))
+  lines.lwd <- nonNull(linesOpt$lwd, 1L, ncol(mat))
+  lines.type <- nonNull(linesOpt$type, "l", ncol(mat))
+  lines.lty <- nonNull(linesOpt$lty, 1L, ncol(mat))
+  lines.pch <- nonNull(linesOpt$pch, 1L, ncol(mat))
+  
+  for(i in 1:ncol(mat)) {
+    lines(mat.dens[[i]], col=lines.col[i], lwd=lines.lwd[i],
+          type=lines.type[i], lty=lines.lty[i], pch=lines.pch[i])
+  }
+  lopt <- list(col=lines.col, lwd=lines.lwd, type=lines.type,
+               lty=lines.lty, pch=lines.pch)
+
+  oh$xlim <- xlim
+  oh$linesOpt <- lopt
+  return(invisible(oh))
+}

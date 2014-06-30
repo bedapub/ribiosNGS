999093c794eaf1cf02c6496cf879bb3e61d3bb4e
zhangj83 <jitao_david.zhang@roche.com>
2014-06-30 15:19:37 +0200

    a faster implementation of longdf2matrix

--- R/matrix2longdf.R
+++ R/matrix2longdf.R
@@ -22,16 +22,31 @@ matrix2longdf <- function(mat,
   return(res)
 }
 
-longdf2matrix <- function(df, row.col=1L, column.col=2L, value.col=3L)  {
+##longdf2matrix <- function(df, row.col=1L, column.col=2L, value.col=3L)  {
+##  sub <- df[, c(row.col, column.col, value.col)]
+##  tbl <- reshape(sub, idvar=colnames(sub)[1L],
+##                 timevar=colnames(sub)[2],
+##                 v.names=colnames(sub)[3],
+##                 direction="wide")
+##  mat <- data.matrix(tbl[,-1L, drop=FALSE])
+##  rownames(mat) <- tbl[,1L,drop=TRUE]
+##  new.cols <- sapply(colnames(mat),
+##                     function(x) paste(strsplit(x, "\\.")[[1]][-1], collapse="."))
+##  colnames(mat) <- new.cols
+##  return(mat)
+##}
+
+longdf2matrix <- function(df, row.col = 1L, column.col = 2L, value.col = 3L) {
   sub <- df[, c(row.col, column.col, value.col)]
-  tbl <- reshape(sub, idvar=colnames(sub)[1L],
-                 timevar=colnames(sub)[2],
-                 v.names=colnames(sub)[3],
-                 direction="wide")
-  mat <- data.matrix(tbl[,-1L, drop=FALSE])
-  rownames(mat) <- tbl[,1L,drop=TRUE]
-  new.cols <- sapply(colnames(mat),
-                     function(x) paste(strsplit(x, "\\.")[[1]][-1], collapse="."))
-  colnames(mat) <- new.cols
+  subrows <- unique(sub[,1L])
+  subcols <- unique(sub[,2L])
+  r.ind <- match(sub[,1L], subrows)
+  c.ind <- match(sub[,2L], subcols)
+  m.ind <- (c.ind-1)*length(subrows) + r.ind
+  stopifnot(length(m.ind)==length(subrows)*length(subcols))
+  mat <- matrix(NA, nrow=length(subrows), ncol=length(subcols))
+  mat[m.ind] <- sub[,3L]
+  rownames(mat) <- subrows
+  colnames(mat) <- subcols
   return(mat)
 }

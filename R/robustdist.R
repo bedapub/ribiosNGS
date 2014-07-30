5fb5839ca926f7ec6e8cad2919291d95675fe9e5
zhangj83 <jitao_david.zhang@roche.com>
2014-07-30 10:41:01 +0200

    robustDist remove bad data points

--- R/robustdist.R
+++ R/robustdist.R
@@ -1,7 +1,10 @@
 robustDist <- function(x,...) {
   res <- dist(x,...)
-  res.max <- max(res, na.rm=TRUE)
+  isBad <- is.na(res) | is.nan(res) | is.infinite(res)
+  
+  res.max <- max(res[!isBad], na.rm=TRUE)
   if(is.na(res.max)) res.max <- 1L
-  res[is.na(res)] <- res.max
+
+  res[isBad] <- res.max
   return(res)
 }

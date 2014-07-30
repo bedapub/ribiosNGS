d9ac47275d256f729268e1c34d4a7bed1a16d2ab
zhangj83 <jitao_david.zhang@roche.com>
2014-07-30 10:39:54 +0200

    add fcol class and fcbrewer function to handle factor-matching colors

--- R/colors.R
+++ R/colors.R
@@ -37,9 +37,42 @@ colorpanel <- function (n, low, mid, high) {
 }
 
 ## colors for factors
+## fbrewer returns a vector of colors matching a factor, and its base colors
+fcbrewer <- function(factor, panel="Set1") {
+  if(!is.factor(factor)) factor <- factor(factor)
+  nlevel <- nlevels(factor)
+  if(is.function(panel)) {
+    cols <- do.call(panel, list(nlevel))
+  } else if (is.character(panel) && length(panel)==1 && exists(panel) && is.function(panelf <- get(panel))) {
+    cols <- do.call(panelf, list(nlevel))
+  } else if (is.character(panel)) {
+    if(length(panel)==1 && panel %in% rownames(RColorBrewer::brewer.pal.info)) {
+      ncol <-  bound(nlevel, 3L, brewer.pal.info[panel, "maxcolors"])
+      cols <- RColorBrewer::brewer.pal(ncol, panel)
+      if (nlevel < 3) {
+        cols <- cols[1:nlevel]
+      } else if (nlevel > ncol) {
+        cols <- grDevices::colorRampPalette(cols)(nlevel)
+      }
+    } else {
+      haltifnot(nlevel==length(panel),
+                msg=sprintf("panel length (%d) must match the number of factor levels (%d)",
+                  length(panel), nlevel))
+      cols <- panel
+    }
+  } else {
+    stop("panel must be either (a) a valid brewer.pal color name (see brewer.pal.info), (b) a function or function name that generates colors, or (c) a vector of colors that match the number of levels of the input factor")
+  }
+
+  
+  names(cols) <- levels(factor)
+  fullcols <- cols[as.integer(factor)]
+  return(fcol(fullcols, base=cols))
+}
+
 ## .factorLevels returns the color used along the levels, whereas .factor return 1:1 mapping from levels to colors
 brewer.pal.factorLevels <- function(factor, name="Greys") {
-  if(!require(RColorBrewer)) return(NULL)
+  .Deprecated("fcbrewer")
   nlevel <- nlevels(factor)
   ncol <- bound(nlevel, 3L, brewer.pal.info[name, "maxcolors"])
   cols <- brewer.pal(ncol, name)
@@ -53,6 +86,7 @@ brewer.pal.factorLevels <- function(factor, name="Greys") {
 }
 
 brewer.pal.factor <- function(factor, name="Greys") {
+  .Deprecated("fcbrewer")
   colbase <- brewer.pal.factorLevels(factor=factor, name=name)
   if(is.null(colbase)) return(NULL)
   return(colbase[factor])

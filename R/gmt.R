c9f7d93fadccec09cd17e11f865f62f05ae47df4
zhangj83 <jitao_david.zhang@roche.com>
2014-08-08 09:53:09 +0200

    add parseGmt

--- R/gmt.R
+++ R/gmt.R
@@ -1,8 +1,3 @@
-readGmt <- function(file) {
-  res <- read_gmt_list(file)
-  as(res, "GeneSets")
-}
-
 `[.GeneSets` <- function(x,i, ...) {
   r <- NextMethod("[")
   as(r, "GeneSets")
@@ -14,13 +9,19 @@ matchGenes <- function(x, vec, na.rm=TRUE) {
     stop("'x' must be a 'GeneSets' object")
   haltifnot(is.vector(vec),
             msg="'vec' must be a vector of gene names")
-  gs.indices <- lapply(x, function(xx) {
-    genes <- setdiff(xx$genes,c("", "-"))
-    ind <- match(genes, vec)
-    if(na.rm) ind <- ind[!is.na(ind)]
-    return(ind)
+  xgl <- lapply(x, function(x) unique(x$genes))
+  xgenes <- unlist(xgl)
+  xgenes[is.na(xgenes) | xgenes=="" | xgenes=="-"] <- NA
+  xgenes.ind <- match(xgenes, vec)
+  xlens <- sapply(xgl, length)
+  xfac <- factor(rep(1:length(xgl), xlens), levels=1:length(xgl))
+  gs.indices <- split(xgenes.ind, xfac)
+  gs.indices <- lapply(gs.indices, function(x) {
+    if(na.rm) x <- x[!is.na(x)]
+    return(x)
   })
   names(gs.indices) <- gsName(x)
+
   return(gs.indices)
 }
 
@@ -41,4 +42,22 @@ geneCountFilter <- function(x, min, max) {
   if(!missing(min)) sel <- sel & gmt.count >= min
   if(!missing(max)) sel <- sel & gmt.count <= max
   x[sel]
+}
+
+## public methods
+readGmt <- function(file) {
+  res <- read_gmt_list(file)
+  as(res, "GeneSets")
+}
+
+parseGmt <- function(file, vec, min, max) {
+  res <- read_gmt_list(file)
+  res <- as(res, "GeneSets")
+  ind <- matchGenes(res, vec, na.rm=TRUE)
+
+  ## final filtering
+  nmin <- ifelse(!missing(min) && is.numeric(min), min, 0)
+  nmax <- ifelse(!missing(max) && is.numeric(max), max, Inf)
+  ind <- ind[sapply(ind, function(x) length(x)>=nmin & length(x)<=nmax)]
+  return(ind)
 }

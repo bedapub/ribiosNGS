00c3a23055d6919f64719b95204425fa0a523aed
zhangj83 <jitao_david.zhang@roche.com>
2014-11-11 12:46:08 +0100

    with getDefaultFontFamily to choose ArialMT for Inkscape editing. However, it gives strange results if used with convert, therefore the function is revoked in the next commit

--- R/openFileDevice.R
+++ R/openFileDevice.R
@@ -1,9 +1,22 @@
-openFileDevice <- function(filename, width=7, height=7, dpi=300L) {
+getDefaultFontFamily <- function() {
+  psFonts <- names(postscriptFonts())
+  if("ArialMT" %in% psFonts) {
+    return("ArialMT")
+  } else if ("Helvetica" %in% psFonts()) {
+    return("Helvetica")
+  } else {
+    return("")
+  }
+}
+openFileDevice <- function(filename, width=7, height=7, dpi=300L, family) {
   filetype <- extname(filename)
+  if(missing(family)) {
+    family <- getDefaultFontFamily()
+  }
   if(identical(filetype,"pdf")) {
-    pdf(filename, width=width, height=height, useDingbats=FALSE)
+    pdf(filename, width=width, height=height, useDingbats=FALSE, family=family)
   } else if (identical(filetype, "png")) {
-    png(filename, width=width, height=height, units="in", type="cairo", res=dpi)
+    png(filename, width=width, height=height, units="in", type="cairo", res=dpi, family=family)
   } else if(identical(filetype,"tiff") || identical(filetype, "tif")) {
     tiff(filename, width=width, height=height, units="in", res=dpi)
   } else if(identical(filetype, "bmp")) {
@@ -12,7 +25,7 @@ openFileDevice <- function(filename, width=7, height=7, dpi=300L) {
     jpeg(filename, width=width, height=height, units="in", res=dpi, quality=90)
   } else {
     warning("Unrecognized output format '", filetype, "'. PDF will be used")
-    pdf(filename, width=width, height=height, useDingbats=FALSE)
+    pdf(filename, width=width, height=height, useDingbats=FALSE, family=family)
   }
 }
 

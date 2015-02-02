5c582be0298d46236383e8b598f269cb64201047
zhangj83 <jitao_david.zhang@roche.com>
2015-02-02 10:49:30 +0100

    generic functions updated

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -1,32 +1,3 @@
 setGeneric("exprsToLong", function(x,...) standardGeneric("exprsToLong"))
-
-
-setMethod("exprsToLong", "matrix", function(x, idvar="illID",timevar="hybridID", valuevar="value", 
-                                            ids=rownames(x), valueType="raw") {
-  x <- as.data.frame(x)
-  colnames(x) <- paste(valuevar, colnames(x), sep=".")
-  va <- 1:ncol(x)
-  x[,idvar] <- ids
-  xLong <- reshape(x, idvar=idvar, varying=va, timevar=timevar, direction="long")
-  rownames(xLong) <- NULL
-  xLong$type <- valueType
-  xLong <- xLong[,c(idvar, timevar, "type", valuevar)]
-  return(xLong)
-})
-setMethod("exprsToLong", "ExpressionSet", function(x,...) {
-  exprsToLong(exprs(x),...)
-})
-setMethod("rowscale", c("ExpressionSet","ANY", "ANY"), function(object, center, scale) {
-  if(missing(center))
-    center <- TRUE
-  if(missing(scale))
-    scale <- TRUE
-  if(storageMode(object)!="lockedEnvironment")
-    warning("The storageMode of the input object is not 'lockedEnvironment': exprs is replaced by row-scaled values\n",
-            "To prevent damaging the data integrity, set the storageMode to 'lockedEnvironment'")
-  
-  exprs(object) <- t(scale(t(exprs(object)),
-                           center=center, scale=scale))
-  return(object)
-})
-
+setGeneric("contrastNames", function(object) standardGeneric("contrastNames"))
+setGeneric("designVariables", function(object) standardGeneric("designVariables"))

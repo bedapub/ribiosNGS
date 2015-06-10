5bd80c8fdb8d9e1c3366a24614916e48bae7d2e2
zhangj83 <jitao_david.zhang@roche.com>
2015-06-10 11:21:26 +0200

    read DAVID output files

---
+++ ribiosIO/R/read_david.R
@@ -0,0 +1,17 @@
+read_david <- function(file) {
+  lines <- readLines(file, skipNul=TRUE)
+  lines <- lines[lines!=""]
+  clusterHeads <- grep("^Annotation Cluster", lines)
+  clusterInd <- cbind(clusterHeads+2, c(clusterHeads[-1]-1, length(lines)))
+  clusters <- apply(clusterInd,
+                    1, function(x) {lines[x[1]:x[2]]})
+  clusters.df <- lapply(clusters, function(x) read.table(textConnection(x), sep="\t", head=FALSE, comment.char="", quote=""))
+  head <- strsplit(lines[clusterHeads[1]+1], "\t")[[1]]
+  head[head=="%"] <- "Percentage"
+  head <- make.names(head)
+  df <- do.call(rbind, clusters.df)
+  colnames(df) <- head
+  df$cluster <- factor(rep(seq(along=clusterHeads), sapply(clusters.df, nrow)))
+  df <- df[,c("cluster", head)]
+  return(df)
+}

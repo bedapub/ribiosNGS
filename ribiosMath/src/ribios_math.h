3ba4c5dbde3d6d8748832a16696aad2fe13e0338
Jitao David Zhang <jitao_david.zhang@roche.com>
2015-08-28 23:40:32 +0200

    use RcppArmadillo for matrix operations

--- ribiosMath/src/ribios_math.h
+++ ribiosMath/src/ribios_math.h
@@ -1,10 +1,12 @@
 #ifndef RIBIOS_MATH_H
 #define RIBIOS_MATH_H
 
-#include <Rcpp.h>
+#include <RcppArmadillo.h>
 
 RcppExport SEXP cossim(SEXP, SEXP, SEXP);
 RcppExport SEXP empval(SEXP a, SEXP b);
 RcppExport SEXP randmat(SEXP x, SEXP n, SEXP N);
+RcppExport SEXP kappaLA(SEXP Xs);
+RcppExport SEXP kappaSimp(SEXP Xs);
 
 #endif

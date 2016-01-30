6508629b67933c809e6185677d1e702e6b147733
zhangj83 <jitao_david.zhang@roche.com>
2016-01-30 07:52:19 +0100

    biosCamera testing

---
+++ ribiosGSEA/tests/testthat/test-biosCamera.R
@@ -0,0 +1,32 @@
+library(limma)
+library(ribiosGSEA)
+
+set.seed(1887)
+y <- matrix(rnorm(1000*6),1000,6)
+rownames(y) <- sprintf("Gene%d", 1:nrow(y))
+design <- cbind(Intercept=1,Group=c(0,0,0,1,1,1))
+
+## index1: genuinely significantly up-regulated
+index1 <- 1:20
+y[index1,4:6] <- y[index1,4:6]+1
+## index2: not DEs
+index2 <- 21:40
+
+gs <- list(GeneSet1=index1, GeneSet2=index2)
+(cameraRes <- camera(y, gs, design))
+(cameraModRes <- biosCamera(y, gs, design))
+addCols <- c("GeneSet", "Score", "ContributingGenes")
+idCols <- c("NGenes", "Correlation", "Direction", "PValue", "FDR")
+stopifnot(identical(cameraRes[,idCols], cameraModRes[,idCols]))
+stopifnot(all(c(addCols, idCols) %in% colnames(cameraModRes)))
+
+## test no row names
+yNoRowNames <- y
+rownames(yNoRowNames) <- NULL
+(cameraModRes.noRowNames <- biosCamera(yNoRowNames, gs, design))
+stopifnot(identical(cameraRes[,idCols], cameraModRes.noRowNames[,idCols]))
+
+## test rankSum test
+(cameraRsRes <- camera(y, gs, design, use.ranks=TRUE))
+(cameraRsModRes <- biosCamera(y, gs, design, use.ranks=TRUE))
+stopifnot(identical(cameraRsRes[,idCols], cameraRsModRes[,idCols]))

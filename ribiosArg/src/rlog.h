77622f1c65c60efa6e2b2c3d0ed192b5262e4669
Jitao David Zhang <davidvonpku@gmail.com>
2016-05-28 07:52:06 +0200

    ribiosArg is now compatible with the new ribiosBase package; and the tests are now robust against warning messages

--- ribiosArg/src/rlog.h
+++
@@ -1,5 +0,0 @@
-#ifndef RIBIOS_ARG_RLOG
-#define RIBIOS_ARG_RLOG
-void die(char *x,...);
-void usage(char *x,...);
-#endif
--- ribiosArg/tests/test_getArg.R
+++ ribiosArg/tests/test_getArg.R
@@ -3,6 +3,12 @@ library(ribiosArg)
 
 rscript <- file.path(dirname(dirname(commandArgs()[1])),
                      "Rscript")
+
+## useful for cases where there is a WARNING message (e.g. "WARNING: ignoring environment value of R_HOME")
+identicalWoWarning <- function(x,y) {
+    x <- x[!grepl("^WARNING",x)]
+    identical(x,y)
+}
 if(file.exists(rscript)) {
   comm.format <- paste(rscript,
                        "-e \"",
@@ -14,33 +20,33 @@ if(file.exists(rscript)) {
                        collapse=" ")
   comm <- sprintf(comm.format, "infile", "NA", "NA")
   comm.out <- system(comm, intern=TRUE)
-  stopifnot(identical(comm.out, "[1] \"a.file\" \"b.file\""));
+  stopifnot(identicalWoWarning(comm.out, "[1] \"a.file\" \"b.file\""));
   
   comm2 <- sprintf(comm.format, "outfile", "NULL", "NULL")
   comm2.out <- system(comm2, intern=TRUE)
-  stopifnot(identical(comm2.out,  "[1] \"o.file\"  \"o2.file\""))
+  stopifnot(identicalWoWarning(comm2.out,  "[1] \"o.file\"  \"o2.file\""))
 
   comm3 <- sprintf(comm.format, "value", "NULL", "NA")
   comm3.out <- system(comm3, intern=TRUE)
-  stopifnot(identical(comm3.out, "[1] \"2,3\""))
+  stopifnot(identicalWoWarning(comm3.out, "[1] \"2,3\""))
 
   comm4 <- sprintf(comm.format, "value2", "\\\"0\\\"","\\\"0\\\"")
   comm4.out <- system(comm4, intern=TRUE)
-  stopifnot(identical(comm4.out, "[1] \"3\"  \"4\"  \"6\"  \"NA\""))
+  stopifnot(identicalWoWarning(comm4.out, "[1] \"3\"  \"4\"  \"6\"  \"NA\""))
 
   comm5 <- sprintf(comm.format, "hallo", "\\\"1887\\\"", "NULL")
   comm5.out <- system(comm5, intern=TRUE)
-  stopifnot(identical(comm5.out,  "[1] \"1887\""))
+  stopifnot(identicalWoWarning(comm5.out,  "[1] \"1887\""))
 
   comm6 <- sprintf(comm.format, "hello", "\\\"Impossible\\\"", "NA")
   comm6.out <- system(comm6, intern=TRUE)
-  stopifnot(identical(comm6.out, "[1] NA"))
+  stopifnot(identicalWoWarning(comm6.out, "[1] NA"))
 
   comm6a <- sprintf(comm.format, "hello", "\\\"Impossible\\\"", "\\\"Sure\\\"")
   comm6a.out <- system(comm6a, intern=TRUE)
-  stopifnot(identical(comm6a.out, "[1] \"Sure\""))
+  stopifnot(identicalWoWarning(comm6a.out, "[1] \"Sure\""))
 
   comm6b <- sprintf(comm.format, "hello", "\\\"Impossible\\\"", "NULL")
   comm6b.out <- system(comm6b, intern=TRUE)
-  stopifnot(identical(comm6b.out, "NULL"))
+  stopifnot(identicalWoWarning(comm6b.out, "NULL"))
 }

c7d418050139edffb3fbc4bc088e28b877dabe73
zhangj83 <jitao_david.zhang@roche.com>
2016-06-01 13:00:45 +0200

    bioinfoCookie adds notes on how to set u/p

--- ribiosAuth/R/bioinfoCookie.R
+++ ribiosAuth/R/bioinfoCookie.R
@@ -7,6 +7,7 @@
 #' The function first checks whether a cached cookie exists: if so, it
 #' simply returns it without querying the webserver; otherwise, it
 #' fetches a newly backed cookie from the webserver.
+#' @note the package has to authenticate itself with the server baking the cookie. The username and password is set in src/get_bioinfo_cookie_pw.h. This should be configured before installation.   
 #' @author Jitao David Zhang, Detlef Wolf, and Clemens Brogers
 #' @examples
 #' \dontrun{bioinfoCookie()}

2d33457e16fe1f4550fe618b83ab09272838a4dd
zhangj83 <jitao_david.zhang@roche.com>
2016-11-19 07:19:20 +0100

    install ribios from github directly

---
+++ install_ribios_from_github.R
@@ -0,0 +1,19 @@
+## install ribios packages from github
+
+if(require("devtools")) {
+  install_github("Accio/ribios/ribiosBase")
+  install_github("Accio/ribios/ribiosDemo")
+  install_github("Accio/ribios/ribiosUtils")
+  install_github("Accio/ribios/ribiosIO")
+  install_github("Accio/ribios/ribiosPlot")
+  install_github("Accio/ribios/ribiosMath")
+  install_github("Accio/ribios/ribiosArg")
+  install_github("Accio/ribios/ribiosExpression")
+  install_github("Accio/ribios/ribiosGSEA")
+  install_github("Accio/ribios/ribiosAnnotation")
+  install_github("Accio/ribios/ribiosGSA")
+  install_github("Accio/ribios/ribiosNGS")
+  install_github("Accio/ribios/ribiosBatchJobs")
+  install_github("Accio/ribios/ribiosAuth")
+  install_github("Accio/ribios/ribiosUDIS")
+}

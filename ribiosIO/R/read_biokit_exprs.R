9ac1dee2efcf566f3ec01b2cdbe9b4a0f6f0e929
zhangj83 <jitao_david.zhang@roche.com>
2016-12-20 15:20:42 +0100

    read_rocheNGS_exprs renamed into read_biokit_exprs

--- ribiosIO/R/read_biokit_exprs.R
+++ ribiosIO/R/read_biokit_exprs.R
@@ -1,6 +1,6 @@
 ## read roche NGS pipeline (built mainly by Fabian Birzele) expression files
-read_rocheNGS_exprs <- function(filename) {
+read_biokit_exprs <- function(filename) {
   filename <- checkfile(filename)
-  res <- .Call("c_read_rocheNGS_exprs", filename)
+  res <- .Call("c_read_biokit_exprs", filename)
   return(res)
 }

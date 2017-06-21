30df8631bcb1d8a418ebf800ea4235e286c2eba9
Jitao David Zhang <jitao_david.zhang@roche.com>
2017-06-21 15:06:47 +0200

    updated loadObject

--- ribiosIO/R/loadFile.R
+++ ribiosIO/R/loadFile.R
@@ -10,12 +10,14 @@ loadFile <- function (rDataFile, env = globalenv()) {
 
 #' Load an object by its name from a RData file
 #' @param file A RData file
-#' @param obj Object name
+#' @param obj Object name. If set as \code{NULL}, all objects are returned
 #' @param verbose Whether the loading process should be verbose, see \code{\link{load}}
 #'
 #' @export
-loadObject <- function(file, obj, verbose=FALSE) {
-    env <- new.env()
-    load(file, env, verbose=verbose)
-    get(obj, env)
+loadObject <- function(file, obj=NULL, verbose=FALSE) {
+  env <- new.env()
+  load(file, env, verbose = verbose)
+  if(is.null(obj))
+     obj <- ls(env=env)
+  get(obj, env)
 }

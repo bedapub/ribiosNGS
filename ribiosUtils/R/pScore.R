4d028d168faf76dec757ab1387f77940427766da
Jitao David Zhang <jitao_david.zhang@roche.com>
2017-07-05 14:26:11 +0200

    allow logical as signs

--- ribiosUtils/R/pScore.R
+++ ribiosUtils/R/pScore.R
@@ -4,13 +4,20 @@
 #' by the following equation: abs(log10(p))*sign.
 #' 
 #' @param p \emph{p}-value between (0,1]
-#' @param sign Sign of the score, either positive (in case of a positive number), 
-#'             negative (in case of a negative number), or zero.
+#' @param sign Sign of the score, either positive (in case of positive numbers), 
+#'             negative (in case of negative numbers), or zero. 
+#'             In case a logical vector, \code{TRUE} is interpreted as positive
+#'             and \code{FALSE} is interpreted as negative.
+#'             
 #' @examples
 #' testPvals <- c(0.001, 0.01, 0.05, 0.1, 0.5, 1)
 #' pScore(testPvals)
 #' testPvalSign <- rep(c(-1,1), 3)
 #' pScore(testPvals, sign=testPvalSign)
+#' testLog <- rep(c(TRUE, FALSE),3)
+#' pScpre(testPvals, testLog)
 pScore <- function(p, sign=1) {
+  if(is.logical(sign))
+    sign <- ifelse(sign, 1, -1)
   abs(log10(p)) * sign(sign)
 }
--- ribiosUtils/tests/testthat/test_pScore.R
+++ ribiosUtils/tests/testthat/test_pScore.R
@@ -3,6 +3,7 @@ context("pScore")
 testPvals <- c(0.001, 0.01, 0.05, 0.1, 0.5, 1)
 testPvalSign <- rep(c(-1,1), 3)
 testPvalLogFC <- c(-2, 3, -4, 5, -1.1, 1.2)
+testLogical <- rep(c(TRUE, FALSE),3)
 
 test_that("pScore works as expected", {
   expect_equal(pScore(testPvals),
@@ -11,4 +12,6 @@ test_that("pScore works as expected", {
                abs(log10(testPvals))*sign(testPvalSign))
   expect_equal(pScore(testPvals, sign=testPvalLogFC),
                abs(log10(testPvals))*sign(testPvalLogFC))
+  expect_equal(pScore(testPvals, sign=testLogical),
+               abs(log10(testPvals))*ifelse(testLogical, 1, -1))
 })

c90330c1bbf5cf55d39fded539df2f02e8800061
Jitao David Zhang <jitao_david.zhang@roche.com>
2017-07-13 09:28:14 +0200

    add a foo function to prevent roxygen's complain about not finding R files

---
+++ ribiosBase/R/ribiosInfo.R
@@ -0,0 +1,6 @@
+#' Print basic information about ribios
+ribiosInfo <- function() {
+  cat("This is RIBIOS, a suite of R packages dedicated to tasks in bioinformatics and computational biology\n")
+  cat("Author: Jitao David Zhang, with inputs from Clemens Broger, Martin Ebeling, Laura Badi, etc.")
+  cat("Contact: jitao_david.zhang@roche.com")
+}

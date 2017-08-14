a84e65f698bc0430d2a6ddf889d4d7640c44fbea
Jitao David Zhang <jitao_david.zhang@roche.com>
2017-08-14 12:18:39 +0200

    update scriptSkelete so that it works in ribiosArgs

--- ribiosArg/R/scriptSkeleton.R
+++ ribiosArg/R/scriptSkeleton.R
@@ -10,10 +10,10 @@ scriptSkeleton <- function(file=stdout()) {
     sentences <- c("#!/usr/bin/env Rscript",
                    "",
                    "# Initialize the script",
-                   "suppressMessage(library(ribiosScript))",
-                   "ribiosScript::initScript()",
-                   "usage <- paste(\"Usage:\", scriptName(), \"USAGES\\nUsage Details\", sep=\"\")",
-                   "argParse(\"Op1,Op1Len Op2,Op2Len, ..., log,1\", \
+                   "suppressMessages(library(ribiosArg))",
+                   "ribiosArg::scriptInit()",
+                   "usage <- paste(\"Usage:\", scriptName(), \" USAGES\\nUsage Details\", sep=\"\")",
+                   "argParse(\"log,1\", \
                      \"Param1 Param2\", usage)",
                    "",
                    "# Create log",

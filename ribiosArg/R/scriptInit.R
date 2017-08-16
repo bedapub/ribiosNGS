de4dfeecbfc88dbdf791fa322f1081e57f86b2bf
Jitao David Zhang <jitao_david.zhang@roche.com>
2017-08-16 21:34:40 +0200

    always include globalenv

--- ribiosArg/R/scriptInit.R
+++ ribiosArg/R/scriptInit.R
@@ -5,8 +5,6 @@
 #' prepare the R environment to run in a script setting.
 #'
 #' @param dumpto Character string, file name to dump to when there is an error
-#' @param include.GlobalEnv logical indicating if a _copy_ of the .GlobalEnv enrivonment should be included
-#' in addition tot he 'sys.frames()'. Useful when used in a batch job
 #' 
 #' @return Only side effect is used
 #' 
@@ -15,12 +13,12 @@
 #' \dontrun{
 #'   scriptInit()
 #' }
-scriptInit <- function(dumpto="ribios.dump", include.GlobalEnv=TRUE) {
+scriptInit <- function(dumpto="ribios.dump") {
   if(interactive()) {
     setDebug()
   } else {
     options(error=quote({
-      dump.frames(dumpto, to.file=TRUE, include.GlobalEnv=include.GlobalEnv)
+      dump.frames(dumpto, to.file=TRUE, include.GlobalEnv=TRUE)
       quit(save="no", status=1L)
     }))
   }

e13d34f22eb7eb5d4376ac3f3a3c8eacfa7360dd
Jitao David Zhang <jitao_david.zhang@roche.com>
2017-09-08 16:26:35 +0200

    fix bug and typo in the function

--- ribiosIO/R/writeStrList.R
+++ ribiosIO/R/writeStrList.R
@@ -6,15 +6,19 @@
 #' @param type Should list items written in columns or rows?
 #' @param index Logical, should integer index be printed along the elements?
 #'
-#' @example
-#' myList <- list("A"=LETTERS[3], "B"=LETTERS[4])
-#' writeStrList(myList, file=tempfile())
-#' writeStrList(myList, file=tempfile(), names=c("ListA", "ListB"))
-#' writeStrList(myList, file=tempfile(), names=c("ListA", "ListB"), type="row")
-#' writeStrList(myList, file=tempfile(), names=c("ListA", "ListB"), type="row", incdex=TRUE)
-writeStrList <- function(list, file, names=names(list), type=c("column", "row"), index=FALSE) {
+#' @examples
+#' myList <- list("A"=LETTERS[3:5], "B"=LETTERS[4])
+#' writeStrList(myList, file=stdout())
+#' writeStrList(myList, file=stdout(), names=c("ListA", "ListB"))
+#' writeStrList(myList, file=stdout(), names=c("ListA", "ListB"), type="row")
+#' writeStrList(myList, file=stdout(), names=c("ListA", "ListB"), type="row", index=TRUE)
+#' writeStrList(myList, file=stdout(), names=c("ListA", "ListB"), type="column", index=TRUE)
+writeStrList <- function(list, file, names=NULL, type=c("column", "row"), index=FALSE) {
   type <- match.arg(type)
-  stopifnot(!is.null(names))
+  if(is.null(names))
+      names <- names(list)
+  if(is.null(names))
+      stop("Input list must have valid names when the parameter 'names' is null")
   maxlen <- max(sapply(list, length))
   flist <- lapply(list, function(x) c(x, rep("", maxlen-length(x))))
   if(type=="column") {
@@ -28,11 +32,17 @@ writeStrList <- function(list, file, names=names(list), type=c("column", "row"),
     tbl <- do.call(rbind, flist)
     rownames(tbl) <- names
     if(index) {
-      colnames(tbl) <- 1:nrow(tbl)
+      rownames(tbl) <- 1:nrow(tbl)
     }
-    write.table(tbl, file=file,
-                quote=FALSE, sep="\t", row.names=TRUE,
-                col.names=index, dec=".")
+    if(!index) {
+        write.table(tbl, file=file,
+                    quote=FALSE, sep="\t", row.names=TRUE,
+                    col.names=FALSE, dec=".")
+    } else {
+        write.table(tbl, file=file,
+                    quote=FALSE, sep="\t", row.names=TRUE,
+                    col.names=NA, dec=".")
+    }
   } else {
     stop("Should not be here")
   }

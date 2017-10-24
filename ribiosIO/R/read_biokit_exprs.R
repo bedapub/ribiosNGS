908fc0a2685baafe3ed91893d67a19579ce014df
Jitao David Zhang <jitao_david.zhang@roche.com>
2017-10-24 14:34:39 +0200

    rename file1-3.expression biokit-output-1-4.expression

--- ribiosIO/R/read_biokit_exprs.R
+++ ribiosIO/R/read_biokit_exprs.R
@@ -1,4 +1,11 @@
-## read roche NGS pipeline (built mainly by Fabian Birzele) expression files
+#' Read BioKit expression file into a data.frame
+#' 
+#' @param filename A BioKit expression file
+#' 
+#' The function uses an efficient C routine to read BioKit expression files.
+#' 
+#' @example 
+#' 
 read_biokit_exprs <- function(filename) {
   filename <- checkfile(filename)
   res <- .Call("c_read_biokit_exprs", filename)

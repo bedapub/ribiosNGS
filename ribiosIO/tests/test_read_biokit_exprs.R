2613e9ba6756c3af51abc082853c0d5ac1fe6e2e
Jitao David Zhang <jitao_david.zhang@roche.com>
2017-10-24 15:07:35 +0200

    add randomly selected rows and all rows containing LOC.*345.* to test rows with different column counts

--- ribiosIO/tests/test_read_biokit_exprs.R
+++ ribiosIO/tests/test_read_biokit_exprs.R
@@ -1,15 +1,17 @@
 library(ribiosIO)
 
-ngsFile <- system.file("extdata/NGS_exprs/file1.expression", package="ribiosIO")
+ngsFile <- system.file("extdata/biokit_expression_files/biokit-output-1.expression", package="ribiosIO")
 ngs <- read_biokit_exprs(ngsFile)
 
 ## nrows must be correct
-stopifnot(nrow(ngs)==20786)
+stopifnot(nrow(ngs)==186)
 
-## mandatory columns
+## mandatory columns (name set in C routine)
 stopifnot(identical(colnames(ngs)[1:6],
-                    c("RPKM_MultiMap", "ReadCount_MultiMap", "RPKM_UniqMap", "ReadCount_UniqMap", "MultiProp", "AllMappingReads")))
+                    c("RPKM_MultiMap", "ReadCount_MultiMap", 
+                      "RPKM_UniqMap", "ReadCount_UniqMap",
+                      "MultiProp", "AllMappingReads")))
 
 ## rownames
 stopifnot(identical(rownames(ngs)[1:3],
-                    c("101930417", "101929892", "101927617")))
+                    c("55423", "5568", "126")))

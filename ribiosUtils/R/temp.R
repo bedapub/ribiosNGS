24a767f96d8b3b8aa9bc215b479ecf38bd66b78b
Jitao David Zhang <jitao_david.zhang@roche.com>
2017-10-28 22:02:45 +0200

    document the functions

--- ribiosUtils/R/temp.R
+++ ribiosUtils/R/temp.R
@@ -9,6 +9,10 @@ ribiosTempdir <- function() {
 
 #' A temporary file which (1) every machine in the cluster has access to and (2) there is sufficient space
 #'
+#' @param pattern Character string, file pattern
+#' @param tmpdir Character string, temp directory
+#' @param fileext CHaracter string, file name extension (suffix)
+#' 
 #' @return a character string of the file name
 #' @seealso \code{\link{ribiosTempdir}}
 

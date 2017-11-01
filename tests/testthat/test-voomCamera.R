80f8e54ac02b8a6546bc6559443955be844d3fa4
Jitao David Zhang <jitao_david.zhang@roche.com>
2017-11-01 17:10:41 +0100

    out of unknown reasons voomCamera still sorts the table; the temp solution is to make sure that check works

--- tests/testthat/test-voomCamera.R
+++ tests/testthat/test-voomCamera.R
@@ -1,5 +1,6 @@
 library(ribiosNGS)
 library(ribiosGSEA)
+library(ribiosUtils)
 
 mat <- matrix(c(10,3,5,9,3,5,
                 2,4,8,12,9,9,
@@ -28,8 +29,11 @@ voomCameraTbl <- voomCameraOut@enrichTables
 ## validate
 ## Note that from limma 3.29.6, the default parameters of camera changed: inter.gene.cor=0.01 (used to be NA), and allow.neg.cor=FALSE (used to be TRUE)
 matVoom <- as.matrix(voom(mat))
-resContrast1 <- camera(matVoom, gsInd, design=designMatrix, contrast=contrastMatrix[,1], inter.gene.cor=NA, allow.neg.cor=TRUE)
-resContrast2 <- camera(matVoom, gsInd, design=designMatrix, contrast=contrastMatrix[,2], inter.gene.cor=NA, allow.neg.cor=TRUE)
+resContrast1 <- camera(matVoom, gsInd, design=designMatrix, contrast=contrastMatrix[,1], inter.gene.cor=NA, allow.neg.cor=FALSE)
+resContrast2 <- camera(matVoom, gsInd, design=designMatrix, contrast=contrastMatrix[,2], inter.gene.cor=NA, allow.neg.cor=FALSE)
+
+## make sure that the order is correct
+voomCameraTbl <- with(voomCameraTbl, voomCameraTbl[order(Contrast, GeneSet),])
 
 expect_identical_field <- function(field) {
     expect_identical(c(resContrast1[, field],resContrast2[,field]),
@@ -46,3 +50,4 @@ expect_identical_field("Correlation")
 expect_identical_field("Direction")
 expect_identical(c(rownames(resContrast1),rownames(resContrast2)),
                    voomCameraTbl$GeneSet)
+

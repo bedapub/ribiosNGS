cfc53a19c00cace7f8792bd7c8563967d1ed82d8
Jitao David Zhang <jitao_david.zhang@roche.com>
2017-11-08 16:01:33 +0100

    install the current package in all R versions in use

---
+++ utils/installInAllRversions.bash
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+## Install the current R package to all R versions in use, including the latest version as well as the version used in RStudio
+ml R/3.3.2-goolf-1.7.20
+make install
+ml load R/3.4.0-goolf-1.7.20
+make install

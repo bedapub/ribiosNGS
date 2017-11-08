71634f5f068f83b0d0ed65793e6b804027794a4f
Jitao David Zhang <jitao_david.zhang@roche.com>
2017-11-08 15:39:01 +0100

    fix typos in biosCamera documentation

--- ribiosGSEA/R/camera.R
+++ ribiosGSEA/R/camera.R
@@ -1,7 +1,7 @@
 #' An adapted and enhanced version of limma::camera
 #' 
 #' @param y 	a numeric matrix of log-expression values or log-ratios of expression values, or any data object containing such a matrix. Rows correspond to probes and columns to samples. Any type of object that can be processed by getEAWP is acceptable.
-#' @param index an index vector or a list of index vectors. Can be any vector such that y[index,] of statistic[index] selects the rows corresponding to the test set. The list can be made using \codes{ids2indices}.
+#' @param index an index vector or a list of index vectors. Can be any vector such that y[index,] of statistic[index] selects the rows corresponding to the test set. The list can be made using \code{ids2indices}.
 #' @param design Design matrix
 #' @param contrast contrast of the linear model coefficients for which the test is required. Can be an integer specifying a column of design, or else a numeric vector of same length as the number of columns of design.
 #' @param weights numeric matrix of observation weights of same size as \code{y}, or a numeric vector of array weights with length equal to \code{ncol(y)}, or a numeric vector of gene weights with length equal to \code{nrow(y)}.
@@ -26,13 +26,13 @@
 #'   \item{GeneSet}{Gene set name}
 #'   \item{NGenes}{Number of genes in the set}
 #'   \item{Correlation}{Estimated correlation}
-#'   \item{Direction}{Direction of set-wise regulation, \code{Up} or \cod{Down}}
+#'   \item{Direction}{Direction of set-wise regulation, \code{Up} or \code{Down}}
 #'   \item{Score}{Gene-set enrichment score, defined as \code{log10(pValue)*I(directionality)}, where \code{I(directionality)} equals \code{1} if the directionality is \code{Up} and \code{-1} if the directionality is \code{Down}}
 #'   \item{ContribuingGenes}{A character string, containing all genes labels of genes that are in the set and regulated in the same direction as the set-wise direction, and the respective statistic}
 #' }
 #' 
 #' @note 
-#' Since limma 3.29.6, the default setting of allow.neg.cor changes from TRUE to FALSE, and a new parameter, inter.gene.cor, is added with the default value of 0.01, namely a prior inter-gene correlation is set for all gene sets. Currently, \code{biosCamera} does not have  the parameter \cpde{inter.gene.cor}, but \code{allow.neg.cor} is set by default to \code{FALSE} to be consistent with the latest camera function.
+#' Since limma 3.29.6, the default setting of allow.neg.cor changes from TRUE to FALSE, and a new parameter, inter.gene.cor, is added with the default value of 0.01, namely a prior inter-gene correlation is set for all gene sets. Currently, \code{biosCamera} does not have  the parameter \code{inter.gene.cor}, but \code{allow.neg.cor} is set by default to \code{FALSE} to be consistent with the latest camera function.
 #' 
 #' @examples 
 #' y <- matrix(rnorm(1000*6),1000,6)
@@ -51,8 +51,6 @@
 #' # compare with the output of camera: columns 'GeneSet', 'Score', 'ContributingGenes' are missing, and in case \code{inter.gene.cor} is (as default) set to a numeric value, the column 'Correlation' is also missing
 #' limma::camera(y, index1, design) 
 #' limma::camera(y, index1, design, inter.gene.cor=NA)
-
-
 biosCamera <- function (y, index, design = NULL, contrast = ncol(design), weights = NULL,
                         geneLabels=NULL,
                         use.ranks = FALSE, allow.neg.cor = FALSE, trend.var = FALSE, 

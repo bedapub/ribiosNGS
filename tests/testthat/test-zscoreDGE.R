2d2f7788ba087211a7acef88be78126d27ad2047
Jitao David Zhang <jitao_david.zhang@roche.com>
2017-11-08 17:40:43 +0100

    add man pages

---
+++ tests/testthat/test-zscoreDGE.R
@@ -0,0 +1,6 @@
+library(edgeR)
+library(ribiosNGS)
+
+## make sure that zscoreDGE and .zscoreDGE in edgeR (not exported) are identical
+## TODO: better to check consistency outcome
+expect_equal(zscoreDGE, edgeR:::.zscoreDGE)

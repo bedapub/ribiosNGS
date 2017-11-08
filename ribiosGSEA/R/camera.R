4782fb2305055f55f792f72460c8696f4df847c1
Jitao David Zhang <jitao_david.zhang@roche.com>
2017-11-08 11:14:28 +0100

    add documentation of biosCamera

--- ribiosGSEA/R/camera.R
+++ ribiosGSEA/R/camera.R
@@ -1,11 +1,62 @@
-## biosCamera: an adaption of limma::camera, with following improvments
-## (1) more user-friendly output
-## (2) scores calculated based on p-value and on directionality
-## (3) contributing genes
+#' An adapted and enhanced version of limma::camera
+#' 
+#' @param y 	a numeric matrix of log-expression values or log-ratios of expression values, or any data object containing such a matrix. Rows correspond to probes and columns to samples. Any type of object that can be processed by getEAWP is acceptable.
+#' @param index an index vector or a list of index vectors. Can be any vector such that y[index,] of statistic[index] selects the rows corresponding to the test set. The list can be made using \codes{ids2indices}.
+#' @param design Design matrix
+#' @param contrast contrast of the linear model coefficients for which the test is required. Can be an integer specifying a column of design, or else a numeric vector of same length as the number of columns of design.
+#' @param weights numeric matrix of observation weights of same size as \code{y}, or a numeric vector of array weights with length equal to \code{ncol(y)}, or a numeric vector of gene weights with length equal to \code{nrow(y)}.
+#' @param geneLabels Labels of the features in the input matrix
+#' @param use.ranks do a rank-based test (TRUE) or a parametric test (FALSE)?
+#' @param allow.neg.cor should reduced variance inflation factors be allowed for negative correlations?
+#' ## @param inter.gene.cor numeric, optional preset value for the inter-gene correlation within tested sets. If NA or NULL, then an inter-gene correlation will be estimated for each tested set.
+#' @param trend.var logical, should an empirical Bayes trend be estimated? See \code{eBayes} for details.
+#' @param sort logical, should the results be sorted by p-value?
+#' 
+#' The function was adapted from \code{\link[limma]{camera}}, with following improvments
+#' \enumerate{
+#'   \item The output data.frame is more user-friendly
+#'   \item The column 'FDR' is always present, even when only one gene-set was tested
+#'   \item Scores are calculated, defined as \code{log10(pValue)*I(directionality)}, where \code{I(directionality)} equals \code{1} if the directionality is \code{Up} and \code{-1} if the directionality is \code{Down}
+#'   \item Contributing genes and statistics are printed
+#' }
+#' 
+#' @return 
+#' A \code{data.frame} with one row per set and the following columns:
+#' \describe{
+#'   \item{GeneSet}{Gene set name}
+#'   \item{NGenes}{Number of genes in the set}
+#'   \item{Correlation}{Estimated correlation}
+#'   \item{Direction}{Direction of set-wise regulation, \code{Up} or \cod{Down}}
+#'   \item{Score}{Gene-set enrichment score, defined as \code{log10(pValue)*I(directionality)}, where \code{I(directionality)} equals \code{1} if the directionality is \code{Up} and \code{-1} if the directionality is \code{Down}}
+#'   \item{ContribuingGenes}{A character string, containing all genes labels of genes that are in the set and regulated in the same direction as the set-wise direction, and the respective statistic}
+#' }
+#' 
+#' @note 
+#' Since limma 3.29.6, the default setting of allow.neg.cor changes from TRUE to FALSE, and a new parameter, inter.gene.cor, is added with the default value of 0.01, namely a prior inter-gene correlation is set for all gene sets. Currently, \code{biosCamera} does not have  the parameter \cpde{inter.gene.cor}, but \code{allow.neg.cor} is set by default to \code{FALSE} to be consistent with the latest camera function.
+#' 
+#' @examples 
+#' y <- matrix(rnorm(1000*6),1000,6)
+#' design <- cbind(Intercept=1,Group=c(0,0,0,1,1,1))
+
+#' # First set of 20 genes are genuinely differentially expressed
+#' index1 <- 1:20
+#' y[index1,4:6] <- y[index1,4:6]+1
+
+#' # Second set of 20 genes are not DE
+#' index2 <- 21:40
+
+#' biosCamera(y, index1, design)
+#' biosCamera(y, index2, design)
+
+#' # compare with the output of camera: columns 'GeneSet', 'Score', 'ContributingGenes' are missing, and in case \code{inter.gene.cor} is (as default) set to a numeric value, the column 'Correlation' is also missing
+#' limma::camera(y, index1, design) 
+#' limma::camera(y, index1, design, inter.gene.cor=NA)
+
+
 biosCamera <- function (y, index, design = NULL, contrast = ncol(design), weights = NULL,
                         geneLabels=NULL,
                         use.ranks = FALSE, allow.neg.cor = FALSE, trend.var = FALSE, 
-                        sort = TRUE) 
+                        sort = FALSE) 
 {
     y <- as.matrix(y)
     G <- nrow(y)
@@ -163,9 +214,11 @@ biosCamera <- function (y, index, design = NULL, contrast = ncol(design), weight
     tab$Direction <- Direction
     tab$PValue <- tab$TwoSided
     tab$Down <- tab$Up <- tab$TwoSided <- NULL
-    if (nsets > 1) 
+    if (nsets > 1) {
         tab$FDR <- p.adjust(tab$PValue, method = "BH")
-
+    } else {
+        tab$FDR <- tab$PValue
+    }
     tab$Score <- -log10(tab$PValue) * ifelse(Direction=="Up", 1, -1)
     tab$ContributingGenes <- as.character(conts)
     tab$GeneSet <- names(index)

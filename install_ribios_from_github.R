74ff036d56eae8d23b25bf3fdc0bdf2ed3e78d15
Jitao David Zhang <jitao_david.zhang@roche.com>
2017-12-18 10:34:07 +0100

    the order suggested by Sebastian

--- install_ribios_from_github.R
+++ install_ribios_from_github.R
@@ -9,11 +9,12 @@ if(require("devtools")) {
   install_github("Accio/ribios/ribiosMath")
   install_github("Accio/ribios/ribiosArg")
   install_github("Accio/ribios/ribiosExpression")
+  install_github("Accio/ribios/ribiosUDIS")
+  install_github("Accio/ribios/ribiosCGI")
   install_github("Accio/ribios/ribiosGSEA")
   install_github("Accio/ribios/ribiosAnnotation")
   install_github("Accio/ribios/ribiosGSA")
   install_github("Accio/ribios/ribiosNGS")
   install_github("Accio/ribios/ribiosBatchJobs")
   install_github("Accio/ribios/ribiosAuth")
-  install_github("Accio/ribios/ribiosUDIS")
 }

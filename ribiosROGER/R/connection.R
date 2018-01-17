f0dbe2eb433d93203db3accaf927934914cb34a1
Jitao David Zhang <jitao_david.zhang@roche.com>
2018-01-17 08:26:05 +0100

    devROGER allows user to change the destination of connection

--- ribiosROGER/R/connection.R
+++ ribiosROGER/R/connection.R
@@ -2,12 +2,14 @@
 #'
 #' @importFrom DBI dbConnect
 #' @importFrom RSQLite SQLite 
+#'
+#' @param dest SQLite3 file which holds the ROGER database. The default value is the prototype database.
 #' 
 #' @export
 #' 
 #' @examples
 #' devROGER()
-devROGER <- function() {
+devROGER <- function(dest="/pstore/data/bi/apps/ROGER/protROGER-v0.1.db") {
   DBI::dbConnect(RSQLite::SQLite(), 
-                 "/pstore/data/bi/apps/ROGER/protROGER-v0.1.db")
+                 dest)
 }

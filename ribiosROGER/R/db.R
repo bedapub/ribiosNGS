490e9e94ed7636d73882a7bc77fc6ce1baaf9b1f
Jitao David Zhang <jitao_david.zhang@roche.com>
2018-01-17 14:55:21 +0100

    add function to import limma results

--- ribiosROGER/R/db.R
+++ ribiosROGER/R/db.R
@@ -436,4 +436,254 @@ addContrasts <- function(conn,
   return(newContrastIDs)
 }
 
+## Import microarray data
+
+## limmaDgeTable to be merged with ribiosNGS::dgeTable
+
+#' limma DGE table of one contrast
+#'
+#' @param eFit An \code{MArrayLM} object
+#' @param contrast Contrast of choice
+#'
+#' @importFrom limma topTable
+#' @importFrom ribiosExpression limmaTopTable2dgeTable
+limmaOneDgeTable <- function(eFit, contrast) {
+    tbl <- topTable(eFit, coef=contrast, number=nrow(eFit))
+    tbl <- limmaTopTable2dgeTable(tbl)
+    return(tbl)
+}
+## to be merged with ribiosNGS::dgeTables
+
+#' limma DGE table
+#'
+#' @param eFit An \code{MArrayLM} object
+#'
+#' @importFrom ribiosUtils putColsFirst
+#' @importFrom ribiosExpression contrastNames
+limmaDgeTables <- function(eFit) {
+    conts <- contrastNames(eFit)
+    res <- lapply(conts, function(x) limmaOneDgeTable(eFit, x))
+    return(res)
+}
+## to be merged with ribiosNGS::dgeTable
+
+#' limma DGE table
+#'
+#' @param eFit An \code{\link[limma]{MArrayLM-class}} object
+#' @param contrast Contrast(s) of choice
+#'
+#' @importFrom ribiosUtils putColsFirst
+limmaDgeTable <- function(eFit, contrast=NULL) {
+    if (is.logical(contrast) || is.numeric(contrast)) {
+        contrast <- contrastNames(eFit)[contrast]
+    }
+    if(!is.null(contrast)) {
+        if (length(contrast) == 0) {
+            stop("No contrast selected")
+        }
+        else if (!all(contrast %in% contrastNames(eFit))) {
+            stop("Following contrasts are not found:", setdiff(contrast, 
+                                                               contrastNames(eFit)))
+        }
+    }
+    if(!is.null(contrast) && length(contrast==1)) {
+        res <- limmaOneDgeTable(eFit, contrast)
+        res$Contrast <- contrast
+    } else {
+        if(is.null(contrast)) {
+            contrast <- contrastNames(eFit)
+        }
+        tbls <- lapply(contrast, function(x) limmaOneDgeTable(eFit, x))
+        res <- do.call(rbind, tbls)
+        res$Contrast <- rep(contrast, sapply(tbls, nrow))
+    }
+    res <- putColsFirst(res, "Contrast")
+    rownames(res) <- NULL
+    return(res)
+}
+
+#' @importFrom Biobase sampleNames
+maPhenoData <- function(eset, phenoDataFile=NULL, groups=NULL) {
+    if(!is.null(phenoDataFile)) {
+        ## TODO: fail if the first column is not unique (which should contain sample names)
+        pd <- Biobase::read.AnnotatedDataFrame(phenoDataFile)
+    } else {
+        pd <- data.frame(Sample=sampleNames(eset))
+    }
+    if(is.null(groups))
+        groups <- rep("NA", ncol(eset))
+    pd$`_DatasetSampleIndex` <- 1:nrow(pd)
+    pd$`_Sample` <- sampleNames(eset)
+    pd$`_SampleGroup` <- groups
+    return(pd)
+}
+
+#' Import the content of a EdgeResult object into the database
+#' 
+#' @param conn Database connection
+#' @param eSet An object of the class \code{\link[Biobase]{eSet-class}}
+#' @param eSetFit An object of the class \code{\link[limma]{MArrayLM-class}}
+#' @param enrichTbl Gene-set enrichment table
+#' @param valType Value type
+#' @param xref External reference
+#' @param anno Logical, whether annotation was provided by the limma script
+#' @param verbose Logical, whether verbose or not
+#' 
+#' The script is used by maDge_limma.Rscript to import limma results into ROGeR. It is very
+#' experimental: the code has not been thoroughly tested and needs refactoring. Use it with caution.
+#' 
+#' @export
+#' @importFrom ribiosNGS hasNoReplicate dgeTable featureNames
+#' @importFrom ribiosUtils matchColumn trim pScore
+#' @importFrom ribiosExpression contrastMatrix designMatrix contrastNames
+#' @importFrom Biobase featureNames fData exprs pData
+importLimmaResult <- function(conn, eSet, eSetFit, enrichTbl,
+                              valType=c("RMA", "MAS5"),
+                              xref=NA, anno=FALSE, verbose=TRUE)  {
+    valType <- match.arg(valType)
+    if(anno) {
+    annotation <- fData(eSet)
+    annotation$`_DatasetFeatureIndex` <- 1:nrow(annotation)
+    annotation$`_Feature` <- rownames(annotation)
+    ga <- readDfFromDb(conn, "GeneAnnotation")
+    ## TODO: the current way to find _GeneIndex this is only suitable for human features
+    annotation$`_GeneIndex` <- matchColumn(annotation$GeneID,
+                                           ga, "GeneID")$GeneIndex
+  } else {
+    annotation <- NULL
+  }
+
+  ## insert into Datasets
+  dbBegin(conn)
+  res <- list(datasetID=NA, designID=NA, contrastID=NA)
+  tryCatch({
+      if(verbose)
+          message("Writing dataset")
+      dsID <- newDatasetID(conn)
+      exprsMat <- exprs(eSet)
+      currUser <- getUser()
+      currTime <- Sys.time()
+      
+      if(is.na(dsID))
+          stop("Dataset ID failed - please check the integrity of the database")
+      ds <- data.frame(ID=dsID,
+                       Exprs=blobs(exprsMat),
+                       ExprsType=valType,
+                       Pheno=blobs(pData(eSet)),
+                       Feature=blobs(fData(eSet)),
+                       Xref=xref,
+                       CreatedBy=currUser,
+                       CreationTime=currTime)
+      writeDfToDb(conn, ds, tableName="Datasets", row.names=FALSE,
+                  overwrite=FALSE, append=TRUE)
+
+      isUsed <- annotation$`_Feature` %in% rownames(eSetFit$genes)
+      featSubset <- data.frame(DatasetFeatureIndex=annotation$`_DatasetFeatureIndex`,
+                               IsUsed=isUsed,
+                               Description="Default filtering by limma")
+      
+      sampleSubset <- data.frame(DatasetSampleIndex=1:ncol(exprsMat),
+                                 IsUsed=TRUE,
+                                 Description="maDge_limma.Rscript currently only support designs where all samples are used.")
+
+      ## insert into Designs
+      if(verbose) {
+          message("Writing design")
+      }
+      newDesID <- newDesignID(conn)
+      if(is.na(newDesID)) {
+          stop("Design ID failed - please check the integrity of the database")
+      }
+      design <-  data.frame(ID=newDesID,
+                            DatasetID=dsID,
+                            Name="defaultDesign",
+                            Description="limma script default design",
+                            SampleSubset=blobs(sampleSubset),
+                            FeatureSubset=blobs(featSubset),
+                            DesignMatrix=blobs(designMatrix(eSetFit)),
+                            CreatedBy=currUser,
+                            CreationTime=currTime)
+      
+      writeDfToDb(conn=conn, design, tableName="Designs", overwrite=FALSE, append=TRUE)
+
+      ## insert into DGEmodels
+      if(verbose) {
+          message("Writing DGEmodel")
+      }
+      dgeModel <- data.frame(DesignID=newDesID,
+                             DGEmethodID=1L, ## limma
+                             InputObj=blobs(eSet),
+                             FitObj=blobs(eSetFit))
+      writeDfToDb(conn=conn, dgeModel, tableName="DGEmodels", overwrite=FALSE, append=TRUE)
+
+      ## insert into contrasts
+      if(verbose)
+          message("Writing contrasts")
+      contMatrix <- contrastMatrix(eSetFit)
+      newContIDs <- newContrastIDs(conn, contMatrix)
+      if(length(newContIDs)==1 && is.na(newContIDs))
+          stop("Contrast ID failed - please check the integrity of the database")
+      contrasts <- data.frame(ID=newContIDs,
+                              DesignID=newDesID,
+                              Name=colnames(contMatrix),
+                              Description=colnames(contMatrix),
+                              Contrast=serializeMatrixByCol(contMatrix),
+                              CreatedBy=currUser,
+                              CreationTime=currTime)
+      writeDfToDb(conn=conn, contrasts, tableName="Contrasts", overwrite=FALSE, append=TRUE)
+
+      ## insert to DGEtable
+      if(verbose) {
+          message("Writing DGEtable")
+      }
+      dgeTbl <- limmaDgeTable(eSetFit)
+      dgetable <- data.frame(ContrastID=matchColumn(dgeTbl$Contrast, contrasts, "Name")$ID,
+                             FeatureIndex=matchColumn(dgeTbl$Feature, 
+                                                      annotation, "_Feature")$`_DatasetFeatureIndex`,
+                             GeneIndex=matchColumn(dgeTbl$GeneID,
+                                                   annotation, "GeneID")$`_GeneIndex`,
+                             AveExprs=dgeTbl$AveExpr,
+                             Statistic=dgeTbl$t,
+                             LogFC=dgeTbl$logFC,
+                             PValue=dgeTbl$PValue,
+                             FDR=dgeTbl$FDR,
+                             row.names=NULL)
+      writeDfToDb(conn=conn, dgetable, tableName="DGEtables", overwrite=FALSE, append=TRUE)
+
+      ## insert into GSEtables
+      if(verbose) {
+          message("Writing GSEtable")
+      }
+      gseMethodId <- 1L ## camera
+      gmtInfo <- readDfFromDb(conn, "DefaultGenesets")
+      gseContrastID <- newContIDs[match(enrichTbl$Contrast, contrasts$Name)]
+      gsIndex <- matchColumn(ribiosUtils::trim(as.character(enrichTbl$GeneSet)),
+                             gmtInfo, "GenesetName")$ID
+      
+      gsetable <- data.frame(GSEmethodID=gseMethodId,
+                             ContrastID=gseContrastID,
+                             DefaultGenesetID=gsIndex,
+                             Correlation=enrichTbl$Correlation,
+                             Direction=ifelse(enrichTbl$Direction=="Up", 1L, -1L),
+                             PValue=enrichTbl$PValue,
+                             FDR=enrichTbl$FDR,
+                             EnrichmentScore=pScore(enrichTbl$PValue, sign=enrichTbl$Direction=="Up"),
+                             EffGeneCount=enrichTbl$NGenes)
+      writeDfToDb(conn=conn, gsetable, tableName="GSEtables", overwrite=FALSE, append=TRUE)
+      
+      ## results
+      res <- list(datasetID=dsID,
+                  designID=newDesID,
+                  contrastID=newContIDs)
+      dbCommit(conn)
+      if(verbose) 
+          message("Database successfully updated!")
+  }, error=function(e) {
+      dbRollback(conn)
+      print(e)
+      warning("There was an error and hence no changes were done to the database!")
+  })
+    return(res)
+}
+
 #' @include df.R

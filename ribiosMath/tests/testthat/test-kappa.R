55465608fbbbbae38edf919f3b4f5b3ac7cae835
Jitao David Zhang <jitao_david.zhang@roche.com>
2018-01-19 11:11:25 +0100

    remove kappaSimp

--- ribiosMath/tests/testthat/test-kappa.R
+++ ribiosMath/tests/testthat/test-kappa.R
@@ -1,10 +1,8 @@
-## Test kappaLA and kappaSimp
+## Test colKappa
 
 testMat <- cbind(c(1,1,0,0,1,0), c(1,1,0,1,1,0))
 testMatExp <- matrix(c(1,2/3, 2/3, 1), byrow=TRUE, nrow=2)
 
-testMatLAkappa <- kappaLA(testMat)
+testMatLAkappa <- colKappa(testMat)
 expect_that(testMatExp, equals(testMatLAkappa))
 
-testMatSimpKappa <- kappaSimp(testMat)
-expect_that(testMatExp, equals(testMatSimpKappa))

f0b2d336011c46439a5f27598a3ff6033775fb56
Jitao David Zhang <jitao_david.zhang@roche.com>
2018-01-23 16:59:51 +0100

    remove the davidClustering_cpp function for now

--- ribiosMath/src/kappa.cpp
+++ ribiosMath/src/kappa.cpp
@@ -1,4 +1,5 @@
 #include <RcppArmadillo.h>
+// [[Rcpp::depends(RcppArmadillo)]]
 
 using namespace Rcpp;
 
@@ -94,34 +95,4 @@ BEGIN_RCPP
  return rres;
  
 END_RCPP
-}
-
-
-//' Perform heuristic fuzzy multi-linkage partitioning of DAVID
-//' 
-//' @examples 
-//' testMat <- cbind(c(1,1,0,0,1,0), c(1,1,0,1,1,0), c(1,0,1,1,0,1))
-//' testKappaMat <- rowKappa(testMat)
-//' davidClustering_cpp(testKappaMat)
-//' 
-//' @export
-// [[Rcpp::export]]
-Rcpp::List davidClustering_cpp(Rcpp::NumericMatrix kappaMatrix,
-                               double kappaThr = 0.35,
-                               int initialGroupMembership = 3) {
-  int anrow = kappaMatrix.nrow();
-  int ancol = kappaMatrix.ncol();
-  
-  Rcpp::List seeds;
-  for(int i=0; i<anrow; i++) {
-    Rcpp::IntegerVector currSeeds(1, i);
-    for(int j=0; j<ancol; j++)  {
-      if(kappaMatrix(i, j) >= kappaThr and i!=j) {
-        currSeeds.push_back(j);
-        Rcpp::Rcout << "i=" << i << ", j=" << j << std::endl;
-      }
-    }
-    seeds.push_back(currSeeds);
-  }
-  return(seeds);
 }

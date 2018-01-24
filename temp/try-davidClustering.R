b4c36200b8121ad1b2b9b3c09c0259993a8c67e3
Jitao David Zhang <jitao_david.zhang@roche.com>
2018-01-24 12:20:03 +0100

    davidClustering_cpp_R delivers identical results as davidClustering

--- temp/try-davidClustering.R
+++ temp/try-davidClustering.R
@@ -7,10 +7,13 @@ cppFunction('
 Rcpp::List davidClustering_cpp_R(Rcpp::NumericMatrix kappaMatrix,
                                double kappaThr = 0.35,
                                int initialGroupMembership = 3,
-                               double multiLinkageThr=0.5) {
+                               double multiLinkageThr=0.5,
+                               bool debug=0,
+                               int mergeRule=1) {
   int anrow = kappaMatrix.nrow();
   int ancol = kappaMatrix.ncol();
 
+  // TODO: seeds is a vector, which is quite expensive with operations like "erase". To be changed into list
   std::vector< std::set<int> > seeds;
   for(int i=0; i<anrow; i++) {
     std::set<int> currSeeds;
@@ -20,12 +23,13 @@ Rcpp::List davidClustering_cpp_R(Rcpp::NumericMatrix kappaMatrix,
        }
     }
    if(currSeeds.size() >= initialGroupMembership-1) {
-    Rcpp::Rcout << "Candidate seeds from row [" << i << "]" << std::endl;
+    if(debug) {
+      Rcpp::Rcout << "Candidate seeds from row [" << i << "]" << std::endl;
     for(std::set<int>::iterator it=currSeeds.begin(); it!=currSeeds.end(); ++it) {
        Rcpp::Rcout << *it << ",";
     }
     Rcpp::Rcout << std::endl;
-
+    }
      if(currSeeds.size() == 1) {
         currSeeds.insert(i);
         seeds.push_back(currSeeds);
@@ -43,15 +47,18 @@ Rcpp::List davidClustering_cpp_R(Rcpp::NumericMatrix kappaMatrix,
           currSeeds.insert(i);
           seeds.push_back(currSeeds);
         } else {
+          if(debug) {
           Rcpp::Rcout << "Candidate seeds from row [" << i << "] failed because lack of strong linkage" << std::endl;
           Rcpp::Rcout << "total kappa=" << totalKappa;
           Rcpp::Rcout << "strong linkage=" << overThrKappaCnt;
           Rcpp::Rcout << std::endl;
+          }
         }
       }
     }
   }
 
+  if(debug) {
   Rcpp::Rcout << "#Seeds=" << seeds.size() << std::endl;
   for(int i=0; i<seeds.size(); i++) {
     std::set<int> currSeeds = seeds[i];
@@ -61,17 +68,18 @@ Rcpp::List davidClustering_cpp_R(Rcpp::NumericMatrix kappaMatrix,
     }
     Rcpp::Rcout << std::endl;
   }
+  }
 
   // iteratively updating the seeds
   int lastSeedCount;
   int newSeedCount = -1;
   bool changed;
+  bool toMerge;
   while(newSeedCount<0 || newSeedCount != lastSeedCount) {
     lastSeedCount = seeds.size();
     changed = FALSE;
     for(int i=0; i<lastSeedCount-1; i++) {
       for(int j=i+1; j<lastSeedCount; j++) {
-         Rcpp::Rcout << "ITERATING: i=" << i << ", j=" << j << std::endl;
          std::set<int> seedi = seeds[i];
          std::set<int> seedj = seeds[j];
          std::set<int> intersect;
@@ -83,7 +91,15 @@ Rcpp::List davidClustering_cpp_R(Rcpp::NumericMatrix kappaMatrix,
                         seedj.begin(),seedj.end(),
                         std::inserter(ijunion,ijunion.begin()));
          int ninter = intersect.size();
-         if(ninter >= multiLinkageThr * seedi.size() || ninter >= multiLinkageThr * seedj.size()) {
+         toMerge = FALSE;
+         if(mergeRule==1) {
+             toMerge = ninter >= multiLinkageThr * ijunion.size();
+         } else if (mergeRule==2) {
+             toMerge = ninter >= multiLinkageThr * seedi.size() && ninter >= multiLinkageThr * seedj.size();
+         } else if (mergeRule==3) {
+             toMerge = ninter >= multiLinkageThr * seedi.size() || ninter >= multiLinkageThr * seedj.size();
+         }
+         if(toMerge) {
             seeds[i] = ijunion;
             seeds.erase(seeds.begin() + j);
             changed = TRUE;
@@ -97,6 +113,7 @@ Rcpp::List davidClustering_cpp_R(Rcpp::NumericMatrix kappaMatrix,
     newSeedCount = seeds.size();
   }
   
+  if(debug) {
   Rcpp::Rcout << "AFTER FILTERING #Seeds=" << seeds.size() << std::endl;
   for(int i=0; i<seeds.size(); i++) {
     std::set<int> currSeeds = seeds[i];
@@ -106,8 +123,17 @@ Rcpp::List davidClustering_cpp_R(Rcpp::NumericMatrix kappaMatrix,
     }
     Rcpp::Rcout << std::endl;
   }
+  }
 
    Rcpp::List seedsRes;
+   for(int i=0; i<seeds.size() ; ++i) {
+      Rcpp::IntegerVector currVec;
+      std::set<int> currSeeds=seeds[i];
+      for(std::set<int>::iterator it=currSeeds.begin(); it!=currSeeds.end(); ++it) {
+          currVec.push_back(*it+1); // index starts with 1 in R
+      }
+     seedsRes.push_back(currVec);
+   }
    return(seedsRes);
 }')
 
@@ -117,11 +143,30 @@ synData <- matrix(c(rep(c(rep(1, 10), rep(0, 5)), 3),
                     rep(c(rep(0,3), 1), 4)[-16]), ncol=15, byrow=TRUE)
 rownames(synData) <- sprintf("Gene %s", letters[1:8])
 colnames(synData) <- sprintf("t%d", 1:15)
-davidClustering(synData, removeRedundant = TRUE, debug=TRUE)
-davidClustering(t(synData), removeRedundant = TRUE, debug=FALSE)
+testExp1 <- davidClustering(synData, removeRedundant = TRUE, debug=FALSE)
+testExp2 <- davidClustering(t(synData), removeRedundant = TRUE, debug=FALSE)
 synKappaMat <- rowKappa(synData)
 synKappaMat.round2 <- round(synKappaMat, 2)
+synKappaMatT.round2 <- round(colKappa(synData),2)
 testOut1 <- davidClustering_cpp_R(synKappaMat.round2, kappaThr=0.35)
+testOut2 <- davidClustering_cpp_R(synKappaMatT.round2, kappaThr=0.35)
 
+expect_identical(testExp1, testOut1)
+expect_identical(testExp2, testOut2)
+
+benchmark(dcR=davidClustering(t(synData), removeRedundant = TRUE, debug=FALSE),
+          dcCpp=davidClustering_cpp_R(synKappaMatT.round2, kappaThr=0.35))
+
+## large matrix
+largeMat <- matrix(rbinom(5000, 1, 0.25), nrow=500)
+largematKappa <- round(rowKappa(largeMat),2)
+rres <- davidClustering(largeMat)
+cppres <- davidClustering_cpp_R(largematKappa, debug=FALSE, kappaThr=0.35)
+
+expect_identical(rres, cppres)
+
+benchmark(dcR=davidClustering(largeMat),
+          dcCpp=davidClustering_cpp_R(largematKappa))
+          
 ## internal
 clus3Kappa <- rowKappa(synData)[c(1,2,3,5,6,7),c(1,2,3,5,6,7)][lower.tri(rowKappa(synData)[c(1,2,3,5,6,7),c(1,2,3,5,6,7)], diag=FALSE)]

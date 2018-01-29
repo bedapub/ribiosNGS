fb5a2dc4bffe5ca7a2db36c01bcf8b22481055b1
Jitao David Zhang <jitao_david.zhang@roche.com>
2018-01-29 12:24:02 +0100

    the default UNION rule is now set as the third

--- ribiosMath/tests/testthat/test-davidClustering.R
+++ ribiosMath/tests/testthat/test-davidClustering.R
@@ -16,8 +16,8 @@ synKappaMatTp.round2 <- round(synKappaMatTp, 2)
 Rout1 <- ribiosMath:::davidClustering_kappa_R(synKappaMat.round2, removeRedundant = TRUE, debug=FALSE)
 Rout2 <- ribiosMath:::davidClustering_kappa_R(synKappaMatTp.round2, removeRedundant = TRUE, debug=FALSE)
 
-Cppout1 <- davidClustering_kappa(synKappaMat.round2, kappaThr=0.35)
-Cppout2 <- davidClustering_kappa(synKappaMatTp.round2, kappaThr=0.35)
+Cppout1 <- davidClustering_kappa(synKappaMat.round2, kappaThr=0.35, mergeRule=3)
+Cppout2 <- davidClustering_kappa(synKappaMatTp.round2, kappaThr=0.35, mergeRule=3)
 
 expect_identical(Rout1, Cppout1)
 expect_identical(Rout2, Cppout2)
@@ -28,7 +28,7 @@ set.seed(1887)
 largeMat <- matrix(rbinom(10000, 1, 0.25), nrow=1000)
 largematKappa <- round(rowKappa(largeMat),2)
 system.time(rres <- davidClustering_kappa_R(largematKappa))
-system.time(cppres <- davidClustering_kappa(largematKappa))
+system.time(cppres <- davidClustering_kappa(largematKappa, mergeRule=3))
 
 ## note that now the seeds from R and from C++ have different orders
 sortedSeeds <- function(x) x[order(sapply(x, paste, collapse=""))]

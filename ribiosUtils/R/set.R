b40a5574e656906c7aced57b0f1ae92fa053c289
Jitao David Zhang <jitao_david.zhang@roche.com>
2018-03-03 17:04:11 +0100

    if mset has only one paramter, the *unique* element will be returned

--- ribiosUtils/R/set.R
+++ ribiosUtils/R/set.R
@@ -1,12 +1,13 @@
 mset <- function(func, ...) {
   li <- list(...)
   if(length(li)==1L) li <- li[[1L]]
-  if(length(li)==1L) return(li[[1]])
+  if(length(li)==1L) return(unique(li[[1]]))
   
   res <- do.call(func, list(li[[1L]], li[[2L]]))
-  if(length(li)>2L)
+  if(length(li)>2L) {
     for(i in 3L:length(li))
       res <- do.call(func, list(res, li[[i]]))
+  } 
   res
 }
 munion <- function(...) mset(func=union, ...)

ac195b7351f3011bcdca72ec8640ec1e1585e593
Jitao David Zhang <jitao_david.zhang@roche.com>
2018-03-05 10:20:44 +0100

    add readDefaultGenesets so that default genesets can be loaded with one central function

--- ribiosGSEA/R/gmt.R
+++ ribiosGSEA/R/gmt.R
@@ -116,3 +116,92 @@ appendGeneSets <- function(geneSets, newGeneSets, ...) {
     }
     return(res)
 }
+
+#' Read molecular-phenotyping genesets
+#' 
+#' @param file GMT file which stores default molecular-phenotyping genesets
+#' 
+#' @return A \object{GeneSet} object containing molecular-phenotypic screening (MPS) categories and genes
+ 
+readMPSGmt <- function(file) {
+  gs <- read_gmt_list(file)
+  category <- sprintf("MPS %s", sapply(gs, function(x) x$description))
+  resl <- lapply(seq(along = gs), function(i) {
+    return(GeneSet(category = category[[i]], name = gs[[i]]$name, 
+                   desc = gs[[i]]$description, genes = gs[[i]]$genes))
+  })
+  names(resl) <- sapply(resl, gsName)
+  res <- GeneSets(resl)
+  return(res)
+}
+
+#' Read default genesets for gene-set enrichment analysis
+#'
+#' In Roche Bioinformatics we use a default collection of gene-sets for gene-set enrichment analysis. This function loads this collection.
+#'
+#' @param path Character, path to the directory where the gmt files are stored
+#' @param mps Logical,  whether molecular-phenotypic screening (MPS) genesets should be read in as pathway-centric categories (\code{TRUE}) or as one category named \code{MolecularPhenotyping} (\code{FALSE}).
+#' 
+#' @details
+#'
+#' The default collection includes both publicly available genesets as well as proprietary genesets, and therefore they are not included as part of the ribios package.
+#'
+#' Publicly available genesets include
+#' \itemize{
+#'  \item{MSigDB: collections C2, C7 and Hallmark}
+#'  \item{RONET: which is a collection of publicly available pathway databases including REACTOME and NCI-Nature}
+#'  \item{goslim}
+#' }
+#' @examples 
+#' \dontrun{
+#'   readDefaultGenesets("/tmp/defaultGmts")
+#' }
+readDefaultGenesets <- function(path,
+                                mps=FALSE) {
+  assertFile(msigdb.c2.file <- file.path(path, "msigdb.c2.all.symbols.gmt"))
+  assertFile(msigdb.c7.file <- file.path(path, "msigdb.c7.all.symbols.gmt"))
+  assertFile(msigdb.hallmark.file <- file.path(path, "msigdb.hallmark.all.symbols.gmt"))
+  
+  assertFile(mps.pathway <- file.path(path, "MolecularPhenotyping-genesets.gmt"))
+  
+  assertFile(ronet.file <- file.path(path, "path.ronet.roche.symbols.gmt"))
+  assertFile(goslim.file <- file.path(path, "go_slim.bp.roche.symbols.gmt"))
+
+  assertFile(upstream.file <- file.path(path, "MetaBase.downstream.expression.gmt"))  
+  assertFile(mbdisease.file <- file.path(path, "MetaBase.DiseaseBiomarker.gmt"))
+  assertFile(mbmetabolic.file <- file.path(path, "MetaBase.Metabolic.gmt"))
+  assertFile(mbpath.file <- file.path(path, "MetaBase.PathwayMap.gmt"))
+  assertFile(mbtoxicity.file <- file.path(path, "MetaBase.Toxicity.gmt"))
+  assertFile(mbpathology.file <- file.path(path, "MetaBase.ToxicPathology.gmt"))
+  assertFile(mbprocess.file <- file.path(path, "MetaBase.TRprocesses.gmt"))
+
+  assertFile(immunomics.file <- file.path(path, "exp.immune.roche.symbols.gmt"))
+  assertFile(immunespace.file <- file.path(path, "immunespace.gmt"))
+  
+  if(mps) {
+    mpsGSCs <- readMPSGmt(mps.pathway)
+    gscs <- readGmt(msigdbC2=msigdb.c2.file,
+                    msigdbC7=msigdb.c7.file,
+                    msigdbHallmark = msigdb.hallmark.file,
+                    immunomics = immunomics.file,
+                    immunespace = immunespace.file)
+    gscs <- appendGeneSets(mpsGSCs, gscs)
+  } else {
+    gscs <- readGmt(MolecularPhenotyping=mps.pathway,
+                    upstream=upstream.file,
+                    ronet=ronet.file,
+                    goslim=goslim.file,
+                    mbdisease=mbdisease.file,
+                    mbmetabolic=mbmetabolic.file,
+                    mbpath=mbpath.file,
+                    mbprocess=mbprocess.file,
+                    mbtoxicity=mbtoxicity.file,		
+                    mbpathology=mbpathology.file,
+                    msigdbC2=msigdb.c2.file,
+                    msigdbC7=msigdb.c7.file,
+                    msigdbHallmark = msigdb.hallmark.file,
+                    immunomics = immunomics.file,
+                    immunespace = immunespace.file)
+  }
+  return(gscs)
+}

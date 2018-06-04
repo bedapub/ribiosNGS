864448268bd15283bf4c84bc1d3e2e572f3c3bfd
Rudolf Biczok <rudolf.biczok@roche.com>
2018-06-04 14:20:29 +0200

    Refactor tests

--- ribiosBioMart/tests/testthat/helper.queryFun.R
+++ ribiosBioMart/tests/testthat/helper.queryFun.R
@@ -1,80 +1,26 @@
-getRemoteMartGeneAnnotationSample <- function() {
-  ratMart <- useMart("ensembl", dataset="rnorvegicus_gene_ensembl")
-  attributes <- c("ensembl_gene_id_version", "ensembl_gene_id","entrezgene",
-                  "gene_biotype", "external_gene_name")
-
-  rat <- biomaRt::getBM(attributes,
-                        values=TRUE, mart = ratMart)
-  setcolorder(rat, order(colnames(rat)))
-  return(rat)
-}
-
-getRemoteMartTranscriptAnnotationSample <- function() {
-  ratMart <- useMart("ensembl", dataset="rnorvegicus_gene_ensembl")
-  attributes <- c("ensembl_transcript_id_version",
-                  "ensembl_transcript_id", "ensembl_gene_id_version",
-                  "transcript_biotype", "external_transcript_name")
-  rat <- biomaRt::getBM(attributes,
-                        values=TRUE, mart = ratMart)
-  setcolorder(rat, order(colnames(rat)))
-  rat
-}
-
-getLocalMartGeneAnnotationSample <- function() {
-  conn <- dbConnect (MySQL(), user=testDB$user, password=testDB$passwd,
-                     dbname="ensembl_mart_92", host=testDB$host, port=testDB$port)
-  tryCatch({
-    mart <- useLocalMart(conn, dataset="rnorvegicus_gene_ensembl")
-
-    attributes <- c("ensembl_gene_id_version", "ensembl_gene_id", "entrezgene",
-                    "gene_biotype", "external_gene_name")
-
-    rat <- getLocalBM(attributes, mart = mart)
-    setcolorder(rat, order(colnames(rat)))
-    return(rat)
-  }, finally = {
-    dbDisconnect(conn)
-  })
-}
-
-getLocalMartTranscriptAnnotationSample <- function() {
-  conn <- dbConnect (MySQL (), user=testDB$user, password=testDB$passwd,
-                     dbname="ensembl_mart_92", host=testDB$host, port=testDB$port)
-
-  tryCatch({
-    mart <- useLocalMart(conn, dataset="rnorvegicus_gene_ensembl")
-
-    attributes <- c("ensembl_transcript_id_version", "ensembl_transcript_id", "ensembl_gene_id_version",
-                    "transcript_biotype", "external_transcript_name")
-
-    rat <- getLocalBM(attributes, mart = mart)
-    setcolorder(rat, order(colnames(rat)))
-    return(rat)
-  }, finally = {
-    dbDisconnect(conn)
-  })
-}
-
-queryRemote <- function(dataset="rnorvegicus_gene_ensembl",attributes, filters="", values="", verbose=FALSE) {
+queryRemote <- function(dataset, attributes, filters="", values="", verbose=FALSE) {
   martObj <- useMart("ensembl", dataset=dataset)
-  biomaRt::getBM(attributes = attributes,
-                 filters = filters,
-                 values = values,
-                 mart = martObj,
-                 verbose = verbose)
+  result <- biomaRt::getBM(attributes = attributes,
+                           filters = filters,
+                           values = values,
+                           mart = martObj,
+                           verbose = verbose)
+  setcolorder(result, order(colnames(result)))
+  result
 }
 
-queryLocal <- function(dataset="rnorvegicus_gene_ensembl", attributes, filters=NULL, values=NULL, verbose=FALSE) {
+queryLocal <- function(dataset, attributes, filters=NULL, values=NULL, verbose=FALSE) {
   conn <- dbConnect (MySQL (), user=testDB$user, password=testDB$passwd,
                      dbname="ensembl_mart_92", host=testDB$host, port=testDB$port)
-
   tryCatch({
     martObj <- useLocalMart(conn, dataset=dataset)
-    getLocalBM(attributes=attributes,
-               filters=filters,
-               values = values,
-               mart=martObj,
-               verbose=verbose)
+    result <- getLocalBM(attributes=attributes,
+                         filters=filters,
+                         values = values,
+                         mart=martObj,
+                         verbose=verbose)
+    setcolorder(result, order(colnames(result)))
+    result
   }, finally = {
     dbDisconnect(conn)
   })
--- ribiosBioMart/tests/testthat/test.getLocalBM.R
+++ ribiosBioMart/tests/testthat/test.getLocalBM.R
@@ -8,14 +8,22 @@ if(!exists("testDB")) {
 }
 
 test_that("Extract full gene annotation", {
-  geneLocal <- getLocalMartGeneAnnotationSample()
-  geneRemote <- getRemoteMartGeneAnnotationSample()
-  expect_equal(geneRemote, geneLocal)
+  dataset <- "rnorvegicus_gene_ensembl"
+  attributes <- c("ensembl_gene_id_version", "ensembl_gene_id","entrezgene",
+                  "gene_biotype", "external_gene_name")
+
+  localData <- queryLocal(dataset, attributes)
+  remoteData <- queryRemote(dataset, attributes)
+  expect_equal(localData, remoteData)
 })
 
 test_that("Extract full transcript annotation", {
-  transcriptLocal <- getLocalMartTranscriptAnnotationSample()
-  transcriptRemote <- getRemoteMartTranscriptAnnotationSample()
+  dataset <- "rnorvegicus_gene_ensembl"
+  attributes <- c("ensembl_transcript_id_version",
+                  "ensembl_transcript_id", "ensembl_gene_id_version",
+                  "transcript_biotype", "external_transcript_name")
 
-  expect_equal(transcriptRemote, transcriptLocal)
+  localData <- queryLocal(dataset, attributes)
+  remoteData <- queryRemote(dataset, attributes)
+  expect_equal(localData, remoteData)
 })

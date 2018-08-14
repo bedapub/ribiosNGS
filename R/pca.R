f8d45c9845b8176d607c7d3a677b26719a5fbd4c
Jitao David Zhang <jitao_david.zhang@roche.com>
2018-08-14 17:37:18 +0200

    use na.rm explicitly

--- R/pca.R
+++ R/pca.R
@@ -11,7 +11,7 @@ rowVars <- function (x, na.rm=TRUE) {
   sqr = function(x) x * x
   n = rowSums(!is.na(x))
   n[n <= 1] = NA
-  return(rowSums(sqr(x - rowMeans(x, ...)), ...)/(n - 1))
+  return(rowSums(sqr(x - rowMeans(x, na.rm=na.rm)), na.rm=na.rm)/(n - 1))
 }
 
 #' Principal component analysis of expression matrix

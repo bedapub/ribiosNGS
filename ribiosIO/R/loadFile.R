bf9691b44ad4157ff14e86b78237f0af2d50895c
Jitao David Zhang <jitao_david.zhang@roche.com>
2018-08-14 13:05:28 +0200

    add document for loadFIle and loadRDS

--- ribiosIO/R/loadFile.R
+++ ribiosIO/R/loadFile.R
@@ -1,3 +1,9 @@
+#' Load a RData file and returns a logical flag
+#' 
+#' @param rDataFile Character string, name of the RData file to be loaded
+#' @param env Environment, where the data should be read into
+#' 
+#' @return Logical, \code{TRUE} if the file loading was successful, otherwise \code{FALSE}
 loadFile <- function (rDataFile, env = globalenv()) {
     if (file.exists(rDataFile)) {
         load(rDataFile, env)
@@ -20,4 +26,22 @@ loadObject <- function(file, obj=NULL, verbose=FALSE) {
   if(is.null(obj))
      obj <- ls(envir=env)
   get(obj, env)
+}
+
+#' Load an object from a RDS file and returns a logical flag
+#' 
+#' @param rdsFile Character string, name of the rds file to be loaded
+#' @param variableName Character string or variable name, variable name to which the loaded value is assigned to
+#' @param refhook Logical, passed to \code{\link{base::readRDS}}
+#' 
+#' @return Logical, \code{TRUE} if the file loading was successful, otherwise \code{FALSE}
+loadRDS <- function(rdsFile, variableName, refhook=NULL) {
+  variableName <- as.character(substitute(variableName))
+  if(file.exists(rdsFile)) {
+    obj <- readRDS(rdsFile, refhook=refhook)
+    assign(variableName, value=obj, envir=parent.frame())
+    return(TRUE)
+  } else {
+    return(FALSE)
+  }
 }

cc9f242a845f32277fa97c6b602ff88f96dcd1d0
Jitao David Zhang <jitao_david.zhang@roche.com>
2018-08-15 12:26:51 +0200

    fix bug of removeNAfeatures: | instead of ||

--- ribiosNGS/R/logFCmatrix.R
+++ ribiosNGS/R/logFCmatrix.R
@@ -5,7 +5,8 @@
 #' @param contrasts \code{NULL} or characters; if not \code{NULL}, only logFC values of given contrasts will be returned
 #' 
 #' @note TODO: add edgeResult data example
-logFCmatrix <- function(edgeResult, featureIdentifier="GeneSymbol", contrasts=NULL,
+logFCmatrix <- function(edgeResult, featureIdentifier="GeneSymbol", 
+                        contrasts=NULL,
                         removeNAfeatures=TRUE) {
   tbls <- edgeResult@dgeTables
   allContrasts <- contrastNames(edgeResult)
@@ -17,7 +18,7 @@ logFCmatrix <- function(edgeResult, featureIdentifier="GeneSymbol", contrasts=NU
   rownames(mat) <- feats
   colnames(mat) <- allContrasts
   if(removeNAfeatures) {
-      isNAfeat <- is.na(feats) || feats=="-"
+      isNAfeat <- is.na(feats) | feats=="-"
       mat <- mat[!isNAfeat,,drop=FALSE]
   }
   if(!is.null(contrasts)) {

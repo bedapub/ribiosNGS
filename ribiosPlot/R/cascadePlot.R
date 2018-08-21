f8248f11abfa0e03af8903c6b0ed59dd5bd8e910
Jitao David Zhang <jitao_david.zhang@roche.com>
2018-08-21 15:51:10 +0200

    updated functionality of cascadeOrder: it now ranks rows by peak and values considering all columns (instead of only the max), and handles NA rows and invariant rows appropriately

--- ribiosPlot/R/cascadePlot.R
+++ ribiosPlot/R/cascadePlot.R
@@ -1,18 +1,57 @@
-## TODO: the function had a non-functional parameter decreasing, though inside it's always decreasing=FALSE
-unisignCascadeOrder <- function(matrix) {
-    maxtime <- apply(matrix, 1L, function(x) {
-                         if(all(is.na(x))) {
-                             return(ncol(matrix))
-                         } else {
-                             return(which.max(abs(x)))
-                         }})
-    maxval <- apply(matrix, 1L, function(x) {
-                        if(all(is.na(x))) {
-                            return(-Inf)
-                        } else {
-                            return(max(abs(x), na.rm=TRUE))
-                        }})
-    order(maxtime, -maxval, decreasing=FALSE)
+#' Variance of features in rows
+#' 
+#' @param x Numeric matrix
+#' @param na.rm Logical. Should missing values (including NaN) be omitted from the calculations?
+#'
+#' @examples
+#' myVal <- matrix(1:9, nrow=3, byrow=FALSE)
+#' myVar <- rowVars(myVal)
+#' stopifnot(identical(myVar, c(9,9,9)))
+#' @keywords internal
+rowVars <- function (x, na.rm=TRUE) {
+  sqr = function(x) x * x
+  n = rowSums(!is.na(x))
+  n[n <= 1] = NA
+  return(rowSums(sqr(x - rowMeans(x, na.rm=na.rm)), na.rm=na.rm)/(n - 1))
+}
+
+#' Order rows of a matrix with non-negative maximum values in the cascade order
+#' 
+#' @param matrix A matrix with non-negative maximum values of rows. 
+#' 
+#' The function is internally called by \code{\link{cascadeOrder}} to order the rows of the matrix with non-negative maximum values.
+#' The function works by ordering the rows by peaks (in which column does the value peak?) and then by maximum values (what are the maximum values?).
+#' In case of invariant rows and rows of NAs, they are put at last.
+#'
+#' If maximum values of the rows are negative, negate the matrix before passing it to the matrix (see \code{\link{cascadeOrder}}).
+#' @keywords internal
+nonNegCascadeOrder <- function(matrix) {
+  rowInd <- 1:nrow(matrix)
+  naRows <- apply(matrix, 1, function(x) all(is.na(x)))
+  if(any(naRows)) {
+    naInd <- which(naRows)
+    matrix <- matrix[!naRows,, drop=FALSE]
+    rowInd <- rowInd[!naRows]
+  }
+  invarRows <- rowVars(matrix) == 0
+  if(any(invarRows)) {
+    invarInd <- which(invarRows)
+    matrix <- matrix[!invarRows,, drop=FALSE]
+    rowInd <- rowInd[!invarRows]
+  }
+  timeOrd <- t(apply(matrix, 1, order, decreasing=TRUE))
+  valRank <- t(apply(matrix, 1, sort, decreasing=TRUE, na.last=TRUE))
+  timeRank <- cbind(timeOrd, valRank)
+  timeRankOrd <- do.call(order,
+                 as.data.frame(timeRank))
+  res <- rowInd[timeRankOrd]
+  if(any(invarRows)) {
+    res <- c(res, invarInd)
+  }
+  if(any(naRows)) {
+    res <- c(res, naInd)
+  }
+  return(res)
 }
 
 #' Order rows of a matrix in the cascade order
@@ -23,17 +62,42 @@ unisignCascadeOrder <- function(matrix) {
 #' be from 1 to column number minus one.
 #' 
 #' @param matrix A numeric matrix
-#' @param dichotomy How are the rows divided into two? By mean value, median value, or by maximal abs value
+#' @param dichotomy How are the rows divided into two? By maximal abs value (default), mean value, or the median value of each row.
 #'
 #' @examples
-#' myMatrix <- matrix(c(1,2,3, 4,6,5, 8,7,9,3,4,8,9,1,2,-1,-2,-3, -4, -2, -1, -5, -7, -4), byrow=TRUE, ncol=3)
-#' rownames(myMatrix) <- sprintf("Gene%d", 1:nrow(myMatrix))
-#' biosHeatmap(myMatrix, Colv=FALSE, Rowv=FALSE, dendrogram="none")
-#' myOrder <- cascadeOrder(myMatrix)
-#' myOrderedMatrix <- myMatrix[myOrder,]
-#' biosHeatmap(myOrderedMatrix, Colv=FALSE, Rowv=FALSE, dendrogram="none")
-
-cascadeOrder <- function(matrix, dichotomy=c('mean', 'median', 'maxabs')) {
+#' checkBoard <- function() {
+#'   set.seed(1887)
+#'   mat <- matrix(rnorm(76, sd=1), ncol=4)
+#'   delta <- 3
+#'   for(i in seq(1, 16, 2)) {
+#'     rowInd <- i:(i+1)
+#'     colInd <- (i %/% 2) %% 4 +1
+#'     delta <- ifelse(i>8, -6, 6)
+#'     mat[rowInd, colInd] <- mat[rowInd, colInd] + delta
+#'   }
+#'   mat[17,1:4] <- rep(-1, 4)
+#'   mat[18,1] <- NA
+#'   mat[18,2] <- mat[18, 2] -6
+#'   mat[19,1:4] <- rep(NA,4)
+#'   rord <- sample(1:nrow(mat), replace=FALSE)
+#'   mat <- mat[rord,]
+#'   rownames(mat) <- sprintf("Row%d", 1:nrow(mat))
+#'   return(mat)
+#' }
+#' myMat <- checkBoard()
+#' biosHeatmap(myMat, Rowv=FALSE, Colv=FALSE, dendrogram="none",
+#'             zlim=c(-4,4), col="royalbluered",
+#'             main="Original matrix")
+#' ## since dist by default does not accept rows full of NAs, we remove them in the example below
+#' biosHeatmap(myMat[apply(myMat, 1, function(x) !all(is.na(x))),],
+#'             Rowv=TRUE, Colv=TRUE, dendrogram="both",
+#'             zlim=c(-4,4), col="royalbluered",
+#'             main="hclust/dist clustering")
+#' ## note that cascadeOrder handles invariant rows and rows full of NA values
+#' biosHeatmap(myMat[cascadeOrder(myMat),], Rowv=FALSE, Colv=FALSE, dendrogram="none",
+#'             zlim=c(-4,4), col="royalbluered",
+#'             main="Cascade order")
+cascadeOrder <- function(matrix, dichotomy=c('maxabs', 'mean', 'median')) {
     dichotomy <- match.arg(dichotomy)
     if(is.null(row.names(matrix)))
         rownames(matrix) <- 1:nrow(matrix)
@@ -53,10 +117,10 @@ cascadeOrder <- function(matrix, dichotomy=c('mean', 'median', 'maxabs')) {
     isNeg <- apply(matrix, 1, dichfun)
     isNeg[is.na(isNeg)] <- TRUE
     isPos <- !isNeg
-    negMatrix <- matrix[isNeg,]
-    posMatrix <- matrix[isPos,]
-    posOrder <- unisignCascadeOrder(posMatrix)
-    negOrder <- unisignCascadeOrder(negMatrix)
+    negMatrix <- matrix[isNeg,,drop=FALSE]
+    posMatrix <- matrix[isPos,,drop=FALSE]
+    posOrder <- nonNegCascadeOrder(posMatrix)
+    negOrder <- nonNegCascadeOrder(-negMatrix)
     
     ordrn <- c(rownames(matrix)[isPos][posOrder],
                rownames(matrix)[isNeg][negOrder])

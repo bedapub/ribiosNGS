ab362f513f36c9f83195a2c17423c280b836a7dd
Jitao David Zhang <jitao_david.zhang@roche.com>
2019-01-12 10:14:00 +0100

    jaccard index testing added

---
+++ ribiosUtils/tests/testthat/test_jaccardIndex.R
@@ -0,0 +1,7 @@
+expect_identical(ribiosUtils::jaccardIndex(LETTERS[1:5], LETTERS[3:8]),3/8)
+expect_identical(ribiosUtils::jaccardIndex(LETTERS[1:2], LETTERS[3:8]),0)
+expect_identical(ribiosUtils::jaccardIndex(LETTERS[1:4], LETTERS[1:4]),1)
+
+expect_identical(ribiosUtils::jaccardDist(LETTERS[1:5], LETTERS[3:8]),5/8)
+expect_identical(ribiosUtils::jaccardDist(LETTERS[1:2], LETTERS[3:8]),1)
+expect_identical(ribiosUtils::jaccardDist(LETTERS[1:4], LETTERS[1:4]),0)

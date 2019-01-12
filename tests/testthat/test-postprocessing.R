0619c694f6539880be9f8816ec245173364344dd
Jitao David Zhang <jitao_david.zhang@roche.com>
2019-01-12 09:34:13 +0100

    jaccardIndex is now in ribiosUtils

--- tests/testthat/test-postprocessing.R
+++ tests/testthat/test-postprocessing.R
@@ -1,19 +1,19 @@
 library(ribiosNGS)
 
-expect_identical(ribiosNGS:::parseContributingGenes("RXRA(-1.42),CD36(-1.21),PPARG(-0.56),NR1H3(-0.39),CEBPA(-0.28)"),
+expect_identical(ribiosUtils::parseContributingGenes("RXRA(-1.42),CD36(-1.21),PPARG(-0.56),NR1H3(-0.39),CEBPA(-0.28)"),
                  data.frame(Gene=c("RXRA", "CD36", "PPARG", "NR1H3", "CEBPA"),
                             Stat=c(-1.42, -1.21, -0.56, -0.39, -0.28)))
 
-expect_identical(ribiosNGS:::cameraScore(c(0.01, 0.1, 1), c("Up", "Down", "Up")),
+expect_identical(ribiosUtils::cameraScore(c(0.01, 0.1, 1), c("Up", "Down", "Up")),
                  c(2, -1, 0))
 
 
 
 
-expect_identical(ribiosNGS:::jaccardIndex(LETTERS[1:5], LETTERS[3:8]),3/8)
-expect_identical(ribiosNGS:::jaccardIndex(LETTERS[1:2], LETTERS[3:8]),0)
-expect_identical(ribiosNGS:::jaccardIndex(LETTERS[1:4], LETTERS[1:4]),1)
+expect_identical(ribiosUtils::jaccardIndex(LETTERS[1:5], LETTERS[3:8]),3/8)
+expect_identical(ribiosUtils::jaccardIndex(LETTERS[1:2], LETTERS[3:8]),0)
+expect_identical(ribiosUtils::jaccardIndex(LETTERS[1:4], LETTERS[1:4]),1)
 
-expect_identical(ribiosNGS:::jaccardDist(LETTERS[1:5], LETTERS[3:8]),5/8)
-expect_identical(ribiosNGS:::jaccardDist(LETTERS[1:2], LETTERS[3:8]),1)
-expect_identical(ribiosNGS:::jaccardDist(LETTERS[1:4], LETTERS[1:4]),0)
+expect_identical(ribiosUtils::jaccardDist(LETTERS[1:5], LETTERS[3:8]),5/8)
+expect_identical(ribiosUtils::jaccardDist(LETTERS[1:2], LETTERS[3:8]),1)
+expect_identical(ribiosUtils::jaccardDist(LETTERS[1:4], LETTERS[1:4]),0)

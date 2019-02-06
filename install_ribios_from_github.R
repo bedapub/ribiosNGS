4354fe3b264bba0d48d337b082b200040506b86f
Jitao David Zhang <jitao_david.zhang@roche.com>
2019-02-06 17:36:17 +0100

    not installing to-be-merged packages

--- install_ribios_from_github.R
+++ install_ribios_from_github.R
@@ -2,31 +2,35 @@
 
 ## install ribios packages from github
 
-if(require("devtools")) {
+if(require("devtools") && require("BiocManager")) {
   install_github("Accio/ribios/ribiosBase")
+  install_github("Accio/ribios/ribiosDemo")
   install_github("Accio/ribios/ribiosUtils")
   install_github("Accio/ribios/ribiosIO")
-  install_github("Accio/ribios/ribiosExpression")
+  install_github("Accio/ribios/ribiosArg")
   install_github("Accio/ribios/ribiosAuth")
   install_github("Accio/ribios/ribiosMath")
-  install_github("Accio/ribios/ribiosAnnotation")
-  install_github("Accio/ribios/ribiosCGI")
-  install_github("Accio/ribios/ribiosUDIS")
-  install_github("Accio/ribios/ribiosGSEA")
   install_github("Accio/ribios/ribiosPlot")
-  install_github("Accio/ribios/ribiosArg")
+  install_github("Accio/ribios/ribiosAnnotation")
+
+  install.packages("RCurl") ## to be replaced by httr
+  install_github("Accio/ribios/ribiosUDIS")
+  install_github("Accio/ribios/ribiosCGI")
+
+  BiocManager::install(c("edgeR", "limma", "Biobase", "latticeExtra", "globaltest", "data.table"))
+  install_github("Accio/ribios/ribiosExpression")
+  install_github("Accio/ribios/ribiosGSEA")
   install_github("Accio/ribios/ribiosNGS")
-  install_github("Accio/ribios/ribiosBatchJobs")
-  #install_github("Accio/ribios/ribiosBic")
-  install_github("Accio/ribios/ribiosDemo")
-  install_github("Accio/ribios/ribiosDiwa")
-  install_github("Accio/ribios/ribiosNetwork")
-  install_github("Accio/ribios/ribiosPCA")
-  install_github("Accio/ribios/ribiosQC")
-  install_github("Accio/ribios/ribiosReposition")
-  #install_github("Accio/ribios/ribiosRiboSeq")
+  ## install_github("Accio/ribios/ribiosBatchJobs") ## to be merged
+  ## install_github("Accio/ribios/ribiosBic") ## to be merged
+  ## install_github("Accio/ribios/ribiosDiwa") ## to be merged
+  ## install_github("Accio/ribios/ribiosNetwork") ## to be merged
+  ## install_github("Accio/ribios/ribiosPCA")
+  ## install_github("Accio/ribios/ribiosQC")
+  ## install_github("Accio/ribios/ribiosReposition")
+  ## install_github("Accio/ribios/ribiosRiboSeq")
   install_github("Accio/ribios/ribiosROGER")
-  install_github("Accio/ribios/ribiosSeq")
+  ## install_github("Accio/ribios/ribiosSeq")
 } else {
-  stop("Please install 'devtools' first!")
+  stop("Please install 'devtools' and 'BiocManager' first!")
 }

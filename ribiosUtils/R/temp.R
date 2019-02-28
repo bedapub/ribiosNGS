42438055563c8dd5e60ecf0cfce5eeeef368c97a
Jitao David Zhang <jitao_david.zhang@roche.com>
2019-02-28 15:49:00 +0100

    temp

--- ribiosUtils/R/temp.R
+++ ribiosUtils/R/temp.R
@@ -1,21 +1,25 @@
-#' A temporary directory which (1) every machine in the cluster has access to and (2) has sufficient space
-#'
+#' A temporary directory which (1) every machine in the cluster has access to
+#' and (2) has sufficient space
+#' 
+#' 
 #' @return a character string of the directory name
 #' @seealso \code{\link{ribiosTempfile}}
+#' @export ribiosTempdir
 ribiosTempdir <- function() {
     file.path("/data64/bi/tmp/",
               basename(tempdir()))
 }
 
-#' A temporary file which (1) every machine in the cluster has access to and (2) there is sufficient space
-#'
+#' A temporary file which (1) every machine in the cluster has access to and
+#' (2) there is sufficient space
+#' 
+#' 
 #' @param pattern Character string, file pattern
 #' @param tmpdir Character string, temp directory
 #' @param fileext CHaracter string, file name extension (suffix)
-#' 
 #' @return a character string of the file name
 #' @seealso \code{\link{ribiosTempdir}}
-
+#' @export ribiosTempfile
 ribiosTempfile <- function(pattern="file",
                            tmpdir=ribiosTempdir(), fileext="") {
     tempfile(pattern=pattern, tmpdir=tmpdir, fileext=fileext)

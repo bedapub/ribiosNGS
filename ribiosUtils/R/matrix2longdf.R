f574694eb402f1f9a35329ff310085cd39803e51
Jitao David Zhang <jitao_david.zhang@roche.com>
2019-02-28 15:38:28 +0100

    roxygen

--- ribiosUtils/R/matrix2longdf.R
+++ ribiosUtils/R/matrix2longdf.R
@@ -1,3 +1,36 @@
+#' Transform a matrix into a long-format data.frame
+#' 
+#' The function converts a matrix into a long-format, three-column data.frame,
+#' containing row, columna nd value. Such \sQuote{long} data.frames can be
+#' useful in data visualization and modelling.
+#' 
+#' The function converts a matrix into a three-column, \sQuote{long} format
+#' data.frame containing row names, column names, and values of the matrix.
+#' 
+#' @param mat A matrix
+#' @param row.names Character, row names to appear in the \code{data.frame}. If
+#' missing, the \code{rownames} of the matrix will be used. If set to
+#' \code{NULL}, or if the matrix \code{rownames} are \code{NULL}, a integer
+#' index vector starting from 1 will be used.
+#' @param col.names Charater, column names to appear in the \code{data.frame}.
+#' The rule of handling missing or NULL parameters is the same as
+#' \code{row.names} described above.
+#' @param longdf.colnames Character, column names of the output long data frame
+#' @return A \code{data.frame} object with three columns: \code{row},
+#' \code{column} and \code{value}. If the input matrix is of dimesion
+#' \code{MxN}, the returning \code{data.frame} is of the dimension \code{MNx3}.
+#' @note The length of \code{row.names} and \code{col.names} should be as the
+#' same as the matrix dimension. Otherwise the function raises warnings.
+#' @author Jitao David Zhang <jitao_david.zhang@@roche.com>
+#' @examples
+#' 
+#' test.mat <- matrix(1:12, ncol=4, nrow=3, dimnames=list(LETTERS[1:3],
+#' LETTERS[1:4]))
+#' print(test.mat)
+#' print(matrix2longdf(test.mat))
+#' print(matrix2longdf(test.mat, longdf.colnames=c("From", "To", "Time")))
+#' 
+#' @export matrix2longdf
 matrix2longdf <- function(mat,
                           row.names, col.names,
                           longdf.colnames=c("row","column","value")) {
@@ -22,20 +55,33 @@ matrix2longdf <- function(mat,
   return(res)
 }
 
-##longdf2matrix <- function(df, row.col=1L, column.col=2L, value.col=3L)  {
-##  sub <- df[, c(row.col, column.col, value.col)]
-##  tbl <- reshape(sub, idvar=colnames(sub)[1L],
-##                 timevar=colnames(sub)[2],
-##                 v.names=colnames(sub)[3],
-##                 direction="wide")
-##  mat <- data.matrix(tbl[,-1L, drop=FALSE])
-##  rownames(mat) <- tbl[,1L,drop=TRUE]
-##  new.cols <- sapply(colnames(mat),
-##                     function(x) paste(strsplit(x, "\\.")[[1]][-1], collapse="."))
-##  colnames(mat) <- new.cols
-##  return(mat)
-##}
-
+#' Convert a long-format data frame into matrix
+#' 
+#' Input data.frame must contain at least three columns: one contains row names
+#' (specified by \code{row.col}), one contains column names
+#' (\code{column.col}), and one contains values in matrix cells
+#' (\code{value.col}). The output is a 2D matrix.
+#' 
+#' @param df Long-format data frame
+#' @param row.col Character or integer, which column of the input data.frame
+#' contains row names?
+#' @param column.col Character or integer, which column contains column names?
+#' @param value.col Character or integer, which column contains matrix values?
+#' @return A 2D matrix equivalent to the long-format data frame
+#' @author Jitao David Zhang <jitao_david.zhang@@roche.com>
+#' @seealso \code{matrix2longdf}
+#' @examples
+#' 
+#' test.df <- data.frame(H=c("HSV", "BVB", "HSV", "BVB"),
+#' A=c("FCB", "S04", "S04", "FCB"),
+#' score=c(3, 1, 1, 0))
+#' longdf2matrix(test.df, row.col=1L, column.col=2L, value.col=3L)
+#' 
+#' data(Indometh)
+#' longdf2matrix(Indometh, row.col="time", column.col="Subject",value.col="conc")
+#' longdf2matrix(Indometh, row.col="Subject", column.col="time", value.col="conc")
+#' 
+#' @export longdf2matrix
 longdf2matrix <- function(df, row.col = 1L, column.col = 2L, value.col = 3L,
                           missingValue=NULL) {
     warnMissingValue <- is.null(missingValue)

19c839d89f852f2a71bf9076908b98a7aec0840b
Jitao David Zhang <jitao_david.zhang@roche.com>
2019-02-28 15:16:09 +0100

    convert allIdentical to Rd

--- ribiosUtils/R/allIdentical.R
+++ ribiosUtils/R/allIdentical.R
@@ -1,3 +1,23 @@
+#' Testing whether several objects are all identical with each other
+#' 
+#' Given several objects, the function tests whether all of them are identical.
+#' 
+#' 
+#' @param \dots Objects to be tested. Can be given as a list, or simplying
+#' appending names separated by commas, see example.
+#' @return Logical, whether all objects are the same
+#' @author Jitao David Zhang <jitao_david.zhang@@roche.com>
+#' @seealso \code{\link{identical}}
+#' @examples
+#' 
+#' test1 <- test2 <- test3 <- LETTERS[1:3]
+#' allIdentical(test1, test2, test3)
+#' allIdentical(list(test1, test2, test3))
+#' 
+#' num1 <- num2 <- num3 <- num4 <- sqrt(3)
+#' allIdentical(num1, num2, num3, num4)
+#' 
+#' @export allIdentical
 allIdentical <- function(...) {
   x <- list(...)
   if(length(x)==1) x <- x[[1L]]

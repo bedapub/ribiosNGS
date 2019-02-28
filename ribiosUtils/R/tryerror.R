e1130499c01606c0765f99dcc42952cb84aae1e0
Jitao David Zhang <jitao_david.zhang@roche.com>
2019-02-28 15:50:48 +0100

    roxygenize

--- ribiosUtils/R/tryerror.R
+++ ribiosUtils/R/tryerror.R
@@ -1,3 +1,20 @@
+#' Tell whether an object is an error
+#' 
+#' Determines whether an object is of class \code{try-error}
+#' 
+#' 
+#' @param x Any object, potentially produced within a \code{try-error}
+#' structure.
+#' @return Logical value, \code{TRUE} if \code{x} inherits the \code{try-error}
+#' class.
+#' @author Jitao David Zhang <jitao_david.zhang@@roche.com>
+#' @examples
+#' 
+#' if(exists("nonExistObj")) rm(nonExistsObj)
+#' myObj <- try(nonExistObj/5, silent=TRUE)
+#' isError(myObj)
+#' 
+#' @export isError
 isError <- function(x) {
   inherits(x, "try-error")
 }

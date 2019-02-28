a436e4b594db9ae64e7d4eb6145ebd07637eee24
Jitao David Zhang <jitao_david.zhang@roche.com>
2019-02-28 15:47:19 +0100

    stubborngc

--- ribiosUtils/R/stubborngc.R
+++ ribiosUtils/R/stubborngc.R
@@ -1,3 +1,19 @@
+#' Repeat garbage-collecting until all resource is freed
+#' 
+#' stubborngc repeats collecting garbage untill no more resource can be freed
+#' 
+#' 
+#' @param verbose Logical, verbose or not
+#' @param reset Logical, reset or not.
+#' @return Side effect is used.
+#' @author Jitao David Zhang <jitao_david.zhang@@roche.com>
+#' @seealso \code{\link{gc}}
+#' @keywords ~kwd2
+#' @examples
+#' 
+#' stubborngc()
+#' 
+#' @export stubborngc
 stubborngc <- function(verbose=FALSE, reset=TRUE) {
   .local <- function() gc(verbose=verbose, reset=reset)[2,3]
   gct <- .local()

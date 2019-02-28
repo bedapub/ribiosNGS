69dc7fbee2d22a00c1cae5aa22fa34a603ea8e93
Jitao David Zhang <jitao_david.zhang@roche.com>
2019-02-28 15:42:30 +0100

    pwdecode

--- ribiosUtils/R/pwdecode.R
+++ ribiosUtils/R/pwdecode.R
@@ -1,3 +1,27 @@
+#' Decode password with function implemented with pwencode
+#' 
+#' Decode password encypted with pwencode.
+#' 
+#' See pwdecode function documentation in BIOS for implemetnation details.
+#' 
+#' Note that since R does not support strings embedding null values
+#' (\code{\000}), the password to be decoded has to be given with two slashes,
+#' e.g. \sQuote{ \\001\\000\\129\\235}.
+#' 
+#' @param password Character string to be decoded. If starting with a empty
+#' character, the string is sent for decoding; otherwise, it is deemed as clear
+#' text password and returned.
+#' @return Decoded character string, or empty string if decoding fails
+#' @author Jitao David Zhang <jitao_david.zhang@@roche.com>. The C library code
+#' was written by Detlef Wolf.
+#' @references BIOS C module list,
+#' \url{http://bioinfo.bas.roche.com:8080/bios/common/modulelist.html}
+#' @examples
+#' 
+#' mycode <- " \\001\\000\\141\\314\\033\\033\\033\\033\\033\\142\\303\\056\\166\\311\\037\\042"
+#' pwdecode(mycode)
+#' 
+#' @export pwdecode
 pwdecode <- function(password) {
   if(!grepl("^ ", password)) {
     return(password)
@@ -7,6 +31,7 @@ pwdecode <- function(password) {
   .Call("pwdecode",x)
 }
 
+#' @export pwencode
 pwencode <- function(label="VAR", key) {
   if(missing(key))
     stop("'key' must not be missing")

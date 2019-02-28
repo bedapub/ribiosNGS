4b3ca2cd7ccc185175e5b43b47b3c5a430daa14c
Jitao David Zhang <jitao_david.zhang@roche.com>
2019-02-28 15:39:50 +0100

    namespace roxygenize

--- ribiosUtils/R/namespace.R
+++ ribiosUtils/R/namespace.R
@@ -1,4 +1,21 @@
-## sofar only accepts character
+#' Reload a package
+#' 
+#' Reload a package by first detaching and loading the library.
+#' 
+#' 
+#' @param pkg Character string, name of the package
+#' @return Side effect is used.
+#' @note So far only character is accepted
+#' @author Jitao David Zhang <jitao_david.zhang@@roche.com>
+#' @seealso \code{\link{detach}} and \code{\link{library}}
+#' @examples
+#' 
+#' \dontrun{
+#' library(ribiosUtils)
+#' reload(ribiosUtils)
+#' }
+#' 
+#' @export reload
 reload <- function(pkg) {
   pkg <- as.character(substitute(pkg))
   name <- sprintf("package:%s", pkg)

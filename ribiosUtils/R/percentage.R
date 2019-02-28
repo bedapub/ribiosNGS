e8b58ca134417c469a478375d74de50fc928bc61
Jitao David Zhang <jitao_david.zhang@roche.com>
2019-02-28 15:41:59 +0100

    percentage

--- ribiosUtils/R/percentage.R
+++ ribiosUtils/R/percentage.R
@@ -1,16 +1,17 @@
 #' Print a decimal number in procent format
-#'
-#' @aliases percentage
+#' 
 #' 
 #' @param x a decimal number, usually between -1 and 1
-#' @param fmt format string, '1.1' means a digit before and after the decimal point will be printed
-#'
+#' @param fmt format string, '1.1' means a digit before and after the decimal
+#' point will be printed
 #' @examples
+#' 
 #' percentage(c(0,0.1,0.25,1))
 #' percentage(c(0,0.1,0.25,1), fmt="1.4")
 #' percentage(c(0,-0.1,0.25,-1), fmt="+1.1")
-#'
-#' @export
+#' 
+#' 
+#' @export percentage
 percentage <- function(x, fmt="1.1") {
     format <- paste("%", fmt, "f%%", sep="")
     sprintf(format, x*100)

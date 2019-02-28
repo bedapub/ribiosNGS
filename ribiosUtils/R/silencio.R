c3d2e9b937d361b79f8440cf7a86beca4465744b
Jitao David Zhang <jitao_david.zhang@roche.com>
2019-02-28 15:46:18 +0100

    roxygenize

--- ribiosUtils/R/silencio.R
+++ ribiosUtils/R/silencio.R
@@ -1,1 +1,18 @@
+#' Keep silent by suppressing warnings and messages
+#' 
+#' The function is used to keep the command silent by suppressing warnings and
+#' messages
+#' 
+#' 
+#' @param \dots Any function call
+#' @return The same as the function call
+#' @author Jitao David Zhang <jitao_david.zhang@@roche.com>
+#' @seealso \code{\link{suppressWarnings}}, \code{\link{suppressMessages}}
+#' @examples
+#' 
+#' wsqrt <- function(x) {warning("Beep");message("Calculating square");return(x^2)}
+#' \dontrun{wsqrt(3)}
+#' silencio(wsqrt(3))
+#' 
+#' @export silencio
 silencio <- function(...) suppressWarnings(suppressMessages(...))

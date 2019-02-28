0ab9919103b543d1092a2387445c2f6ccf0aa881
Jitao David Zhang <jitao_david.zhang@roche.com>
2019-02-28 15:39:17 +0100

    roxygenize

--- ribiosUtils/R/na.false.R
+++ ribiosUtils/R/na.false.R
@@ -1,1 +1,12 @@
+#' Replace NA with FALSE
+#' 
+#' Replace \code{NA} in a vector with \code{FALSE}
+#' 
+#' 
+#' @param x A logical vector or matrix
+#' @return Logical vector or matrix with NAs replaced by FALSE
+#' @author Jitao David Zhang <jitao_david.zhang@@roche.com>
+#' @seealso myX <- c("HSV", "FCK", "FCN", NA, "BVB") res <- myX == "HSV"
+#' na.false(res)
+#' @export na.false
 na.false <- function(x) {x[is.na(x)] <- FALSE; return(x)}

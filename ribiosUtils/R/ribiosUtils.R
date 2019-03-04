b0d13a5d28ecc31a8fd1eb6ccfec73ea104bfa6a
Jitao David Zhang <jitao_david.zhang@roche.com>
2019-03-04 17:08:28 +0100

    document the package

---
+++ ribiosUtils/R/ribiosUtils.R
@@ -0,0 +1,9 @@
+#' ribiosUtils
+#'
+#' ribiosUtils is a swiss-knife package providing misc utilities
+#' @docType package
+#' @author Jitao David Zhang <jitao_david.zhang@roche.com>, with inputs from
+#'    Clemens Broger, Martin Ebeling, Laura Badi and Roland Schmucki
+#' @import methods grDevices stats utils
+#' @useDynLib ribiosUtils, .registration=TRUE, .fixes="C_"
+NULL

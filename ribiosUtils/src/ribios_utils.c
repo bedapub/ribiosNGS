773b4ac8a9f8527fc5fedcdcdaf5d962da831e70
Jitao David Zhang <jitao_david.zhang@roche.com>
2019-03-04 17:05:20 +0100

    register dynamic symbols

--- ribiosUtils/src/ribios_utils.c
+++ ribiosUtils/src/ribios_utils.c
@@ -1,6 +1,7 @@
 #include "ribios_utils.h"
 
 #include <R_ext/Rdynload.h> /* DllInfo is needed to register the routines*/
+#include <R_ext/Visibility.h>
 
 #include "r_trim.h"
 #include "r_mmatch.h"
@@ -15,4 +16,14 @@ static R_CallMethodDef callMethods[] = {
 
 void R_init_ribiosUtils(DllInfo *info) {
   R_registerRoutines(info, NULL /*CMethods*/, callMethods, NULL, NULL);
+  /* the line below says that the DLL is not to be searched
+   * for entry points specified by character strings so
+   * .C etc calls will only find registered symbols
+   */  
+  R_useDynamicSymbols(info, FALSE); 
+  /* R_forceSymbols call only allows .C etc calls which 
+   * specify entry points by R objects such as C_routineName
+   * (and not by character strings)
+   */ 
+  R_forceSymbols(info, TRUE);
 }

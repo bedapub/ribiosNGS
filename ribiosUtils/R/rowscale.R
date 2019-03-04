ad72048846109ee8a8fe3e697954bb1393d8469c
Jitao David Zhang <jitao_david.zhang@roche.com>
2019-03-04 16:58:07 +0100

    register S3 method

---
+++ ribiosUtils/R/rowscale.R
@@ -0,0 +1,39 @@
+#' S3 method for row-scaling
+#' @param x Any object
+#' @param center Logical, whether centering should be done before scaling
+#' @param scale Logical, whether scaling should be done
+#' @export rowscale
+rowscale <- function(x, center = TRUE, scale=TRUE) UseMethod("rowscale")
+
+#' Scale a matrix by row
+#' 
+#' Scaling a matrix by row can be slightly slower due to a transposing step.
+#' 
+#' @param x An matrix
+#' @param center Logical, passed to \code{scale}. 
+#' to \code{TRUE}
+#' @param scale Logical, passed to \code{scale}. 
+#' \code{TRUE}
+#' @return A matrix with each row scaled.
+#' @author Jitao David Zhang <jitao_david.zhang@@roche.com>
+#' @seealso \code{\link{scale}}
+#' @method rowscale matrix
+#' @S3method rowscale matrix
+#' @examples
+#' 
+#' mat <- matrix(rnorm(20), nrow=4)
+#' rs.mat <- rowscale(mat)
+#' 
+#' print(mat)
+#' print(rs.mat)
+#' rowMeans(rs.mat)
+#' apply(rs.mat, 1L, sd)
+#' 
+#' rowscale(mat, center=FALSE, scale=FALSE) ## equal to mat
+#' rowscale(mat, center=TRUE, scale=FALSE)
+#' rowscale(mat, center=FALSE, scale=TRUE)
+#' 
+#' @export rowscale.matrix
+rowscale.matrix <- function(x, center=TRUE, scale=TRUE) {
+  t(scale(t(x),center=center, scale=scale))
+}

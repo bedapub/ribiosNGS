4f8ad740eb31f0611c81ca35d025500c677fe65f
Jitao David Zhang <jitao_david.zhang@roche.com>
2019-04-08 09:50:16 +0200

    Add unit tests for pScore, pAbsLog10Score, and pQnormScore

--- ribiosUtils/tests/testthat/test_pScore.R
+++ ribiosUtils/tests/testthat/test_pScore.R
@@ -5,13 +5,33 @@ testPvalSign <- rep(c(-1,1), 3)
 testPvalLogFC <- c(-2, 3, -4, 5, -1.1, 1.2)
 testLogical <- rep(c(TRUE, FALSE),3)
 
-test_that("pScore works as expected", {
-  expect_equal(pScore(testPvals),
+test_that("pScore correctly wrapps pAbsLog10Score and pQnormScore", {
+  expect_equal(pScore(testPvals, sign=testPvalSign, method="absLog10"),
+               pAbsLog10Score(testPvals, sign=testPvalSign))
+  expect_equal(pScore(testPvals, sign=testPvalSign, method="qnorm"),
+               pQnormScore(testPvals, sign=testPvalSign))
+})
+
+test_that("pAbsLog10Score works as expected", {
+  expect_equal(pScore(testPvals, method="absLog10"),
                abs(log10(testPvals)))
-  expect_equal(pScore(testPvals, sign=testPvalSign),
+  expect_equal(pScore(testPvals, sign=testPvalSign, method="absLog10"),
                abs(log10(testPvals))*sign(testPvalSign))
-  expect_equal(pScore(testPvals, sign=testPvalLogFC),
+  expect_equal(pScore(testPvals, sign=testPvalLogFC, method="absLog10"),
                abs(log10(testPvals))*sign(testPvalLogFC))
-  expect_equal(pScore(testPvals, sign=testLogical),
+  expect_equal(pScore(testPvals, sign=testLogical, method="absLog10"),
                abs(log10(testPvals))*ifelse(testLogical, 1, -1))
+})
+
+invGaussianCdf <- function(p) qnorm(1-p/2, lower.tail=TRUE)
+
+test_that("pQnormScore works as expected", {
+  expect_equal(pScore(testPvals, method="qnorm"),
+               invGaussianCdf(testPvals))
+  expect_equal(pScore(testPvals, sign=testPvalSign, method="qnorm"),
+               invGaussianCdf(testPvals)*sign(testPvalSign))
+  expect_equal(pScore(testPvals, sign=testPvalLogFC, method="qnorm"),
+               invGaussianCdf(testPvals)*sign(testPvalLogFC))
+  expect_equal(pScore(testPvals, sign=testLogical, method="qnorm"),
+               invGaussianCdf(testPvals)*ifelse(testLogical, 1, -1))
 })

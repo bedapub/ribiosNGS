8c395928be836b3c4fde94afb1d5fa87da529e60
Jitao David Zhang <jitao_david.zhang@roche.com>
2020-01-21 10:37:23 +0100

    add read_illumina_sampleSheet_xls

--- R/mps-io.R
+++ R/mps-io.R
@@ -197,3 +197,59 @@ readMolPhenCoverageGct <- function(file) {
   return(res)
 }
 
+toString.data.frame = function (object, ..., digits=NULL, quote=FALSE, right=TRUE, row.names=TRUE) {
+  nRows = length(row.names(object));
+  if (length(object)==0) {
+    return(paste(
+      sprintf(ngettext(nRows, "data frame with 0 columns and %d row", "data frame with 0 columns and %d rows")
+              , nRows)
+      , "\\n", sep = "")
+    ); 
+  } else if (nRows==0) {
+    return(gettext("<0 rows> (or 0-length row.names)\\n")); 
+  } else {
+    # get text-formatted version of the data.frame
+    m = as.matrix(format.data.frame(object, digits=digits, na.encode=FALSE)); 
+    # define row-names (if required)
+    if (isTRUE(row.names)) {
+      rowNames = dimnames(object)[[1]];
+      if(is.null(rowNames)) { 
+        # no row header available -> use row numbers
+        rowNames = as.character(1:NROW(m));
+      } 
+      # add empty header (used with column headers)
+      rowNames = c("", rowNames);
+    }
+    # add column headers
+    m = rbind(dimnames(m)[[2]], m);
+    # add row headers
+    m = cbind(rowNames, m);
+    # max-length per-column
+    maxLen = apply(apply(m, c(1,2), stringr::str_length), 2, max, na.rm=TRUE);
+    
+    # add right padding
+    ##  t is needed because "If each call to FUN returns a vector of length n, then apply returns an array of dimension c(n, dim(X)[MARGIN])"
+    m = t(apply(m, 1, stringr::str_pad, width=maxLen, side="right"));
+    m = t(apply(m, 1, stringr::str_pad, width=maxLen+3, side="left"));
+    # merge columns
+    m = apply(m, 1, paste, collapse="");
+    # merge rows (and return)
+    return(paste(m, collapse="\n"));
+  }
+}
+
+#' Read Illumina MolPhen sample sheet from XLS files
+#' @param file A XLS/XLSX file containing in the first sheet
+#'  the sample sheet of a molecular phenotyping experiment
+#' @return A \code{data.frame} annotating the samples
+#' 
+read_illumina_sampleSheet_xls <- function(file) {
+   content <- readxl::read_excel(file, col_names=FALSE, 
+                                 .name_repair = "minimal")
+   tmp <- textConnection("str", "w")
+   ribiosIO::writeMatrix(content, tmp, row.names = FALSE)
+   close(tmp)
+   res <- ribiosIO::read_illumina_sampleSheet(textConnection(str, "r"),
+                                              sep="\t")
+   return(res)
+}

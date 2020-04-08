680a6e35672386d5fabfdf369dfc313cfbc22cfe
Jitao David Zhang <jitao_david.zhang@roche.com>
2020-04-08 14:38:36 +0200

    (1) exportClass and exportMethod are correct names iunstead of classes and methods (2) export functions exported by ribiosExpression for easy use

--- R/ribiosNGS.R
+++ R/ribiosNGS.R
@@ -25,7 +25,7 @@ NULL
 #' @importFrom ribiosGSEA biosCamera geneSetPerm gmtListCamera
 #' @importFrom ribiosAnnotation querydb annotateGeneIDs annotateGeneSymbols annotatemRNAs
 #' @importFrom BiocGenerics counts annotation
-#' @importFrom ribiosExpression groups dispGroups designMatrix contrastMatrix contrastNames nContrast contrastSampleIndices truncateDgeTable annotate
+#' @importFrom ribiosExpression groups dispGroups designMatrix contrastMatrix contrastNames nContrast contrastSampleIndices truncateDgeTable annotate DesignContrast
 #' @importFrom edgeR cpm aveLogCPM estimateGLMRobustDisp rpkm
 #' @importFrom graphics pairs
 #' @importFrom limma plotMDS zscoreT squeezeVar
@@ -43,6 +43,7 @@ NULL
 #' @importClassesFrom BioQC GmtList
 #' @importClassesFrom Biobase AnnotatedDataFrame ExpressionSet
 #' @export dgeWithEdgeR doGse gseWithLogFCgage gseWithCamera sigGene sigGenes sigPosGene sigNegGene sigPosGenes sigNegGenes sigGeneCounts updateSigFilter logFCmatrix model.DGEList countsSVA countsRemoveSV voomSVA voomRemoveSV cpmSVA cpmRemoveSV isEmptySV doSVA cpm.EdgeObject inferSV voomSVA updateDesignMatrixBySVA filterByCPM isAnyNA replaceNAwithZero estimateGLMDisp fitGLM testGLM annotate dgeTables dgeTable writeDgeTables writeTruncatedDgeTables modLogCPM voom logFCgage voomCamera zscoreDGE camera.EdgeResult fullEnrichTable hasCommonDisp setCommonDispIfMissing normBoxplot plotMDS.EdgeObject pairs.EdgeResult volcanoPlot smearPlot isAnnotated readMPS mpsAnnotateReporters doFisher sigGeneBarchart parseMolPhenFeat readMolPhenCoverageGct read_illumina_sampleSheet_xls translationEfficiency doEdgeR doBabel summarizeBabel mergeBabelEdgeR babelAllRnks riboseq.panel xyplot xyplot.RiboSeq riboseq.panel plotRNAMDS plotRPFMDS refBoxplot write.tableList readGseaGeneSet doGeneSetTests cpmBoxplotFile exprsScatterFile mdsFile teBoxplotFile logFCscatterFile babelVolcanoFile rspaceFile upstreamFile functionFile pathwayFile fullTableFile rnkFiles writeHTML readAmpliSeq isCherryPickingRepeat removeCherryPickingRepeat mpsReporterAssocPathway mpsReporterUpstream mpsReporterUpstreamPos mpsReporterUpstreamNeg mpsReporterDownstream mpsReporterAssocGO mpsReporterAssocRCTM readBiokitExpression readBiokitGctFile readBiokitAsDGEList DGEListToLongTable writeDGEList tpm rpkm2tpm edgeRcommand slurmEdgeRcommand slurmEdgeR minGroupCount countByGroup maxCountByGroup hasNoReplicate readCameraResults expandSigCameraResults cameraTable2network visualizeCameraNetworksByContrast parseContributingGenes parseGenesetsContributingGenes parseCameraContributingGenes rowVars prcompExprs prcomp.DGEList checkBiokitSampleAnnotation writeBiokitSampleAnnotation exportEdgeResult staticGeneLevelPlots exportStaticGeneLevelPlots checkContrastNames mergeDGEList vsnSVA
-#' @exportClasses RiboSeq riboSeqAnalysisObject EdgeResult EdgeObject EdgeGSE DGEList2
-#' @exportMethods setRnks RiboSeq normalize countRNA countRPF cpmRNA cpmRPF cpmRNAGroupSum cpmRPFGroupSum cpmFilter featureNames sampleNames commonDisp commonDisp<- BCV show plotBCV dgeList normFactors EdgeObject isAnnotated designMatrix<- contrastMatrix<-
+#' @exportClass RiboSeq riboSeqAnalysisObject EdgeResult EdgeObject EdgeGSE DGEList2
+#' @exportMethod setRnks RiboSeq normalize countRNA countRPF cpmRNA cpmRPF cpmRNAGroupSum cpmRPFGroupSum cpmFilter featureNames sampleNames commonDisp commonDisp<- BCV show plotBCV dgeList normFactors EdgeObject isAnnotated designMatrix<- contrastMatrix<-
+#' @export groups dispGroups designMatrix contrastMatrix contrastNames nContrast DesignContrast
 NULL

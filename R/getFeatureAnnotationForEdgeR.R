59d9e486f9060d9a551fae5156891eea78f5b9b4
Jitao David Zhang <jitao_david.zhang@roche.com>
2020-05-29 10:25:49 +0200

    the current version of annotating EdgeObject, will be changed when we use biokit as input

--- R/getFeatureAnnotationForEdgeR.R
+++ R/getFeatureAnnotationForEdgeR.R
@@ -1,4 +1,16 @@
-getFeatureAnnotationForEdgeR <- function(file=NULL,
-                                         featureNames=NULL) {
-  
+getFeatureAnnotationForEdgeR <- function(file = NULL,
+                                         featureNames = NULL,
+                                         noAnno = FALSE) {
+  if (!noAnno & is.null(file)) {
+    fd <- annotate(obj)
+  } else if (!is.null(file)) {
+    doLog("Annotate features with annotation file")
+    featAnno <-
+      ribiosExpression::readfile(file)
+    ordFeatAnno <- matchColumn(rownames(counts(obj)), featAnno, 1L)
+    objAnno <- obj
+    fData(objAnno) <- ordFeatAnno
+  } else {
+    objAnno <- obj
+  }
 }

6b01bc4c9d391e7dc7be875c7f46f8cec9ea3d55
Jitao David Zhang <jitao_david.zhang@roche.com>
2020-05-29 21:26:51 +0200

    File was removed from function names

--- R/readSampleAnnotationForEdgeR.R
+++ R/readSampleAnnotationForEdgeR.R
@@ -3,6 +3,7 @@
 #' @param file Character string, path to a tab-delimited file, or \code{NULL}. The first column must be 
 #'   either row names (namely no colum name), or sample names in the same order
 #'   of \code{sampleNames}. 
+#' @param ... Other parameter passed to \code{read.table}.
 #' @return A \code{data.frame} containing sample annotation, removing
 #'   `lib.size`, and `norm.factors` because they will be added
 #'   by the edgeR pipeline
@@ -12,8 +13,8 @@
 #' @examples 
 #' phenoDataFile <- system.file("extdata/phenoData/test-phenoData.txt",
 #'   package="ribiosNGS")
-#' readSampleAnnotationFileForEdgeR(phenoDataFile)
-#' readSampleAnnotationFileForEdgeR(file=NULL, sampleNames=as.character(1:4))
+#' readSampleAnnotationForEdgeR(phenoDataFile)
+#' readSampleAnnotationForEdgeR(file=NULL, sampleNames=as.character(1:4))
 readSampleAnnotationForEdgeR <- function(sampleNames, 
                                              file=NULL, ...) {
   stopifnot((!is.null(file) && file.exists(file)) | !is.null(sampleNames))

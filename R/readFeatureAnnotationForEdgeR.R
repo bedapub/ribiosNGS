0825cd97cf64ed23b875229f4a9cbb7a3304664d
Jitao David Zhang <jitao_david.zhang@roche.com>
2020-08-10 16:33:44 +0200

    update readFeatureAnnotationForEdge: allow better annotation with guessAndAnnotate

--- R/readFeatureAnnotationForEdgeR.R
+++ R/readFeatureAnnotationForEdgeR.R
@@ -3,20 +3,39 @@
 #' @param file A tab-delimited file with header that provides feature annotation
 #' @return A \code{data.frame}, with the first column named \code{FeatureName},
 #'   which are the input feature names. The rest columns contain annotations.
+#'
+#' The functions tries to parse feature annotation file if it is present. If
+#' not, it will use \code{\link[ribiosAnnotation]{guessAndAnnotate}} to 
+#' annotate the features.
+#' 
+#' @examples
+#' anno <- "GeneID\tGeneSymbol\n1234\tCCR5\n1235\tCCR6"
+#' annoFile <- tempfile()
+#' writeLines(anno, annoFile)
+#' 
+#' featIds <- c("1235", "1234")
+#' ## use file
+#' readFeatureAnnotationForEdgeR(featIds, file=annoFile)
+#' ## use ribiosAnnotation
+#' readFeatureAnnotationForEdgeR(featIds, file=NULL)
+#' 
 #' @importFrom ribiosExpression readFeatureAnnotationFile
-#' @importFrom ribiosUtils matchColumn
-#' @importFrom ribiosAnnotation annotateAnyIDs
+#' @importFrom ribiosUtils haltifnot matchColumn
+#' @importFrom ribiosAnnotation guessAndAnnotate
 #' @export
 readFeatureAnnotationForEdgeR <- function(featureNames,
                                           file=NULL) {
-  haltifnot(!missing(featureNames),
+  ribiosUtils::haltifnot(!missing(featureNames),
             msg="`featureNames` must be given.")
   if (!is.null(file) && file.exists(file)) {
     featAnno <- ribiosExpression::readFeatureAnnotationFile(file)
     res <- ribiosUtils::matchColumn(featureNames, featAnno, "FeatureName")
   } else {
-    res <- ribiosAnnotation::annotateAnyIDs(featureNames)
-    colnames(res)[1] <- "FeatureName"
+    if(!"package:ribiosAnnotation" %in% search()) {
+      attachNamespace("ribiosAnnotation")
+    }
+    anno <- ribiosAnnotation::guessAndAnnotate(featureNames)
+    res <- cbind(FeatureName=featureNames, anno)
   }
   return(res)
 }

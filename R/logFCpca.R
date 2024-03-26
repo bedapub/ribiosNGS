2d476bdfc4b9a185603e96cb959ec0edf71daefb
Jitao David Zhang <jitao_david.zhang@roche.com>
2024-03-26 10:50:51 +0100

    add logFCmatrixPca

--- R/logFCpca.R
+++ R/logFCpca.R
@@ -2,7 +2,8 @@
 NULL
 
 #' Perform principal component analysis to the log fold-change matrix
-#' @param edgeResult An \code{EdgeResult} object
+#' @param lfc A matrix of log2 fold changes, with features in rows and
+#'  contrasts in columns
 #' @return A \code{PCAScoreMatrix} object
 #' 
 #' The function performs principal component analysis (PCA) to the log 
@@ -17,12 +18,35 @@ NULL
 #' @importFrom ribiosPlot pcaScores
 #' @export
 #' @examples 
+#' my_lfc_mat <- matrix(rnorm(1000), nrow=100, ncol=10)
+#' my_lfc_pca <- logFCmatrixPCA(my_lfc_mat)
+#' my_lfc_pca
+logFCmatrixPCA <- function(lfc) {
+  lfcWithZero <- cbind(lfc, ZeroLfc = 0)
+  lfcPca <- stats::prcomp(t(lfcWithZero))
+  scores <- pcaScores(lfcPca, offset = ncol(lfcWithZero))
+  scores <- scores[-nrow(scores), , drop = FALSE]
+  return(scores)
+}
+
+#' Perform principal component analysis to an EdgeResult object
+#' @param edgeResult An \code{EdgeResult} object
+#' @return A \code{PCAScoreMatrix} object
+#' 
+#' The function performs principal component analysis (PCA) to the log 
+#' fold-change matrix. 
+#' 
+#' By using a column of zeros during the PCA analysis,
+#' which was removed from the final result, the point of origin represents 
+#' an ideal contrast which yield absolutely no differential gene expression. 
+#' It is easier to interpret the PCA results with this transformation.
+#' 
+#' @seealso logFCmatrixPCA
+#' @export
+#' @examples 
 #' ## TODO: add edgeResult sample
 logFCpca <- function(edgeResult) {
   lfc <- logFCmatrix(edgeResult)
-  lfcWithZero <- cbind(lfc, ZeroLfc=0)
-  lfcPca <- stats::prcomp(t(lfcWithZero))
-  scores <- pcaScores(lfcPca, offset=ncol(lfcWithZero))
-  scores <- scores[-nrow(scores),,drop=FALSE]
+  scores <- logFCmatrixPCA(lfc)
   return(scores)
 }
